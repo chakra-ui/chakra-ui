8f8edd02244df4931e8035fe9f14eabac89ac303
Sage <joseshegs@gmail.com>
2019-06-07 14:46:29 +0100

    Setup Project using nwb

--- packages/chakra-ui/demo/src/index.js
+++ packages/chakra-ui/demo/src/index.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-
-import Example from '../../src'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>chakra-ui Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>chakra-ui Demo</h1>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
