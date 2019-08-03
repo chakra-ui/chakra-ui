b1d43dc8735d9654cd1f02b10f6cfd21f7aff61c
Sage <joseshegs@gmail.com>
2019-08-03 14:45:34 +0100

    some changes

--- packages/chakra-ui/babel.config.js
+++ packages/chakra-ui/babel.config.js
@@ -1,11 +1,84 @@
-module.exports = function(api) {
-  api.cache(true);
+let defaultPresets;
 
-  const presets = ["@babel/preset-env", "@babel/preset-react"];
-  const plugins = [];
+// We release a ES version of Material-UI.
+// It's something that matches the latest official supported features of JavaScript.
+// Nothing more (stage-1, etc), nothing less (require, etc).
+if (process.env.BABEL_ENV === "es") {
+  defaultPresets = [];
+} else {
+  defaultPresets = [
+    [
+      "@babel/preset-env",
+      {
+        modules: ["esm", "production-umd"].includes(process.env.BABEL_ENV)
+          ? false
+          : "commonjs"
+      }
+    ]
+  ];
+}
 
-  return {
-    presets,
-    plugins
-  };
+const defaultAlias = {
+  "chakra-ui": "./src"
+};
+
+const productionPlugins = [
+  [
+    "babel-plugin-transform-react-remove-prop-types",
+    {
+      mode: "unsafe-wrap"
+    }
+  ]
+];
+
+module.exports = {
+  presets: defaultPresets.concat(["@babel/preset-react"]),
+  plugins: [
+    ["@babel/plugin-proposal-object-rest-spread", { loose: true }],
+    "@babel/plugin-transform-runtime",
+    // for IE 11 support
+    "@babel/plugin-transform-object-assign"
+  ],
+  ignore: [/@babel[\\|/]runtime/], // Fix a Windows issue.
+  env: {
+    cjs: {
+      plugins: productionPlugins
+    },
+    esm: {
+      plugins: [
+        ...productionPlugins,
+        ["@babel/plugin-transform-runtime", { useESModules: true }]
+      ]
+    },
+    es: {
+      plugins: [
+        ...productionPlugins,
+        ["@babel/plugin-transform-runtime", { useESModules: true }]
+      ]
+    },
+    production: {
+      plugins: [
+        ...productionPlugins,
+        ["@babel/plugin-transform-runtime", { useESModules: true }]
+      ]
+    },
+    "production-umd": {
+      plugins: [
+        ...productionPlugins,
+        ["@babel/plugin-transform-runtime", { useESModules: true }]
+      ]
+    },
+    test: {
+      sourceMaps: "both",
+      plugins: [
+        [
+          "babel-plugin-module-resolver",
+          {
+            root: ["./"],
+            alias: defaultAlias
+          }
+        ]
+      ]
+    }
+  }
 };
