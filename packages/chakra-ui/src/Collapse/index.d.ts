96fd41c80a9bf2d9d47d213ee4a577cc23dc5c46
Sage <joseshegs@gmail.com>
2019-08-17 14:07:27 +0100

    Modify Collapse type defs

--- packages/chakra-ui/src/Collapse/index.d.ts
+++ packages/chakra-ui/src/Collapse/index.d.ts
@@ -2,9 +2,21 @@ import * as React from "react";
 import { BoxProps } from "../Box";
 
 export interface ICollapse {
+  /**
+   * If `true`, the content will be visible
+   */
   isOpen?: boolean;
+  /**
+   * If `true`, the opacity of the content will be animated
+   */
   animateOpacity?: boolean;
+  /**
+   * The animation duration as it expands
+   */
   duration?: number;
+  /**
+   * The height you want the content to be collapsed to
+   */
   startingHeight?: number | string;
   endingHeight?: number | string;
   onAnimationEnd?(props: { newHeight: number }): void;
