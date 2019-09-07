a177372612c783d9f184420ab4f474134d88437e
Vynlar <lightningzygella@gmail.com>
2019-09-07 09:44:02 -0500

    Removed describe block and unneeded call to cleanup

--- packages/chakra-ui/src/Badge/test.tsx
+++ packages/chakra-ui/src/Badge/test.tsx
@@ -1,31 +1,27 @@
 import * as React from "react";
-import { render, cleanup } from "../utils/testing";
+import { render } from "../utils/testing";
 
 import Badge, { IBadge } from "./index";
 
-describe("Badge", () => {
-  afterEach(cleanup);
+it("should render all variants", () => {
+  const variants: IBadge["variant"][] = [
+    undefined,
+    "outline",
+    "solid",
+    "subtle",
+  ];
+  const colors: IBadge["variantColor"][] = ["red", "green"];
+  const rendered: HTMLElement[] = [];
 
-  it("should render all variants", () => {
-    const variants: IBadge["variant"][] = [
-      undefined,
-      "outline",
-      "solid",
-      "subtle",
-    ];
-    const colors: IBadge["variantColor"][] = ["red", "green"];
-    const rendered: HTMLElement[] = [];
-
-    variants.map(variant => {
-      colors.map(color => {
-        const { container } = render(
-          <Badge variant={variant} variantColor={color}>
-            content
-          </Badge>,
-        );
-        rendered.push(container);
-      });
+  variants.map(variant => {
+    colors.map(color => {
+      const { container } = render(
+        <Badge variant={variant} variantColor={color}>
+          content
+        </Badge>,
+      );
+      rendered.push(container);
     });
-    expect(rendered).toMatchSnapshot();
   });
+  expect(rendered).toMatchSnapshot();
 });
