eed0ad4659e2fc05eb2d755d2168c204f2a8f3bf
Sage <joseshegs@gmail.com>
2019-09-25 21:11:16 +0100

    chore: remove proptypes

--- packages/chakra-ui/src/Badge/index.js
+++ packages/chakra-ui/src/Badge/index.js
@@ -1,37 +1,29 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import propTypes from "prop-types";
 import useBadgeStyle from "./styles";
 import Box from "../Box";
 import { forwardRef } from "react";
 
-const Badge = forwardRef(({ variantColor, variant, ...props }, ref) => {
-  const badgeStyleProps = useBadgeStyle({ color: variantColor, variant });
+const Badge = forwardRef(
+  ({ variantColor = "gray", variant = "subtle", ...props }, ref) => {
+    const badgeStyleProps = useBadgeStyle({ color: variantColor, variant });
 
-  return (
-    <Box
-      ref={ref}
-      display="inline-block"
-      px={1}
-      textTransform="uppercase"
-      fontSize="xs"
-      borderRadius="sm"
-      fontWeight="bold"
-      whiteSpace="nowrap"
-      verticalAlign="middle"
-      {...badgeStyleProps}
-      {...props}
-    />
-  );
-});
-
-Badge.propTypes = {
-  variant: propTypes.oneOf(["solid", "subtle", "outline"]),
-};
-
-Badge.defaultProps = {
-  variantColor: "gray",
-  variant: "subtle",
-};
+    return (
+      <Box
+        ref={ref}
+        display="inline-block"
+        px={1}
+        textTransform="uppercase"
+        fontSize="xs"
+        borderRadius="sm"
+        fontWeight="bold"
+        whiteSpace="nowrap"
+        verticalAlign="middle"
+        {...badgeStyleProps}
+        {...props}
+      />
+    );
+  },
+);
 
 export default Badge;
