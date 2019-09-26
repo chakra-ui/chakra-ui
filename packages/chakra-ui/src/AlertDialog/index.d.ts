da3c7065570c73a1947ba2a87c7c5605315ec468
Sage <joseshegs@gmail.com>
2019-09-27 00:00:06 +0100

    fix(alertdialog): update type definitions

--- packages/chakra-ui/src/AlertDialog/index.d.ts
+++ packages/chakra-ui/src/AlertDialog/index.d.ts
@@ -9,28 +9,10 @@ interface IAlertDialog extends Omit<IModal, "initialFocusRef"> {
   leastDestructiveRef: React.RefObject<HTMLElement>;
 }
 
-export type AlertDialogProps = IAlertDialog & BoxProps;
-
-/**
- * AlertDialog component is used interrupt the user with a mandatory confirmation or action.
- */
-declare const AlertDialog: React.FC<AlertDialogProps>;
-
-/**
- * The `AlertDialogHeader` contains the title announced by screen readers.
- */
+export const AlertDialog: React.FC<IAlertDialog>;
 export const AlertDialogHeader: React.FC<BoxProps>;
-
-/**
- * The `AlertDialogBody` contains the description announced by screen readers.
- */
 export const AlertDialogBody: React.FC<BoxProps>;
-
-/**
- * The `AlertDialogFooter` contains the actions of the dialog.
- */
+export const AlertDialogContent: React.FC<BoxProps>;
+export const AlertDialogOverlay: React.FC<BoxProps>;
 export const AlertDialogFooter: React.FC<BoxProps>;
-
 export const AlertDialogCloseButton: React.FC<CloseButtonProps>;
-
-export default AlertDialog;
