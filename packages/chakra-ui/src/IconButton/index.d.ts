0ddbea7ba5df79437d59ef9d74dd7c11bcbf80b0
Emerson Laurentino <emersonlaurentino@hotmail.com>
2019-09-26 23:48:14 -0300

    Fix icon type

--- packages/chakra-ui/src/IconButton/index.d.ts
+++ packages/chakra-ui/src/IconButton/index.d.ts
@@ -12,7 +12,7 @@ interface IIconButton {
    * The icon to be used. Refer to the [Icons](https://chakra-ui.com/icon/) section
    * of the docs for the available icon options.
    */
-  icon?: string;
+  icon: string | JSX.Element;
   /**
    * If `true`, the button will be perfectly round. Else, it'll be slightly round
    */
