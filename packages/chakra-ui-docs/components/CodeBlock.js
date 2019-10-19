f50d11f4d1ac46638aa62e796849d35d602efa33
jescowuester <jesco@storyofams.com>
2019-10-19 13:52:10 +0200

    chore(docs): enabled button to copy to clipboard on readonly code blocks

--- packages/chakra-ui-docs/components/CodeBlock.js
+++ packages/chakra-ui-docs/components/CodeBlock.js
@@ -179,7 +179,13 @@ const CodeBlock = ({
 
   return (
     <LiveProvider disabled {...liveProviderProps}>
-      <LiveEditor padding={20} style={liveEditorStyle} />
+      <Box position="relative">
+        <LiveEditor padding={20} style={liveEditorStyle} />
+
+        <CopyButton top="1.25em" onClick={onCopy}>
+          {hasCopied ? "copied" : "copy"}
+        </CopyButton>
+      </Box>
     </LiveProvider>
   );
 
