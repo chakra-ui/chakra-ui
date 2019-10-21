1dce8fcfac6bfbfc16ed479eb7b10a0ea23c0d8f
Sage <joseshegs@gmail.com>
2019-10-21 17:38:03 +0100

    chore: add gastby starter

---
+++ examples/gatsby-starter-chakra-ui/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/Sage/Documents/CodeProjects/chakra/packages/gatsby-plugin-chakra-ui/gatsby-browser.js'),
+      options: {"plugins":[],"isResettingCSS":true,"isUsingColorMode":true},
+    }]
---
+++ examples/gatsby-starter-chakra-ui/.cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/Sage/Documents/CodeProjects/chakra/examples/gatsby-starter-chakra-ui/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/Sage/Documents/CodeProjects/chakra/examples/gatsby-starter-chakra-ui/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
