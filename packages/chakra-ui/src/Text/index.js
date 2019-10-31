5f1d815db7bc923e6bb8aed2fa55697a878f0b92
hendrikniemann <h.neverman@web.de>
2019-10-31 13:02:46 +0100

    feat(theme): apply fonts to Heading and Text
    This commit adds the `fontFamily`attribute to Heading and Text
    components. This way the font families specified in the theme are
    applied to the headings and texts automatically.
    
    This change is introduced to make the `fonts` property in the theme
    object behave more consistently with the other properties in the theme.
    
    Currently it is still unclear how to apply the font specified in the
    `body` property of the fonts object to all elements automatically but
    this change can be a good start.

--- packages/chakra-ui/src/Text/index.js
+++ packages/chakra-ui/src/Text/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "../Box";
 
 const Text = React.forwardRef((props, ref) => {
-  return <Box ref={ref} as="p" {...props} />;
+  return <Box ref={ref} as="p" fontFamily="body" {...props} />;
 });
 
 export default Text;
