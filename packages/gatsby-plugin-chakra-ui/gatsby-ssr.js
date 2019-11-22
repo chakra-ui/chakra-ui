761bcb257ed61dd1e15853a62e48284722302d8b
alexlbr <alex.lobera@gmail.com>
2019-11-22 18:38:18 +0000

    add build to transpile source files

--- packages/gatsby-plugin-chakra-ui/gatsby-ssr.js
+++ packages/gatsby-plugin-chakra-ui/gatsby-ssr.js
@@ -1,1 +1,8 @@
-export { wrapRootElement } from "./gatsby-browser";
+"use strict";
+
+exports.__esModule = true;
+exports.wrapRootElement = void 0;
+
+var _gatsbyBrowser = require("./gatsby-browser");
+
+exports.wrapRootElement = _gatsbyBrowser.wrapRootElement;
