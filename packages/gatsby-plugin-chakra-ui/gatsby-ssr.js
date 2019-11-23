d64cb06dee100bf92f9e035da7847891aba8af63
alexlbr <alex.lobera@gmail.com>
2019-11-23 10:26:30 +0000

    remove transpiled files from gatsby-plugin-chakra-ui

--- packages/gatsby-plugin-chakra-ui/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.wrapRootElement = void 0;
-
-var _gatsbyBrowser = require("./gatsby-browser");
-
-exports.wrapRootElement = _gatsbyBrowser.wrapRootElement;
