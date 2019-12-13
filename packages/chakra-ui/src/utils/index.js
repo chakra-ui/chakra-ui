fb8b7f2a40677447e320c867eb2d5cceaaabffd2
Sage <joseshegs@gmail.com>
2019-12-13 10:22:31 +0100

    build(docs): remove now.json from docs package

--- packages/chakra-ui/src/utils/index.js
+++ packages/chakra-ui/src/utils/index.js
@@ -146,29 +146,30 @@ export const inputProps = [
 
 export function useVariantColorWarning(label, variantColor) {
   const theme = useTheme();
-  const variantColorIsDefined = variantColor != null;
-
-  if (variantColorIsDefined) {
-    const variantColorExists = variantColor in theme.colors;
-    // If variant color exists in theme object
-    if (!variantColorExists) {
-      throw Error(
-        `You passed an invalid variantColor to the ${label} Component. Variant color values must be a color key in the theme object that has '100' - '900' color values. Check http://chakra-ui.com/theme#colors to see possible values`,
-      );
-    }
-
-    // if variant color exists and is an object
-    // TODO: Check for the 100 - 900 keys
-    if (variantColorExists) {
-      const colorObj = theme.colors[variantColor];
-      const variantColorIsObject =
-        typeof colorObj === "object" && Object.keys(colorObj).length > 0;
-
-      if (!variantColorIsObject) {
-        throw Error(
-          `${label}: The variantColor passed exists in the theme object but is not valid. For a color to be a valid variantColor, it must be an object that has '100' - '900' color values. Use a tool like: 
-        https://smart-swatch.netlify.com/ to generate color values quickly`,
+  if (process.env.NODE_ENV !== "production") {
+    const variantColorIsDefined = variantColor != null;
+    if (variantColorIsDefined) {
+      const variantColorExists = variantColor in theme.colors;
+      // If variant color exists in theme object
+      if (!variantColorExists) {
+        console.error(
+          `You passed an invalid variantColor to the ${label} Component. Variant color values must be a color key in the theme object that has '100' - '900' color values. Check http://chakra-ui.com/theme#colors to see possible values`,
         );
+      }
+
+      // if variant color exists and is an object
+      // TODO: Check for the 100 - 900 keys
+      if (variantColorExists) {
+        const colorObj = theme.colors[variantColor];
+        const variantColorIsObject =
+          typeof colorObj === "object" && Object.keys(colorObj).length > 0;
+
+        if (!variantColorIsObject) {
+          console.error(
+            `${label}: The variantColor passed exists in the theme object but is not valid. For a color to be a valid variantColor, it must be an object that has '100' - '900' color values. Use a tool like: 
+        https://smart-swatch.netlify.com/ to generate color values quickly`,
+          );
+        }
       }
     }
   }
