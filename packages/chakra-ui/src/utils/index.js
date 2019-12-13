8bcadb0152f16346e08a0928e72b4f6dde6ce003
Sage <joseshegs@gmail.com>
2019-12-13 11:17:46 +0100

    chore(variantcolor): change console.error to console.warn

--- packages/chakra-ui/src/utils/index.js
+++ packages/chakra-ui/src/utils/index.js
@@ -152,7 +152,7 @@ export function useVariantColorWarning(label, variantColor) {
       const variantColorExists = variantColor in theme.colors;
       // If variant color exists in theme object
       if (!variantColorExists) {
-        console.error(
+        console.warn(
           `You passed an invalid variantColor to the ${label} Component. Variant color values must be a color key in the theme object that has '100' - '900' color values. Check http://chakra-ui.com/theme#colors to see possible values`,
         );
       }
@@ -165,7 +165,7 @@ export function useVariantColorWarning(label, variantColor) {
           typeof colorObj === "object" && Object.keys(colorObj).length > 0;
 
         if (!variantColorIsObject) {
-          console.error(
+          console.warn(
             `${label}: The variantColor passed exists in the theme object but is not valid. For a color to be a valid variantColor, it must be an object that has '100' - '900' color values. Use a tool like: 
         https://smart-swatch.netlify.com/ to generate color values quickly`,
           );
