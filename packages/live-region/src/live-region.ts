ad9536deb16edca264a486d809784811fbaef77f
Sage <joseshegs@gmail.com>
2020-01-20 14:53:15 +0100

    chore: add doc blocks to live-region

--- packages/live-region/src/live-region.ts
+++ packages/live-region/src/live-region.ts
@@ -4,20 +4,34 @@ export interface LiveRegionOptions {
    */
   id?: string;
   /**
-   * The desired value of the `aria-live` attribute.
+   * Used to mark a part of the page as "live" so that updates will
+   * be communicated to users by screen readers.
+   *
+   * - If set to `polite`: tells assistive technology to alert the user
+   * to this change when it has finished whatever it is currently doing
+   *
+   * - If set to `assertive`: tells assistive technology to interrupt whatever
+   * it's doing and alert the user to this change immediately
+   *
    * @default "polite".
    */
   "aria-live"?: "polite" | "assertive";
   /**
    * The desired value of the role attribute
-   * @default "log"
+   * @default "status"
    */
   role?: "status" | "alert" | "log";
   /**
-   * The desired value of the aria-relevant attribute.
-   * @default "additions"
+   * Indicates what types of changes should be presented to the user.
+   * @default "all"
    */
   "aria-relevant"?: React.AriaAttributes["aria-relevant"];
+  /**
+   * Indicates whether the entire region should be
+   * considered as a whole when communicating updates
+   *
+   * @default true
+   */
   "aria-atomic"?: React.AriaAttributes["aria-atomic"];
   /**
    * The node to append the live region node to
