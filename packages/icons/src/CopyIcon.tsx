5ad27b5f8ead676f7916d74f91d303192ca7fb3d
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-21 23:20:43 +0100

    chore: add addicon and small add icon

---
+++ packages/icons/src/CopyIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Icon, IconProps } from "@chakra-ui/icon";
+
+export function CopyIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
+      />
+    </Icon>
+  );
+}
+
+export default CopyIcon;
---
+++ packages/icons/src/SearchIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Icon, IconProps } from "@chakra-ui/icon";
+
+export function SearchIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M23.384,21.619,16.855,15.09a9.284,9.284,0,1,0-1.768,1.768l6.529,6.529a1.266,1.266,0,0,0,1.768,0A1.251,1.251,0,0,0,23.384,21.619ZM2.75,9.5a6.75,6.75,0,1,1,6.75,6.75A6.758,6.758,0,0,1,2.75,9.5Z"
+      />
+    </Icon>
+  );
+}
+
+export default SearchIcon;
