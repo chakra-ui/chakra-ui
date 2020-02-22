c379deefc0a1b083486f66e3b71dd69c351f59d9
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-22 10:52:20 +0100

    chore: add arrown icon and attachment icon

---
+++ packages/icons/src/ArrowLeftIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Icon, IconProps } from "@chakra-ui/icon"
+
+export function ArrowLeftIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <g fill="currentColor">
+        <path d="M10.416,12a2.643,2.643,0,0,1,.775-1.875L20.732.584a1.768,1.768,0,0,1,2.5,2.5l-8.739,8.739a.25.25,0,0,0,0,.354l8.739,8.739a1.768,1.768,0,0,1-2.5,2.5l-9.541-9.541A2.643,2.643,0,0,1,10.416,12Z" />
+        <path d="M.25,12a2.643,2.643,0,0,1,.775-1.875L10.566.584a1.768,1.768,0,0,1,2.5,2.5L4.327,11.823a.25.25,0,0,0,0,.354l8.739,8.739a1.768,1.768,0,0,1-2.5,2.5L1.025,13.875A2.643,2.643,0,0,1,.25,12Z" />
+      </g>
+    </Icon>
+  )
+}
+
+export default ArrowLeftIcon
