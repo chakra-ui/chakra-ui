daf9e55935907d7f5e6bb9d95b61d7474853adee
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-22 14:58:07 +0100

    chore: add warning icon and arrow up down icon

---
+++ packages/icons/src/WarningIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Icon, IconProps } from "@chakra-ui/icon"
+
+export function WarningIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z"
+      />
+    </Icon>
+  )
+}
+
+export default WarningIcon
---
+++ packages/icons/src/WarningTwoIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Icon, IconProps } from "@chakra-ui/icon"
+
+export function WarningTwoIcon(props: IconProps) {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M23.119,20,13.772,2.15h0a2,2,0,0,0-3.543,0L.881,20a2,2,0,0,0,1.772,2.928H21.347A2,2,0,0,0,23.119,20ZM11,8.423a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Zm1.05,11.51h-.028a1.528,1.528,0,0,1-1.522-1.47,1.476,1.476,0,0,1,1.448-1.53h.028A1.527,1.527,0,0,1,13.5,18.4,1.475,1.475,0,0,1,12.05,19.933Z"
+      />
+    </Icon>
+  )
+}
+
+export default WarningTwoIcon
