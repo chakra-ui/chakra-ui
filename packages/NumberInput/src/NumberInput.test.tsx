687d52d1b622244f35c64e0e6432bcee58bd572d
Sage <joseshegs@gmail.com>
2020-02-23 14:18:57 +0400

    chore: wip test for number-input

--- packages/NumberInput/src/NumberInput.test.tsx
+++ packages/NumberInput/src/NumberInput.test.tsx
@@ -3,11 +3,9 @@ import * as React from "react"
 import { useNumberInput } from "./NumberInput.hook"
 
 // https://github.com/palantir/blueprint/blob/3aa56473d253f5287e0960759bee367a9ff3e045/packages/core/test/controls/numericInputTests.tsx
-describe("useNumberInput", () => {
-  describe("defaults", () => {
-    it("has a stepSize of 1 by default", () => {
-      const { result } = renderHook(() => useNumberInput())
-      expect(result.current.value).toBe(0)
-    })
+describe("defaults", () => {
+  test("has value of 0 by default", () => {
+    const { result } = renderHook(() => useNumberInput())
+    expect(result.current.value).toBe(0)
   })
 })
