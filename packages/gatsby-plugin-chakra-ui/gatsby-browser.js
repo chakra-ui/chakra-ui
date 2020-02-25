88cb71bd8ab3425ac83c9af9c905e026216a834c
Trevor Blades <tdblades@gmail.com>
2020-02-25 14:56:48 -0800

    Update path to src/theme.js

--- packages/gatsby-plugin-chakra-ui/gatsby-browser.js
+++ packages/gatsby-plugin-chakra-ui/gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core";
-import theme from "./theme";
+import theme from "./src/theme";
 
 export const wrapRootElement = (
   { element },
