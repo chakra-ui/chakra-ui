0d34599e1b80b2927176d13992e4ba0f365177bd
Sage <joseshegs@gmail.com>
2020-03-06 16:46:24 +0400

    chore: update init script

--- packages/color-mode/src/InitializeColorModeScript.tsx
+++ packages/color-mode/src/InitializeColorModeScript.tsx
@@ -1,15 +1,47 @@
-import React from "react"
+import * as React from "react"
 
-const noflash = `(function() { try {
-  var mode = localStorage.getItem('chakra-ui-color-mode');
-  if (!mode) return
-  document.body.classList.add('chakra-ui-' + mode);
-} catch (e) {} })();`
+export const InitializeColorMode = () => (
+  <script
+    key="chakra-ui-no-flash"
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function() {
+        var storageKey = "chakra-ui-color-mode";
+        var classNameDark = "chakra-ui-dark";
+        var classNameLight = "chakra-ui-light";
+  
+        function setClassOnDocumentBody(darkMode) {
+          document.body.classList.add(darkMode ? classNameDark : classNameLight);
+          document.body.classList.remove(
+            darkMode ? classNameLight : classNameDark,
+          );
+        }
+  
+        var preferDarkQuery = "(prefers-color-scheme: dark)";
+        var mql = window.matchMedia(preferDarkQuery);
+        var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+        var localStorageTheme = null;
+        try {
+          localStorageTheme = localStorage.getItem(storageKey);
+        } catch (err) {}
+        var localStorageExists = localStorageTheme !== null;
+        if (localStorageExists) {
+          localStorageTheme = JSON.parse(localStorageTheme);
+        }
+  
+        if (localStorageExists) {
+          setClassOnDocumentBody(localStorageTheme);
+        } else if (supportsColorSchemeQuery) {
+          setClassOnDocumentBody(mql.matches);
+          localStorage.setItem(storageKey, mql.matches ? "dark" : "light");
+        } else {
+          var isDarkMode = document.body.classList.contains(classNameDark);
+          localStorage.setItem(storageKey, isDarkMode ? "dark": "light");
+        }
+      })();
+      `,
+    }}
+  />
+)
 
-export const InitializeColorMode = () =>
-  React.createElement("script", {
-    key: "chakra-ui-no-flash",
-    dangerouslySetInnerHTML: {
-      __html: noflash,
-    },
-  })
+export default InitializeColorMode
