882fdd323701d15013c82ddd92b42423fb62500d
Mark Chandler <mark@chanchan.io>
2020-03-26 20:02:06 -0400

    test: add tests for Popper utils

---
+++ packages/popper/src/tests/Popper.utils.test.ts
@@ -0,0 +1,78 @@
+import { getOppositePosition, getArrowStyles } from "../Popper.utils"
+
+describe("getOppositePosition", () => {
+  test.each`
+    direction   | opposite
+    ${"top"}    | ${"bottom"}
+    ${"bottom"} | ${"top"}
+    ${"right"}  | ${"left"}
+    ${"left"}   | ${"right"}
+  `(
+    `opposite position of $direction is $opposite`,
+    ({ direction, opposite }) => {
+      expect(getOppositePosition(direction)).toBe(opposite)
+    },
+  )
+})
+
+describe("getArrowStyles", () => {
+  const arrowSize = 4
+  const baseExpected = {
+    width: 4,
+    height: 4,
+    position: "absolute",
+    transform: "rotate(45deg)",
+  }
+
+  test("Placement: auto", () => {
+    const expected = {}
+
+    expect(getArrowStyles("auto", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("auto-start", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("auto-end", arrowSize)).toEqual(expected)
+  })
+
+  test("Placement: top", () => {
+    const expected = {
+      ...baseExpected,
+      bottom: "-2px",
+    }
+
+    expect(getArrowStyles("top", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("top-start", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("top-end", arrowSize)).toEqual(expected)
+  })
+
+  test("Placement: bottom", () => {
+    const expected = {
+      ...baseExpected,
+      top: "-2px",
+    }
+
+    expect(getArrowStyles("bottom", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("bottom-start", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("bottom-end", arrowSize)).toEqual(expected)
+  })
+
+  test("Placement: right", () => {
+    const expected = {
+      ...baseExpected,
+      left: "-2px",
+    }
+
+    expect(getArrowStyles("right", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("right-start", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("right-end", arrowSize)).toEqual(expected)
+  })
+
+  test("Placement: left", () => {
+    const expected = {
+      ...baseExpected,
+      right: "-2px",
+    }
+
+    expect(getArrowStyles("left", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("left-start", arrowSize)).toEqual(expected)
+    expect(getArrowStyles("left-end", arrowSize)).toEqual(expected)
+  })
+})
