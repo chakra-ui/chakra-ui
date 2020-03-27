585712eab92d40418c006dd94f968c423cd6b3f4
Segun Adebayo <joseshegs@gmail.com>
2020-03-27 18:02:46 +0400

    test: add tests for layout components

--- packages/layout/src/tests/layout.test.tsx
+++ packages/layout/src/tests/layout.test.tsx
@@ -1,17 +1,35 @@
 import { render } from "@chakra-ui/test-utils"
 import React from "react"
-import { Box } from ".."
+import { Box, Badge } from ".."
 
-test("should render box correctly", () => {
-  const tools = render(<Box>This is box</Box>)
-  expect(tools.asFragment()).toMatchSnapshot()
+describe("Box", () => {
+  test("should render box correctly", () => {
+    const tools = render(<Box>This is box</Box>)
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
+
+  test("as - prop works correctly", () => {
+    const tools = render(
+      <Box as="a" href="www.google.com">
+        This is box
+      </Box>,
+    )
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
 })
 
-test("as - prop works correctly", () => {
-  const tools = render(
-    <Box as="a" href="www.google.com">
-      This is box
-    </Box>,
-  )
-  expect(tools.asFragment()).toMatchSnapshot()
+describe("Badge", () => {
+  test("should render with default theming", () => {
+    const tools = render(<Badge>Badge</Badge>)
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
+
+  test("override the theming props", () => {
+    const tools = render(
+      <Badge variant="outline" colorScheme="pink">
+        Badge
+      </Badge>,
+    )
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
 })
--- packages/system/src/styled-basic.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { chakra } from "./styled"
-
-export default {
-  title: "Test",
-}
-
-export const basic = () => <chakra.div>welcome</chakra.div>
