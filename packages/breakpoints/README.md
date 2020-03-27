8ade77d04d1457930bbcdc5e23dab31b9bf40485
Segun Adebayo <joseshegs@gmail.com>
2020-03-28 01:22:08 +0400

    chore: updates

--- packages/breakpoints/README.md
+++ packages/breakpoints/README.md
@@ -7,3 +7,10 @@ A Quick description of the component
 ```sh
 yarn add @chakra-ui/<component>
 ```
+
+## Inspirations
+
+Chakra takes inspiration from these amazing libraries
+
+- https://github.com/iiroj/use-breakpoint
+- https://github.com/contra/react-responsive
--- packages/breakpoints/src/breakpoints.hook.tsx
+++ packages/breakpoints/src/breakpoints.hook.tsx
@@ -2,6 +2,93 @@ import { useTheme } from "@chakra-ui/system"
 import { Dict, isNumber } from "@chakra-ui/utils"
 import { useCallback, useEffect, useMemo, useState } from "react"
 
+/**
+ * useBreakpoint
+ *
+ * React hook for getting the current responsive media breakpoint.
+ *
+ * @param defaultBreakpoint default breakpoint name
+ * (in non-window environments like SSR)
+ *
+ * For SSR, you can use a package like [is-mobile](https://github.com/juliangruber/is-mobile)
+ * to get the default breakpoint value from the user-agent
+ */
+
+export function useBreakpoint(defaultBreakpoint?: string) {
+  const theme = useTheme() as Dict
+
+  const { breakpoints } = theme
+
+  const mediaQueries = useMemo(
+    () => createMediaQueries({ base: `0px`, ...breakpoints }),
+    [breakpoints],
+  )
+
+  const [currentBreakpoint, setCurrentBreakpoint] = useState(() => {
+    if (!defaultBreakpoint) return undefined
+    const mediaQuery = mediaQueries.find(
+      query => query.breakpoint === defaultBreakpoint,
+    )
+
+    if (mediaQuery) {
+      const { query, ...breakpoint } = mediaQuery
+      return breakpoint
+    }
+
+    return undefined
+  })
+
+  const current = currentBreakpoint?.breakpoint
+
+  const update = useCallback(
+    (query: MediaQueryList, breakpoint: Breakpoint) => {
+      if (query.matches && current !== breakpoint.breakpoint) {
+        setCurrentBreakpoint(breakpoint)
+      }
+    },
+    [current],
+  )
+
+  useEffect(() => {
+    const listeners: Listener[] = []
+
+    mediaQueries.forEach(({ query, ...breakpoint }) => {
+      const mediaQuery = window.matchMedia(query)
+
+      // trigger an initial update to determine media query
+      update(mediaQuery, breakpoint)
+
+      const handleChange = () => {
+        update(mediaQuery, breakpoint)
+      }
+
+      // add media query-listender
+      mediaQuery.addListener(handleChange)
+
+      // push the media query list handleChange
+      // so we can use it to remove Listener
+      listeners.push({ mediaQuery, handleChange })
+
+      return () => {
+        // clean up 1
+        mediaQuery.removeListener(handleChange)
+      }
+    })
+    return () => {
+      // clean up 2: for safety
+      listeners.forEach(({ mediaQuery, handleChange }) => {
+        mediaQuery.removeListener(handleChange)
+      })
+    }
+  }, [mediaQueries, breakpoints, update])
+
+  return currentBreakpoint?.breakpoint
+}
+
+/**
+ * @todo add useBreakpointValue hook
+ */
+
 export type Breakpoint = {
   breakpoint: string
   maxWidth?: string
@@ -61,89 +148,3 @@ const createMediaQueries = (breakpoints: Dict) => {
     return mediaQuery
   })
 }
-
-/**
- * useBreakpoint
- *
- * React hook for getting the current responsive media breakpoint.
- *
- * @param defaultBreakpoint default breakpoint name
- * (in non-window environments like SSR)
- *
- * For SSR, you can use a package like [is-mobile](https://github.com/juliangruber/is-mobile)
- * to get the default breakpoint value from the user-agent
- */
-
-export function useBreakpoint(defaultBreakpoint?: string) {
-  const theme = useTheme() as Dict
-
-  const { breakpoints } = theme
-
-  const mediaQueries = useMemo(
-    () => createMediaQueries({ base: `0px`, ...breakpoints }),
-    [breakpoints],
-  )
-
-  const [currentBreakpoint, setCurrentBreakpoint] = useState(() => {
-    if (!defaultBreakpoint) return undefined
-    const val = mediaQueries.find(
-      query => query.breakpoint === defaultBreakpoint,
-    )
-    if (val) {
-      const { query, ...breakpoint } = val
-      return breakpoint
-    }
-
-    return undefined
-  })
-
-  const current = currentBreakpoint && currentBreakpoint.breakpoint
-
-  const update = useCallback(
-    ({ matches }: MediaQueryList, breakpoint: Breakpoint) => {
-      if (matches && current !== breakpoint.breakpoint) {
-        setCurrentBreakpoint(breakpoint)
-      }
-    },
-    [current],
-  )
-
-  useEffect(() => {
-    const listeners: Listener[] = []
-
-    mediaQueries.forEach(({ query, ...breakpoint }) => {
-      const mediaQuery = window.matchMedia(query)
-
-      // trigger an initial update to determine media query
-      update(mediaQuery, breakpoint)
-
-      const handleChange = () => {
-        update(mediaQuery, breakpoint)
-      }
-
-      // add media query-listender
-      mediaQuery.addListener(handleChange)
-
-      // push the media query list handleChange
-      // so we can use it to remove Listener
-      listeners.push({ mediaQuery, handleChange })
-
-      return () => {
-        // clean up 1
-        mediaQuery.removeListener(handleChange)
-      }
-    })
-    return () => {
-      // clean up 2: for safety
-      listeners.forEach(({ mediaQuery, handleChange }) => {
-        mediaQuery.removeListener(handleChange)
-      })
-    }
-  }, [mediaQueries, breakpoints, update])
-
-  return currentBreakpoint?.breakpoint
-}
-
-/**
- * @todo add useBreakpointValue hook
- */
