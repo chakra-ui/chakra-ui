5a5da4787401b65673800f0ce126364822c17321
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-04-03 01:11:37 +0100

    fix: update spinner component stories

--- packages/spinner/src/Spinner.stories.tsx
+++ packages/spinner/src/Spinner.stories.tsx
@@ -5,10 +5,43 @@ export default {
   title: "Spinner",
 }
 
-export const Default = () => (
+/**
+ * A simple spinner.
+ */
+
+export const basic = () => <Spinner color="red.500" />
+
+/**
+ * Pass the `color` prop to change the background color of
+ * the moving section of the spinner.
+ */
+
+export const color = () => <Spinner color="red.500" />
+
+/**
+ * Pass the `size` prop to change the size of the spinner.
+ */
+
+export const size = () => (
   <div>
     {["xl", "lg", "md", "sm", "xs"].map(size => (
       <Spinner key={size} margin={3} color="red.500" size={size} />
     ))}
   </div>
+)
+
+/**
+ * Pass the `speed` prop to change the animation speed of the spinner.
+ */
+
+export const speed = () => (
+  <Spinner color="red.500" emptyColor="gray.200" speed="0.65s" />
+)
+
+/**
+ * Pass the `emptyColor` prop to change the background color of the spinner.
+ */
+
+export const emptyColor = () => (
+  <Spinner color="red.500" emptyColor="gray.200" />
 )
