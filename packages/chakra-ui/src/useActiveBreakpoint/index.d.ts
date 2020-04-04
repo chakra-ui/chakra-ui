afcaf88505147ba8e512ef9cc51f68bdd428cc7d
dmercertaylor <dmercertaylor01@hamline.edu>
2020-04-04 16:35:37 -0500

    fix: updates typescript module export
    updates the typescript export from the previously used interface
    to simply a number, actually functional.

--- packages/chakra-ui/src/useActiveBreakpoint/index.d.ts
+++ packages/chakra-ui/src/useActiveBreakpoint/index.d.ts
@@ -1,11 +1,6 @@
-export interface UseActiveBreakpointReturn {
-    breakpointIndex: number;
-  }
-  
-  /**
-   * useActiveBreakpoint is a hook which returns the index of the currently active theme breakpoint, or -1 if only the baseline is active.
-   */
-  declare function useActiveBreakpoint(): UseActiveBreakpointReturn;
-  
-  export default useActiveBreakpoint;
-  
+/**
+ * useActiveBreakpoint is a hook which returns the index of the currently active theme breakpoint, or -1 if only the baseline is active.
+ */
+declare function useActiveBreakpoint(): number;
+
+export default useActiveBreakpoint;
--- packages/chakra-ui/src/useActiveBreakpoint/utils.js
+++ packages/chakra-ui/src/useActiveBreakpoint/utils.js
@@ -1,19 +1,25 @@
-export function generateMediaQueries(breakpoints){
-    return breakpoints.map(p => window.matchMedia(`screen and (min-width: ${p})`));
+export function generateMediaQueries(breakpoints) {
+  return breakpoints.map(p =>
+    window.matchMedia(`screen and (min-width: ${p})`),
+  );
 }
 
-export function findActiveQueries(mediaQueries){
-    let activeQuery = -1;
-    for(let i = 0; i < mediaQueries.length; i++){
-        if(mediaQueries[i].matches) activeQuery = i;
+export function findActiveQueries(mediaQueries) {
+  let activeQuery = -1;
+  for (let i = 0; i < mediaQueries.length; i++) {
+    if (mediaQueries[i].matches) {
+      activeQuery = i;
+    } else {
+      break;
     }
-    return activeQuery;
+  }
+  return activeQuery;
 }
 
 export function isEqualish(arr1, arr2) {
-    if(arr1.length !== arr2.length) return false;
-    for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] !== arr2[i]) return false;
-    }
-    return true;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
 }
