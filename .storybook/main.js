1ebfd5a700301b1b3cffefcba3408f382e007251
Segun Adebayo <joseshegs@gmail.com>
2020-04-07 17:20:25 +0400

    refactor(avatar): add theming support and clean up code

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require("path")
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
 module.exports = {
-  stories: ["../packages/modal/**/*.stories.tsx"],
+  stories: ["../packages/avatar/**/*.stories.tsx"],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
