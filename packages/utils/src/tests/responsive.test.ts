fd429751cd732ad881e033fd53d6489cd43043f6
Amad <amadxk@gmail.com>
2020-04-08 03:06:31 +0400

    test: add test for util mapResponsive

---
+++ packages/utils/src/tests/responsive.test.ts
@@ -0,0 +1,11 @@
+import { mapResponsive } from "../responsive"
+
+test("mapResponsive - 'prop: [1, 2, 3], mapper: item => (item += 1)' should equal [ 2, 3, 4 ]", () => {
+  expect(mapResponsive([1, 2, 3], item => (item += 1))).toStrictEqual([2, 3, 4])
+})
+
+test("mapResponsive - 'prop: { a: 1, b: 2, c: 3 }, mapper: item => (item += 1)' should equal { a: 2, b: 3, c: 4 }", () => {
+  expect(
+    mapResponsive({ a: 1, b: 2, c: 3 }, item => (item += 1)),
+  ).toStrictEqual({ a: 2, b: 3, c: 4 })
+})
