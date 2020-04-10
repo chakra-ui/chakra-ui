8898ef29e0dd6cc6eb43b2393b8bd283c09264fc
Amad <amadxk@gmail.com>
2020-04-10 14:47:26 +0400

    feat: add tests for dom utls

--- packages/utils/src/tests/dom.test.ts
+++ packages/utils/src/tests/dom.test.ts
@@ -1,3 +1,30 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3)
+import {
+  ariaAttr,
+  dataAttr,
+  getOwnerDocument,
+  getWindow,
+  normalizeEventKey,
+} from "../dom"
+
+test("should get window object", () => {
+  expect(getWindow()).toBe(window)
+})
+
+test("should normalize keyboard events", () => {
+  const keyboardEvent: any = { key: "Left", keyCode: 38 }
+  expect(normalizeEventKey(keyboardEvent)).toBe("ArrowLeft")
+})
+
+test("should return data attribute value from boolean", () => {
+  const isActive = true
+  expect(dataAttr(isActive)).toBe("")
+})
+
+test("should return aria attribute value from boolean", () => {
+  const isDisabled = false
+  expect(ariaAttr(isDisabled)).toBe(undefined)
+})
+
+test("should get document object", () => {
+  expect(getOwnerDocument()).toBe(document)
 })
