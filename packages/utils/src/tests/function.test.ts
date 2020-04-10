bcf42957872ae3752628fabefbec79c1f1b17e1d
Amad <amadxk@gmail.com>
2020-04-10 14:24:22 +0400

    refactor: modify callbackHandler and mapResponsive tests to be realistic

--- packages/utils/src/tests/function.test.ts
+++ packages/utils/src/tests/function.test.ts
@@ -5,13 +5,15 @@ test("should run function if function or else return value", () => {
   expect(runIfFn(2)).toStrictEqual(2)
 })
 
-test("should call passed all functions", () => {
+const event = { target: { value: 1 } }
+
+test("should call all passed functions on event triggered", () => {
   let val1 = 0
   let val2 = 0
-  const func1 = () => val1++
-  const func2 = () => (val2 += 2)
+  const func1 = (event: any) => (val1 = event.target.value + 1)
+  const func2 = (event: any) => (val2 = event.target.value + 2)
 
-  callAllHandlers(func1, func2)({} as any)
-  expect(val1).toStrictEqual(1)
-  expect(val2).toStrictEqual(2)
+  callAllHandlers(func1, func2)(event)
+  expect(val1).toStrictEqual(2)
+  expect(val2).toStrictEqual(3)
 })
