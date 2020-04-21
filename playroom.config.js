4486722fb2e80cd20b5eee057bd5023c33aae677
Segun Adebayo <joseshegs@gmail.com>
2020-04-21 19:34:31 +0400

    chore: update snipper in playroom

--- playroom.config.js
+++ playroom.config.js
@@ -6,8 +6,9 @@ module.exports = {
   frameComponent: "./.playroom/FrameComponent.jsx",
   widths: [320, 768, 1024, 1400],
   exampleCode: `
-    <Box>
-      Hello World!
-    </Box>
+    <Stack direction={["column", "row"]} spacing="6" divider={<StackDivider color="gray.200"/>}>
+      <Box bg="green.500" padding="40px" color="white">Welcome</Box>
+      <Box p="40px" bg="red.100">Welcome</Box>
+    </Stack>
   `,
 }
