150de975690b5e4f8e795a1faeec15520500ca1c
tafelito <tafelito@gmail.com>
2020-04-27 13:58:12 -0400

    fix: validate variantColors
    fixes #644

--- packages/chakra-ui/src/utils/index.js
+++ packages/chakra-ui/src/utils/index.js
@@ -163,18 +163,24 @@ export function useVariantColorWarning(label, variantColor) {
         );
       }
 
-      // if variant color exists and is an object
-      // TODO: Check for the 100 - 900 keys
+      // if variant color exists check if is a string or an object with 50 - 900 keys
       if (variantColorExists) {
         const colorObj = theme.colors[variantColor];
-        const variantColorIsObject =
-          typeof colorObj === "object" && Object.keys(colorObj).length > 0;
-
-        if (!variantColorIsObject) {
-          console.warn(
-            `${label}: The variantColor passed exists in the theme object but is not valid. For a color to be a valid variantColor, it must be an object that has '100' - '900' color values. Use a tool like: 
-        https://smart-swatch.netlify.com/ to generate color values quickly`,
-          );
+        if (typeof colorObj !== "string") {
+          const colorObjKeys = Object.keys(colorObj);
+          const variantKeys = [
+            "50",
+            ...Array.from(new Array(9), (_, i) => `${i * 100 + 100}`),
+          ];
+          if (
+            !colorObjKeys.length ||
+            !colorObjKeys.every((elem) => variantKeys.includes(elem))
+          ) {
+            console.warn(
+              `${label}: The variantColor passed exists in the theme object but is not valid. For a color to be a valid variantColor, it must be an object that has '50', '100' - '900' color values. Use a tool like: 
+              https://smart-swatch.netlify.com/ to generate color values quickly`
+            );
+          }
         }
       }
     }
