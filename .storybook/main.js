b4f8c7f325e15a1e9dc358e89981e85e7e08ccc4
Segun Adebayo <joseshegs@gmail.com>
2020-05-08 03:23:49 +0400

    chore: update tabs components

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require("path")
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
 module.exports = {
-  stories: ["../packages/switch/**/*.stories.tsx"],
+  stories: ["../packages/tabs/**/*.stories.tsx"],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
