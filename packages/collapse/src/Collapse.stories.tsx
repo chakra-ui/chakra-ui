26256cd24661048e8b43eb9614dabd8c951d263d
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-05-11 16:05:17 +0100

    chore: clean up collapse stories

--- packages/collapse/src/Collapse.stories.tsx
+++ packages/collapse/src/Collapse.stories.tsx
@@ -43,24 +43,3 @@ export function WithStringAsChild() {
     </>
   )
 }
-
-function setCookie(name: string, value: any, days?: number) {
-  let expires = ""
-  if (days) {
-    const date = new Date()
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    expires = "; expires=" + date.toUTCString()
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/"
-}
-
-function getCookie(name) {
-  const nameEQ = name + "="
-  const ca = document.cookie.split(";")
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i]
-    while (c.charAt(0) == " ") c = c.substring(1, c.length)
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
-  }
-  return null
-}
