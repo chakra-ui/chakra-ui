a4d08d9822b2fabc24158d4e4b3efa9d6cfcd257
Sage <joseshegs@gmail.com>
2020-05-16 08:47:18 +0400

    docs: add create-react-app ts example

--- examples/create-react-app/src/App.tsx
+++ examples/create-react-app/src/App.tsx
@@ -1,36 +1,30 @@
-import * as React from "react"
+import React from "react"
 import logo from "./logo.svg"
-import "./App.css"
-import { ThemeProvider, Box } from "@chakra-ui/core"
-import styled from "@emotion/styled"
+import { Button, chakra, Badge } from "@chakra-ui/core"
 
-interface Props {
-  as?: string
-}
-
-const SampleBox: React.FC<Props> = styled.div``
-
-const App: React.FC = () => {
+function App() {
   return (
-    <ThemeProvider>
-      <div className="App">
-        {/* <Box as="img" src="welcome.png" /> */}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ThemeProvider>
+    <div>
+      <chakra.header display="flex" flexDir="column" alignItems="center">
+        <chakra.img src={logo} alt="logo" boxSize="200px" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button colorScheme="blue" size="sm">
+          Welcome
+        </Button>
+        <chakra.a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </chakra.a>
+        <Badge colorScheme="red" variant="outline">
+          Welcome home
+        </Badge>
+      </chakra.header>
+    </div>
   )
 }
 
