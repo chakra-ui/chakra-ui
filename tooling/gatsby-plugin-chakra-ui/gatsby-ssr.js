7fe52e4623280ba2e03f2e313a51ab635c93791e
Sage <joseshegs@gmail.com>
2020-05-16 12:28:14 +0400

    chore: update gatsby ssr

--- tooling/gatsby-plugin-chakra-ui/gatsby-ssr.js
+++ tooling/gatsby-plugin-chakra-ui/gatsby-ssr.js
@@ -1,1 +1,6 @@
 export { wrapRootElement } from "./gatsby-browser"
+import { InitializeColorMode } from "@chakra-ui/core"
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([<InitializeColorMode />])
+}
