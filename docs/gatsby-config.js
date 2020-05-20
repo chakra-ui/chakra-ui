a26a61204c8c4825b93f2daf56d155b4c0a0d343
jmiazga <jmiazga@gmail.com>
2020-05-20 19:18:21 -0400

    fix(docs): updated gatsby-plugin-manifest options from starter values

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-default-mdx-basic",
-        short_name: "starter",
+        name: "Chakra UI",
+        short_name: "Chakra UI",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#000000",
+        theme_color: "#319795",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        icon: "../logo/logomark-colored@2x.png", // This path is relative to the root of the site.
       },
     },
     {
