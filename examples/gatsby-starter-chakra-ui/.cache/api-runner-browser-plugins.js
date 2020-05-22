0173cabfe7c798b356c58c2023a35ccf4f30c206
Segun Adebayo <joseshegs@gmail.com>
2020-05-22 20:14:02 +0400

    chore: remove gatsby example and plan to re-add it

--- examples/gatsby-starter-chakra-ui/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/Sage/Documents/CodeProjects/chakra/packages/gatsby-plugin-chakra-ui/gatsby-browser.js'),
-      options: {"plugins":[],"isResettingCSS":true,"isUsingColorMode":true},
-    }]
