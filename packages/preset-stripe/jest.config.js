f399d8d313fc35617e048d4a3ade8762c0a82792
Segun Adebayo <joseshegs@gmail.com>
2020-05-22 19:52:40 +0400

    chore: remove preset-stripe

--- packages/preset-stripe/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}"],
-  transform: { ".(ts|tsx)$": "ts-jest/dist" },
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-}
--- packages/preset-stripe/src/foundations/radius.ts
+++
@@ -1,8 +0,0 @@
-const radii = {
-  md: "4px",
-  circle: "50%",
-}
-
-export type Radii = typeof radii
-
-export default radii
