657103491bc70194fdd62dcf6ef7e16648c2a6c3
Mark Chandler <mark@chanchan.io>
2020-05-23 18:18:58 -0400

    chore(storybook): upgrade to 6.0.0-beta

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,25 +1,18 @@
 import CSSReset from "@chakra-ui/css-reset"
 import theme from "@chakra-ui/theme"
-import {
-  ColorModeProvider,
-  GlobalStyle,
-  ThemeProvider,
-} from "@chakra-ui/system"
+import { ChakraProvider } from "@chakra-ui/core"
 import { addDecorator } from "@storybook/react"
 import * as React from "react"
 
-export const ChakraProvider: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <ColorModeProvider>
-      <GlobalStyle />
-      <CSSReset />
-      {children}
-    </ColorModeProvider>
-  </ThemeProvider>
+export const Chakra: React.FC = ({ children }) => (
+  <ChakraProvider theme={theme}>
+    <CSSReset />
+    {children}
+  </ChakraProvider>
 )
 
 addDecorator((StoryFn: Function) => (
-  <ChakraProvider>
+  <Chakra>
     <StoryFn />
-  </ChakraProvider>
+  </Chakra>
 ))
