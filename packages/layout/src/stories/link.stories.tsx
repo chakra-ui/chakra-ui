e57d7e06ef77d4f4a47e98425dd4684bca6ea3e1
Segun Adebayo <joseshegs@gmail.com>
2020-06-05 23:35:37 +0400

    chore: fix link stories

--- packages/layout/src/stories/link.stories.tsx
+++ packages/layout/src/stories/link.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "../Link"
+import { Link } from ".."
 import { BrowserRouter, Link as ReactRouterLink } from "react-router-dom"
 
 export default {
