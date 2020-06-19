1eb90fa14b9fd3ca3d2a00a47e5007bf12a1a4e1
Segun Adebayo <joseshegs@gmail.com>
2020-06-20 02:40:23 +0400

    docs: update docs

--- website/src/components/theming-guide-image.js
+++ website/src/components/theming-guide-image.js
@@ -35,3 +35,13 @@ export function BadgeSpec() {
     </Box>
   )
 }
+
+export function ButtonSpec() {
+  const data = useData()
+  const badge = getImage(data, "button-spec.png")
+  return (
+    <Box maxWidth="560px" paddingY="40px">
+      <Img fluid={{ ...badge.node.childImageSharp.fluid }} />
+    </Box>
+  )
+}
