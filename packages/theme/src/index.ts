26b9d222e1cc42ef4f11b1ba89613c75b532f7d3
Segun Adebayo <joseshegs@gmail.com>
2020-06-20 14:30:21 +0400

    chore: add color mode config to theme

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,10 +2,19 @@ import foundations from "./foundations"
 import components from "./components"
 import styles from "./styles"
 
+/**
+ * Color mode config
+ */
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "light",
+}
+
 const theme = {
   ...foundations,
   components,
   styles,
+  config,
 }
 
 export type Theme = typeof theme
