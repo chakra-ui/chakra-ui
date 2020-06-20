24a81c5d6c01fbe3340b7e5215db56c208bddb3f
Segun Adebayo <joseshegs@gmail.com>
2020-06-20 11:49:30 +0400

    chore: write function to convert object to array responsive

--- packages/utils/src/responsive.ts
+++ packages/utils/src/responsive.ts
@@ -1,6 +1,7 @@
 import { isArray, isObject } from "./assertion"
 import { Dict } from "./types"
-import { objectKeys } from "./object"
+import { objectKeys, merge } from "./object"
+import { getLastItem } from "./array"
 
 export function mapResponsive(prop: any, mapper: (val: any) => any) {
   if (isArray(prop)) {
@@ -19,4 +20,36 @@ export function mapResponsive(prop: any, mapper: (val: any) => any) {
   }
 
   return null
+}
+
+export function objectToArrayNotation(obj: Dict) {
+  const base = [
+    ["base", null],
+    ["sm", null],
+    ["md", null],
+    ["lg", null],
+  ]
+
+  const entries = merge(base, Object.entries(obj))
+  const mergedObj = Object.fromEntries(entries)
+  let array = Object.values(mergedObj)
+
+  let isNullBetweenValues = false
+
+  array.forEach((item, index) => {
+    const next = array[index + 1]
+    if (item === null && next != null) {
+      isNullBetweenValues = true
+    }
+  })
+
+  if (!isNullBetweenValues) {
+    array = array.filter((item) => item !== null)
+  }
+
+  while (getLastItem(array) === null) {
+    array.pop()
+  }
+
+  return array
 }
--- packages/utils/tests/responsive.test.ts
+++ packages/utils/tests/responsive.test.ts
@@ -1,4 +1,4 @@
-import { mapResponsive } from "../src"
+import { mapResponsive, objectToArrayNotation } from "../src"
 
 const arr = [2, 3]
 const obj = { sm: 2, md: 3 }
@@ -13,4 +13,28 @@ test("should run mapper on array or object and return mapped data", () => {
     sm: "grid-template-columns(2, 1fr )",
     md: "grid-template-columns(3, 1fr )",
   })
+})
+
+test("should convert object to array notation", () => {
+  expect(objectToArrayNotation({ lg: 400, sm: 100, base: 40 })).toEqual([
+    40,
+    100,
+    null,
+    400,
+  ])
+  expect(objectToArrayNotation({ sm: 100 })).toEqual([null, 100])
+  expect(objectToArrayNotation({ md: 100 })).toEqual([null, null, 100])
+  expect(objectToArrayNotation({ base: 100 })).toEqual([100])
+  expect(objectToArrayNotation({ base: 100, lg: 1300 })).toEqual([
+    100,
+    null,
+    null,
+    1300,
+  ])
+  expect(objectToArrayNotation({ base: 100, md: 400 })).toEqual([
+    100,
+    null,
+    400,
+  ])
+  expect(objectToArrayNotation({})).toEqual([])
 })
