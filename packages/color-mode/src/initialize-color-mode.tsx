26f821f4ff2565f7edcf5f809b799b9b97c1effb
Sage <joseshegs@gmail.com>
2020-06-28 20:07:54 +0400

    revert: color mode script till we figure out a better soln

--- packages/color-mode/src/initialize-color-mode.tsx
+++ packages/color-mode/src/initialize-color-mode.tsx
@@ -1,40 +1,10 @@
 import * as React from "react"
 
-const noFlash = `(function() {
-  const key = 'chakra-ui-color-mode';
-  const classNameDark = 'chakra-ui-dark';
-  const classNameLight = 'chakra-ui-light';
-
-  function setClassOnDocumentBody(isDark) {
-    document.body.classList.add(isDark ? classNameDark : classNameLight);
-    document.body.classList.remove(isDark ? classNameLight : classNameDark);
-  }
-
-  const preferDarkQuery = '(prefers-color-scheme: dark)';
-  const mql = window.matchMedia(preferDarkQuery);
-  const supportsColorSchemeQuery = mql.media === preferDarkQuery;
-  let stored = null;
-
-  try {
-    stored = localStorage.getItem(key);
-  } catch (err) {}
-
-  let exist = stored !== null;
-  if (exist) {
-    stored = JSON.parse(stored);
-  }
-
-  if (exist) {
-    const isDark = stored === "dark"
-    setClassOnDocumentBody(isDark);
-  } else if (supportsColorSchemeQuery) {
-    setClassOnDocumentBody(mql.matches);
-    localStorage.setItem(key, mql.matches ? "dark" : "light");
-  } else {
-    let isDark = document.body.classList.contains(classNameDark);
-    localStorage.setItem(key, JSON.stringify(isDark ? "dark": "light"));
-  }
-})();`
+const noFlash = `(function() { try {
+  var mode = localStorage.getItem('chakra-ui-color-mode');
+  if (!mode) return
+  document.body.classList.add('chakra-ui-' + mode);
+} catch (e) {} })();`
 
 /**
  * Script to add to the root of your application to help prevent
