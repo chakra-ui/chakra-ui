deda869f14feea829df2ec9601bc5dd92d1f116b
Sage <joseshegs@gmail.com>
2020-06-30 19:28:40 +0400

    docs: update docs related to pseudobox #941

--- packages/layout/src/box.tsx
+++ packages/layout/src/box.tsx
@@ -8,7 +8,7 @@ export type BoxProps = PropsOf<typeof Box>
  * Box is the most abstract component on top of which other chakra
  * components are built. It renders a `div` element by default.
  *
- * @see Docs https://chakra-ui.com/components/box
+ * @see Docs https://chakra-ui.com/docs/layout/box
  */
 export const Box = chakra.div
 
