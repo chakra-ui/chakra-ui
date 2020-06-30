ab8b4f4f0c0cc21cfa0af119a629bcbfddcd909c
Julien Viala <julien.viala@ekino.com>
2020-06-30 16:14:28 +0200

    fix(stack): default align-items css value

--- packages/layout/src/stack.tsx
+++ packages/layout/src/stack.tsx
@@ -77,7 +77,7 @@ export const Stack = React.forwardRef(function Stack(
 ) {
   const {
     direction = "column",
-    align = "flex-start",
+    align = "normal",
     justify,
     spacing = "0.5rem",
     wrap,
