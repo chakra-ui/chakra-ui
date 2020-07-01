78161b85ce47ecb64e8c7223f656101bd629756d
Sage <joseshegs@gmail.com>
2020-07-01 11:23:23 +0400

    chore: update deps and add target version #955

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -9,6 +9,7 @@ import {
   HStack,
   chakra,
   useColorModeValue,
+  Badge,
 } from "@chakra-ui/core"
 import { DiGithubBadge } from "react-icons/di"
 import { FaMoon, FaSun } from "react-icons/fa"
@@ -64,6 +65,16 @@ const HeaderContent = () => {
         >
           <Logo />
         </chakra.a>
+        <Badge
+          px="1"
+          ml="3"
+          fontFamily="mono"
+          borderRadius="md"
+          colorScheme="teal"
+          variant="solid"
+        >
+          v1
+        </Badge>
         <HStack
           as="nav"
           spacing="4"
