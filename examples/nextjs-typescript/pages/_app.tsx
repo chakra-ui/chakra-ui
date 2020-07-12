8cb31138206806852025a20dae8f6aff3ce66e3c
Mark Chandler <mark@chanchan.io>
2020-07-12 13:53:11 -0400

    chore(next-ts): update package imports

--- examples/nextjs-typescript/pages/_app.tsx
+++ examples/nextjs-typescript/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react"
-import { ChakraProvider } from "@chakra-ui/core"
+import { ChakraProvider, CSSReset } from "@chakra-ui/core"
 import theme from "@chakra-ui/theme"
-import CSSReset from "@chakra-ui/css-reset"
 
 function App(props: any) {
   const { Component, pageProps } = props
