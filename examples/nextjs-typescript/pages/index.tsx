ddc1d345dfdef56639812cc22f02be1d8016d282
James Gee <geemanjs@gmail.com>
2020-07-13 20:27:13 +0100

    feat: issue 1191 update nextjs typescript project

--- examples/nextjs-typescript/pages/index.tsx
+++ examples/nextjs-typescript/pages/index.tsx
@@ -1,30 +1,36 @@
-import Link from "next/link"
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  Button,
-} from "@chakra-ui/core"
-import Layout from "../components/Layout"
+import { Box, Text, Link, VStack, Code, Grid } from "@chakra-ui/core"
+import { ColorModeSwitcher } from "../components/ColorModeSwitcher"
+import { Layout } from "../components/Layout"
+import { Logo } from "../components/Logo"
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Accordion allowToggle>
-        <AccordionItem>
-          <AccordionButton>Accordion 1</AccordionButton>
-          <AccordionPanel>Welcome home</AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-      <Button colorScheme="red" marginTop="40px">
-        Welcome
-      </Button>
-    </p>
+  <Layout title="Next.js + TypeScript example">
+    <Box textAlign="center" fontSize="xl">
+      <Grid
+        minH="100vh"
+        p={3}
+        direction="column"
+        align="center"
+        justify="center"
+      >
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">pages/index.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
   </Layout>
 )
 
