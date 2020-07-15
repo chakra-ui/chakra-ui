f3ee5f15c48da242c4d4d43de0dc67ff7664c81e
Sage <joseshegs@gmail.com>
2020-07-15 21:41:48 +0400

    revert: breakpoint handling

--- packages/theme/src/foundations/breakpoints.ts
+++ packages/theme/src/foundations/breakpoints.ts
@@ -1,2 +1,8 @@
-const breakpoints = { sm: "30em", md: "48em", lg: "62em", xl: "80em" }
+const breakpoints: any = ["30em", "48em", "62em", "80em"]
+
+breakpoints.sm = breakpoints[0]
+breakpoints.md = breakpoints[1]
+breakpoints.lg = breakpoints[2]
+breakpoints.xl = breakpoints[3]
+
 export default breakpoints
