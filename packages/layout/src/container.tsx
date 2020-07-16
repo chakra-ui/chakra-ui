50cdc614fe4c369b06ccc5e448fbbcd8e460083e
Sage <joseshegs@gmail.com>
2020-07-16 05:29:23 +0400

    docs: update container docs

--- packages/layout/src/container.tsx
+++ packages/layout/src/container.tsx
@@ -1,17 +1,14 @@
-import { chakra, PropsOf, useTheme } from "@chakra-ui/system"
+import { chakra, PropsOf, useTheme, SystemStyleObject } from "@chakra-ui/system"
 import { cx, Dict, get, mapResponsive, __DEV__ } from "@chakra-ui/utils"
 import * as React from "react"
 
-export type ContainerProps = PropsOf<typeof chakra.div>
-
-const StyledContainer = chakra("div", {
-  baseStyle: {
-    width: "100%",
-    marginX: "auto",
-    maxWidth: "60ch",
-    paddingX: "1rem",
-  },
-})
+export type ContainerProps = PropsOf<typeof chakra.div> & {
+  /**
+   * If `true`, container will center it's children
+   * regardless of their width.
+   */
+  centerContent?: boolean
+}
 
 function transform(theme: Dict, props: Dict) {
   const result = {} as Dict
@@ -38,7 +35,17 @@ export const Container = React.forwardRef(function Container(
   props: ContainerProps,
   ref: React.Ref<any>,
 ) {
-  const { maxWidth, width, minWidth, w, minW, maxW, className, ...rest } = props
+  const {
+    maxWidth,
+    width,
+    minWidth,
+    w,
+    minW,
+    maxW,
+    className,
+    centerContent,
+    ...rest
+  } = props
 
   const theme = useTheme()
 
@@ -53,12 +60,25 @@ export const Container = React.forwardRef(function Container(
 
   const _className = cx("chakra-container", className)
 
+  const styles: SystemStyleObject = {
+    width: "100%",
+    marginX: "auto",
+    maxWidth: "60ch",
+    paddingX: "1rem",
+    ...(centerContent && {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }),
+  }
+
   return (
-    <StyledContainer
+    <chakra.div
       className={_className}
       ref={ref}
       {...widthProps}
       {...rest}
+      __css={styles}
     />
   )
 })
