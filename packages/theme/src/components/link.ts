b66a3bcb59eec1b6d1944b6ca6925ce48bce3994
Peng Jie <im@jiepeng.me>
2020-07-20 15:39:57 +0800

    fix: remove boxshadow when disabled

--- packages/theme/src/components/link.ts
+++ packages/theme/src/components/link.ts
@@ -4,28 +4,30 @@ const register = {
   parts: ["link"],
 } as const
 
-const baseStyle: BaseStyle<typeof register> = {
-  link: {
-    transition: `all 0.15s ease-out`,
-    cursor: "pointer",
-    textDecoration: "none",
-    outline: "none",
-    color: "inherit",
-    _hover: {
-      textDecoration: "underline",
-    },
-    _focus: {
-      boxShadow: "outline",
-    },
-    _disabled: {
-      opacity: 0.4,
-      cursor: "not-allowed",
+const baseStyle: BaseStyle<typeof register> = (props) => {
+  return {
+    link: {
+      transition: `all 0.15s ease-out`,
+      cursor: "pointer",
       textDecoration: "none",
+      outline: "none",
+      color: "inherit",
+      _hover: {
+        textDecoration: "underline",
+      },
+      _focus: {
+        boxShadow: props.isDisabled ? "none" : "outline",
+      },
+      _disabled: {
+        opacity: 0.4,
+        cursor: "not-allowed",
+        textDecoration: "none",
+      },
+      "_disabled:active": {
+        pointerEvents: "none",
+      },
     },
-    "_disabled:active": {
-      pointerEvents: "none",
-    },
-  },
+  }
 }
 
 const link = {
