a1ac7890d2cdb485f96324d4468a9ec417c835bc
Mark Chandler <mark@chanchan.io>
2020-07-25 17:45:11 -0400

    chore(storybook): add color mode toggle button to stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,43 @@
 import CSSReset from "@chakra-ui/css-reset"
 import theme from "@chakra-ui/theme"
-import { ChakraProvider } from "@chakra-ui/core"
+import {
+  ChakraProvider,
+  useColorMode,
+  useColorModeValue,
+  Flex,
+  IconButton,
+} from "@chakra-ui/core"
+import { FaMoon, FaSun } from "react-icons/fa"
 import { addDecorator } from "@storybook/react"
 import * as React from "react"
 import { withPerformance } from "storybook-addon-performance"
 import { withA11y } from "@storybook/addon-a11y"
 
+const ColorModeToggleBar = () => {
+  const { toggleColorMode } = useColorMode()
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+  const nextMode = useColorModeValue("dark", "light")
+
+  return (
+    <Flex justify="flex-end" mb={4}>
+      <IconButton
+        size="md"
+        fontSize="lg"
+        aria-label={`Switch to ${nextMode} mode`}
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+      />
+    </Flex>
+  )
+}
+
 const withChakra = (StoryFn: Function) => (
   <ChakraProvider theme={theme}>
     <CSSReset />
+    <ColorModeToggleBar />
     <StoryFn />
   </ChakraProvider>
 )
