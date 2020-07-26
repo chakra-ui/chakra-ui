f36a747007451204d4895f48d0d22e5afd393d45
Sage <joseshegs@gmail.com>
2020-07-26 14:32:00 +0400

    fix: select option issue in dark mode

--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -1,4 +1,4 @@
-import { multiStyleConfig } from "@chakra-ui/theme-tools"
+import { multiStyleConfig, mode } from "@chakra-ui/theme-tools"
 import input from "./input"
 
 const select = multiStyleConfig({
@@ -6,19 +6,22 @@ const select = multiStyleConfig({
     field: "the select field itself",
     icon: "the select field icon",
   },
-  baseStyle: {
+  baseStyle: (props) => ({
     field: {
       ...input.baseStyle?.field,
       appearance: "none",
       paddingBottom: "1px",
       lineHeight: "normal",
+      "> option": {
+        bg: mode("white", "gray.700")(props),
+      },
     },
     icon: {
       color: "currentColor",
       fontSize: "1.25rem",
       _disabled: { opacity: 0.5 },
     },
-  },
+  }),
 
   sizes: input.sizes,
   variants: input.variants,
