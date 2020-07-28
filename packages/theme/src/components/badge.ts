1070a63b4f0adef821d712904995e442356da1f3
Héricles <hericles.me@gmail.com>
2020-07-28 15:58:58 -0300

    feat: split and export badge variants

--- packages/theme/src/components/badge.ts
+++ packages/theme/src/components/badge.ts
@@ -5,48 +5,68 @@ import {
   transparentize,
 } from "@chakra-ui/theme-tools"
 
+const baseStyle = {
+  paddingX: 1,
+  textTransform: "uppercase",
+  fontSize: "xs",
+  borderRadius: "sm",
+  fontWeight: "bold",
+}
+
+const variantSolid = function (props: Record<string, any>) {
+  const { colorScheme: c, theme } = props
+  const dark = transparentize(`${c}.500`, 0.6)(theme)
+  return {
+    bg: mode(`${c}.500`, dark)(props),
+    color: mode(`white`, `whiteAlpha.800`)(props),
+  }
+}
+
+const variantSubtle = function (props: Record<string, any>) {
+  const { colorScheme: c, theme } = props
+  const darkBg = transparentize(`${c}.200`, 0.16)(theme)
+  return {
+    bg: mode(`${c}.100`, darkBg)(props),
+    color: mode(`${c}.800`, `${c}.200`)(props),
+  }
+}
+
+const variantOutline = function (props: Record<string, any>) {
+  const { colorScheme: c, theme } = props
+  const darkColor = transparentize(`${c}.200`, 0.8)(theme)
+  const lightColor = getColor(theme, `${c}.500`)
+  const color = mode(lightColor, darkColor)(props)
+
+  return {
+    color,
+    boxShadow: `inset 0 0 0px 1px ${color}`,
+  }
+}
+
+const variants = {
+  solid: variantSolid,
+  subtle: variantSubtle,
+  outline: variantOutline,
+}
+
+const defaultProps = {
+  variant: "subtle",
+  colorScheme: "gray",
+}
+
 const badge = styleConfig({
-  baseStyle: {
-    paddingX: 1,
-    textTransform: "uppercase",
-    fontSize: "xs",
-    borderRadius: "sm",
-    fontWeight: "bold",
-  },
-  variants: {
-    solid: function (props) {
-      const { colorScheme: c, theme } = props
-      const dark = transparentize(`${c}.500`, 0.6)(theme)
-      return {
-        bg: mode(`${c}.500`, dark)(props),
-        color: mode(`white`, `whiteAlpha.800`)(props),
-      }
-    },
-    subtle: function (props) {
-      const { colorScheme: c, theme } = props
-      const darkBg = transparentize(`${c}.200`, 0.16)(theme)
-      return {
-        bg: mode(`${c}.100`, darkBg)(props),
-        color: mode(`${c}.800`, `${c}.200`)(props),
-      }
-    },
-    outline: function (props) {
-      const { colorScheme: c, theme } = props
-      const darkColor = transparentize(`${c}.200`, 0.8)(theme)
-      const lightColor = getColor(theme, `${c}.500`)
-      const color = mode(lightColor, darkColor)(props)
-
-      return {
-        color,
-        boxShadow: `inset 0 0 0px 1px ${color}`,
-      }
-    },
-  },
-
-  defaultProps: {
-    variant: "subtle",
-    colorScheme: "gray",
-  },
+  baseStyle,
+  variants,
+  // @ts-ignore
+  defaultProps,
 })
+
+export const badgeStyles = {
+  baseStyle,
+  variantSolid,
+  variantSubtle,
+  variantOutline,
+  defaultProps,
+}
 
 export default badge
