6569cc55548e80eb078ca8e7f60ec1908d0b5b06
Héricles <hericles.me@gmail.com>
2020-07-28 16:02:35 -0300

    feat: split and export checkbox baseStyle

--- packages/theme/src/components/checkbox.ts
+++ packages/theme/src/components/checkbox.ts
@@ -1,90 +1,102 @@
 import { mode, multiStyleConfig } from "@chakra-ui/theme-tools"
 
+const parts = {
+  control: "the main checkbox container",
+  label: "the checkbox label",
+  description: "the checkbox assistive text",
+  icon: "the checkmark icon",
+}
+
+const baseStyleControl = function (props: any) {
+  const { colorScheme: c } = props
+
+  return {
+    w: "100%",
+    transition: "box-shadow 250ms",
+    border: "2px solid",
+    borderRadius: "sm",
+    borderColor: "inherit",
+    color: "white",
+
+    _checked: {
+      bg: mode(`${c}.500`, `${c}.200`)(props),
+      borderColor: mode(`${c}.500`, `${c}.200`)(props),
+      color: mode("white", "gray.900")(props),
+
+      _hover: {
+        bg: mode(`${c}.600`, `${c}.300`)(props),
+        borderColor: mode(`${c}.600`, `${c}.300`)(props),
+      },
+
+      _disabled: {
+        borderColor: mode("gray.200", "transparent")(props),
+        bg: mode("gray.200", "whiteAlpha.300")(props),
+        color: mode("gray.500", "whiteAlpha.500")(props),
+      },
+    },
+
+    _indeterminate: {
+      bg: mode(`${c}.500`, `${c}.200`)(props),
+      borderColor: mode(`${c}.500`, `${c}.200`)(props),
+      color: mode("white", "gray.900")(props),
+    },
+
+    _disabled: {
+      bg: mode("gray.100", "whiteAlpha.100")(props),
+      borderColor: mode("gray.100", "transparent")(props),
+    },
+
+    _focus: {
+      boxShadow: "outline",
+    },
+
+    _invalid: {
+      borderColor: mode("red.500", "red.300")(props),
+    },
+  }
+}
+
+const baseStyleLabel = {
+  userSelect: "none",
+  _disabled: { opacity: 0.4 },
+}
+
+const baseStyleIcon = {
+  fontSize: "0.625rem",
+}
+
+const baseStyle = (props: any) => ({
+  control: baseStyleControl(props),
+  label: baseStyleLabel,
+  icon: baseStyleIcon,
+})
+
+const sizes = {
+  sm: {
+    control: { h: 3, w: 3 },
+    label: { fontSize: "sm" },
+  },
+  md: {
+    control: { w: 4, h: 4 },
+    label: { fontSize: "md" },
+  },
+  lg: {
+    control: { w: 5, h: 5 },
+    label: { fontSize: "lg" },
+  },
+}
+
+const defaultProps = {
+  size: "md",
+  colorScheme: "blue",
+}
+
 const checkbox = multiStyleConfig({
-  parts: {
-    control: "the main checkbox container",
-    label: "the checkbox label",
-    description: "the checkbox assistive text",
-    icon: "the checkmark icon",
-  },
-  baseStyle: function (props) {
-    const { colorScheme: c } = props
-    return {
-      control: {
-        w: "100%",
-        transition: "box-shadow 250ms",
-        border: "2px solid",
-        borderRadius: "sm",
-        borderColor: "inherit",
-        color: "white",
-
-        _checked: {
-          bg: mode(`${c}.500`, `${c}.200`)(props),
-          borderColor: mode(`${c}.500`, `${c}.200`)(props),
-          color: mode("white", "gray.900")(props),
-
-          _hover: {
-            bg: mode(`${c}.600`, `${c}.300`)(props),
-            borderColor: mode(`${c}.600`, `${c}.300`)(props),
-          },
-
-          _disabled: {
-            borderColor: mode("gray.200", "transparent")(props),
-            bg: mode("gray.200", "whiteAlpha.300")(props),
-            color: mode("gray.500", "whiteAlpha.500")(props),
-          },
-        },
-
-        _indeterminate: {
-          bg: mode(`${c}.500`, `${c}.200`)(props),
-          borderColor: mode(`${c}.500`, `${c}.200`)(props),
-          color: mode("white", "gray.900")(props),
-        },
-
-        _disabled: {
-          bg: mode("gray.100", "whiteAlpha.100")(props),
-          borderColor: mode("gray.100", "transparent")(props),
-        },
-
-        _focus: {
-          boxShadow: "outline",
-        },
-
-        _invalid: {
-          borderColor: mode("red.500", "red.300")(props),
-        },
-      },
-
-      label: {
-        userSelect: "none",
-        _disabled: { opacity: 0.4 },
-      },
-
-      icon: {
-        fontSize: "0.625rem",
-      },
-    }
-  },
-
-  sizes: {
-    sm: {
-      control: { h: 3, w: 3 },
-      label: { fontSize: "sm" },
-    },
-    md: {
-      control: { w: 4, h: 4 },
-      label: { fontSize: "md" },
-    },
-    lg: {
-      control: { w: 5, h: 5 },
-      label: { fontSize: "lg" },
-    },
-  },
-
-  defaultProps: {
-    size: "md",
-    colorScheme: "blue",
-  },
+  parts,
+  baseStyle,
+  sizes,
+  // @ts-ignore
+  defaultProps,
 })
 
 export default checkbox
