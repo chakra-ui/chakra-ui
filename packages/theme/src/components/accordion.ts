44dfa911a61560572df640896de8714c2ceee255
Héricles <hericles.me@gmail.com>
2020-07-28 14:43:27 -0300

    feat: split and export accordion baseStyle

--- packages/theme/src/components/accordion.ts
+++ packages/theme/src/components/accordion.ts
@@ -1,40 +1,57 @@
 import { multiStyleConfig } from "@chakra-ui/theme-tools"
 
+const parts = {
+  container: "the container for an accordion item",
+  button: "the trigger for an accordion item",
+  panel: "the panel for an accordion item",
+}
+
+const baseStyleContainer = {
+  borderTopWidth: "1px",
+  borderColor: "inherit",
+  _last: {
+    borderBottomWidth: "1px",
+  },
+}
+
+const baseStyleButton = {
+  fontSize: "1rem",
+  _focus: {
+    boxShadow: "outline",
+  },
+  _hover: {
+    bg: "blackAlpha.50",
+  },
+  _disabled: {
+    opacity: 0.4,
+    cursor: "not-allowed",
+  },
+  px: 4,
+  py: 2,
+}
+
+const baseStylePanel = {
+  pt: 2,
+  px: 4,
+  pb: 5,
+}
+
+const baseStyle = {
+  container: baseStyleContainer,
+  button: baseStyleButton,
+  panel: baseStylePanel,
+}
+
 const accordion = multiStyleConfig({
-  parts: {
-    container: "the container for an accordion item",
-    button: "the trigger for an accordion item",
-    panel: "the panel for an accordion item",
-  },
-  baseStyle: {
-    container: {
-      borderTopWidth: "1px",
-      borderColor: "inherit",
-      _last: {
-        borderBottomWidth: "1px",
-      },
-    },
-    button: {
-      fontSize: "1rem",
-      _focus: {
-        boxShadow: "outline",
-      },
-      _hover: {
-        bg: "blackAlpha.50",
-      },
-      _disabled: {
-        opacity: 0.4,
-        cursor: "not-allowed",
-      },
-      px: 4,
-      py: 2,
-    },
-    panel: {
-      pt: 2,
-      px: 4,
-      pb: 5,
-    },
-  },
+  parts,
+  baseStyle,
 })
+
+export const accordionStyles = {
+  parts,
+  baseStyleContainer,
+  baseStyleButton,
+  baseStylePanel,
+}
 
 export default accordion
