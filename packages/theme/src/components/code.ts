a9300a84b6c684f83e7872eff1dbaf888cf797ea
Héricles <hericles.me@gmail.com>
2020-07-28 16:15:16 -0300

    feat: export code baseStyle and variants

--- packages/theme/src/components/code.ts
+++ packages/theme/src/components/code.ts
@@ -1,16 +1,39 @@
 import { styleConfig } from "@chakra-ui/theme-tools"
-import badge from "./badge"
+import { badgeStyles } from "./badge"
+
+const {
+  defaultProps,
+  variantSolid,
+  variantSubtle,
+  variantOutline,
+} = badgeStyles
+
+const baseStyle = {
+  fontFamily: "mono",
+  fontSize: "sm",
+  px: "0.2em",
+  borderRadius: "sm",
+}
+
+const variants = {
+  solid: variantSolid,
+  subtle: variantSubtle,
+  outline: variantOutline,
+}
 
 const code = styleConfig({
-  baseStyle: {
-    fontFamily: "mono",
-    fontSize: "sm",
-    px: "0.2em",
-    borderRadius: "sm",
-  },
-  variants: badge.variants,
-  sizes: badge.sizes,
-  defaultProps: badge.defaultProps,
+  baseStyle,
+  variants,
+  // @ts-ignore
+  defaultProps,
 })
+
+export const codeStyles = {
+  baseStyle,
+  variantSolid,
+  variantSubtle,
+  variantOutline,
+  defaultProps,
+}
 
 export default code
