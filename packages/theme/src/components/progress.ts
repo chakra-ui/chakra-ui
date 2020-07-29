88e8b595f09c4523473e5cf26e43a1c216db5ed3
Héricles <hericles.me@gmail.com>
2020-07-29 10:53:23 -0300

    feat: split and export progress baseStyle

--- packages/theme/src/components/progress.ts
+++ packages/theme/src/components/progress.ts
@@ -5,52 +5,6 @@ import {
   multiStyleConfig,
 } from "@chakra-ui/theme-tools"
 
-const progress = multiStyleConfig({
-  parts: {
-    track: "the linear progress track",
-    filledTrack: "the inner filled track",
-    label: "the value indicator or label",
-  },
-
-  baseStyle: function (props) {
-    return {
-      label: {
-        lineHeight: "1",
-        fontSize: "0.25em",
-        fontWeight: "bold",
-        color: "white",
-      },
-      track: {
-        bg: mode(`gray.100`, `whiteAlpha.300`)(props),
-      },
-      filledTrack: {
-        transition: "all 0.3s",
-        ...filledStyle(props),
-      },
-    }
-  },
-
-  sizes: {
-    xs: {
-      track: { h: "0.25rem" },
-    },
-    sm: {
-      track: { h: "0.5rem" },
-    },
-    md: {
-      track: { h: "0.75rem" },
-    },
-    lg: {
-      track: { h: "1rem" },
-    },
-  },
-
-  defaultProps: {
-    size: "md",
-    colorScheme: "blue",
-  },
-})
-
 function filledStyle(props: Record<string, any>) {
   const { colorScheme: c, theme: t, isIndeterminate, hasStripe } = props
 
@@ -74,6 +28,78 @@ function filledStyle(props: Record<string, any>) {
     ...(addStripe && stripeStyle),
     bgColor: isIndeterminate ? gradient : bg,
   }
+}
+
+const parts = {
+  track: "the linear progress track",
+  filledTrack: "the inner filled track",
+  label: "the value indicator or label",
+}
+
+const baseStyleLabel = {
+  lineHeight: "1",
+  fontSize: "0.25em",
+  fontWeight: "bold",
+  color: "white",
+}
+
+// @ts-ignore
+const baseStyleTrack = function (props) {
+  return {
+    bg: mode(`gray.100`, `whiteAlpha.300`)(props),
+  }
+}
+
+// @ts-ignore
+const baseStyleFilledTrack = function (props) {
+  return {
+    transition: "all 0.3s",
+    ...filledStyle(props),
+  }
+}
+
+// @ts-ignore
+const baseStyle = function (props) {
+  return {
+    label: baseStyleLabel,
+  }
+}
+
+const sizes = {
+  xs: {
+    track: { h: "0.25rem" },
+  },
+  sm: {
+    track: { h: "0.5rem" },
+  },
+  md: {
+    track: { h: "0.75rem" },
+  },
+  lg: {
+    track: { h: "1rem" },
+  },
+}
+
+const defaultProps = {
+  size: "md",
+  colorScheme: "blue",
+}
+
+const progress = multiStyleConfig({
+  parts,
+  baseStyle,
+  sizes,
+  // @ts-ignore
+  defaultProps,
+})
+
+export const progressStyles = {
+  parts,
+  sizes,
+  baseStyleLabel,
+  baseStyleTrack,
+  baseStyleFilledTrack,
+  defaultProps,
 }
 
 export default progress
