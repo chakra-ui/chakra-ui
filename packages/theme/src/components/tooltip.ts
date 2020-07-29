526f830f54535d780615f9103bc9462e0e04c278
Héricles <hericles.me@gmail.com>
2020-07-29 12:12:20 -0300

    feat: split and export tooltip baseStyle

--- packages/theme/src/components/tooltip.ts
+++ packages/theme/src/components/tooltip.ts
@@ -1,20 +1,27 @@
 import { mode, styleConfig } from "@chakra-ui/theme-tools"
 
+// @ts-ignore
+const baseStyle = function (props) {
+  return {
+    px: "8px",
+    py: "2px",
+    bg: mode("gray.700", "gray.300")(props),
+    color: mode("whiteAlpha.900", "gray.900")(props),
+    borderRadius: "sm",
+    fontWeight: "medium",
+    pointerEvents: "none",
+    fontSize: "sm",
+    boxShadow: "md",
+    maxW: "320px",
+  }
+}
+
 const tooltip = styleConfig({
-  baseStyle: function (props) {
-    return {
-      px: "8px",
-      py: "2px",
-      bg: mode("gray.700", "gray.300")(props),
-      color: mode("whiteAlpha.900", "gray.900")(props),
-      borderRadius: "sm",
-      fontWeight: "medium",
-      pointerEvents: "none",
-      fontSize: "sm",
-      boxShadow: "md",
-      maxW: "320px",
-    }
-  },
+  baseStyle,
 })
+
+export const tooltipStyles = {
+  baseStyle,
+}
 
 export default tooltip
