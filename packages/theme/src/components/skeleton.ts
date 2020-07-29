d27d272cb2db6a2419b7f23fb766e67ae0c5c4a1
Héricles <hericles.me@gmail.com>
2020-07-29 11:27:41 -0300

    feat: split and export skeleton baseStyle

--- packages/theme/src/components/skeleton.ts
+++ packages/theme/src/components/skeleton.ts
@@ -1,36 +1,43 @@
 import { keyframes } from "@chakra-ui/system"
 import { getColor, mode, styleConfig } from "@chakra-ui/theme-tools"
 
-const skeleton = styleConfig({
-  baseStyle: function (props) {
-    const defaultStartColor = mode("gray.100", "gray.800")(props)
-    const defaultEndColor = mode("gray.400", "gray.600")(props)
-
-    const {
-      startColor = defaultStartColor,
-      endColor = defaultEndColor,
-      speed,
-      theme,
-    } = props
-
-    const start = getColor(theme, startColor)
-    const end = getColor(theme, endColor)
-
-    return {
-      opacity: 0.7,
-      borderRadius: "2px",
-      borderColor: start,
-      background: end,
-      animation: `${speed}s linear infinite alternate ${fade(start, end)}`,
-    }
-  },
-})
-
-export default skeleton
-
 export function fade(startColor: string, endColor: string) {
   return keyframes({
     from: { borderColor: startColor, background: startColor },
     to: { borderColor: endColor, background: endColor },
   })
 }
+
+// @ts-ignore
+const baseStyle = function (props) {
+  const defaultStartColor = mode("gray.100", "gray.800")(props)
+  const defaultEndColor = mode("gray.400", "gray.600")(props)
+
+  const {
+    startColor = defaultStartColor,
+    endColor = defaultEndColor,
+    speed,
+    theme,
+  } = props
+
+  const start = getColor(theme, startColor)
+  const end = getColor(theme, endColor)
+
+  return {
+    opacity: 0.7,
+    borderRadius: "2px",
+    borderColor: start,
+    background: end,
+    animation: `${speed}s linear infinite alternate ${fade(start, end)}`,
+  }
+}
+
+const skeleton = styleConfig({
+  baseStyle,
+})
+
+export const skeletonStyles = {
+  baseStyle,
+}
+
+export default skeleton
