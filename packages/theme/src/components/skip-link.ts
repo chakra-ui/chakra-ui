dc3067211e6b90cb76153dc2342e7cb28193ffc2
Héricles <hericles.me@gmail.com>
2020-07-29 11:28:55 -0300

    feat: split and export skip-link baseStyle

--- packages/theme/src/components/skip-link.ts
+++ packages/theme/src/components/skip-link.ts
@@ -1,20 +1,27 @@
 import { mode, styleConfig } from "@chakra-ui/theme-tools"
 
+// @ts-ignore
+const baseStyle = function (props) {
+  return {
+    borderRadius: "md",
+    fontWeight: "semibold",
+    _focus: {
+      boxShadow: "outline",
+      padding: "1rem",
+      position: "fixed",
+      top: "1.5rem",
+      left: "1.5rem",
+      bg: mode("white", "gray.700")(props),
+    },
+  }
+}
+
 const skipLink = styleConfig({
-  baseStyle: function (props) {
-    return {
-      borderRadius: "md",
-      fontWeight: "semibold",
-      _focus: {
-        boxShadow: "outline",
-        padding: "1rem",
-        position: "fixed",
-        top: "1.5rem",
-        left: "1.5rem",
-        bg: mode("white", "gray.700")(props),
-      },
-    }
-  },
+  baseStyle,
 })
+
+export const skipLinkStyles = {
+  baseStyle,
+}
 
 export default skipLink
