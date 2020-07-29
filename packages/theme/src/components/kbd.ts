8bcc810b91dd04c453199cf87e12856e86843816
Héricles <hericles.me@gmail.com>
2020-07-29 09:48:30 -0300

    feat: split and export kbd baseStyle

--- packages/theme/src/components/kbd.ts
+++ packages/theme/src/components/kbd.ts
@@ -1,19 +1,25 @@
 import { mode, styleConfig } from "@chakra-ui/theme-tools"
 
+const baseStyle = function (props: any) {
+  return {
+    bg: mode("gray.100", "whiteAlpha")(props),
+    borderRadius: "md",
+    borderWidth: "1px",
+    borderBottomWidth: "3px",
+    fontSize: "0.8em",
+    fontWeight: "bold",
+    lineHeight: "normal",
+    px: "0.4em",
+    whiteSpace: "nowrap",
+  }
+}
+
 const kbd = styleConfig({
-  baseStyle: function (props) {
-    return {
-      bg: mode("gray.100", "whiteAlpha")(props),
-      borderRadius: "md",
-      borderWidth: "1px",
-      borderBottomWidth: "3px",
-      fontSize: "0.8em",
-      fontWeight: "bold",
-      lineHeight: "normal",
-      px: "0.4em",
-      whiteSpace: "nowrap",
-    }
-  },
+  baseStyle,
 })
+
+export const kbdStyles = {
+  baseStyle,
+}
 
 export default kbd
