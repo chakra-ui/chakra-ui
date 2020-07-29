08484c2e8e246621200b86111d20f8e321f0368f
Héricles <hericles.me@gmail.com>
2020-07-29 10:49:13 -0300

    feat: split and export popover baseStyle

--- packages/theme/src/components/popover.ts
+++ packages/theme/src/components/popover.ts
@@ -1,45 +1,68 @@
 import { mode, multiStyleConfig } from "@chakra-ui/theme-tools"
 
-const popover = multiStyleConfig({
-  parts: {
-    content: "the popover's content wrapper",
-    header: "the popover heading",
-    body: "the popover main content",
-    footer: "the action footers for popover",
-  },
+const parts = {
+  content: "the popover's content wrapper",
+  header: "the popover heading",
+  body: "the popover main content",
+  footer: "the action footers for popover",
+}
 
-  baseStyle: function (props) {
-    return {
-      content: {
-        bg: mode("white", "gray.700")(props),
-        border: "1px solid",
-        borderColor: "inherit",
-        borderRadius: "md",
-        boxShadow: "sm",
-        w: "100%",
-        maxW: "xs",
-        zIndex: "1",
-        _focus: {
-          outline: 0,
-          boxShadow: "outline",
-        },
-      },
-      header: {
-        px: 3,
-        py: 2,
-        borderBottomWidth: "1px",
-      },
-      body: {
-        px: 3,
-        py: 2,
-      },
-      footer: {
-        px: 3,
-        py: 2,
-        borderTopWidth: "1px",
-      },
-    }
-  },
+// @ts-ignore
+const baseStyleContent = function (props) {
+  return {
+    bg: mode("white", "gray.700")(props),
+    border: "1px solid",
+    borderColor: "inherit",
+    borderRadius: "md",
+    boxShadow: "sm",
+    w: "100%",
+    maxW: "xs",
+    zIndex: "1",
+    _focus: {
+      outline: 0,
+      boxShadow: "outline",
+    },
+  }
+}
+
+const baseStyleHeader = {
+  px: 3,
+  py: 2,
+  borderBottomWidth: "1px",
+}
+
+const baseStyleBody = {
+  px: 3,
+  py: 2,
+}
+
+const baseStyleFooter = {
+  px: 3,
+  py: 2,
+  borderTopWidth: "1px",
+}
+
+// @ts-ignore
+const baseStyle = function (props) {
+  return {
+    content: baseStyleContent(props),
+    header: baseStyleHeader,
+    body: baseStyleBody,
+    footer: baseStyleFooter,
+  }
+}
+
+const popover = multiStyleConfig({
+  parts,
+  baseStyle,
 })
+
+export const popoverStyles = {
+  parts,
+  baseStyleContent,
+  baseStyleHeader,
+  baseStyleFooter,
+  baseStyleBody,
+}
 
 export default popover
