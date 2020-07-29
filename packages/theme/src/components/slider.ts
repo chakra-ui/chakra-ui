f054d69594e6fc3122d565fa2d39e8917db6c5e4
Héricles <hericles.me@gmail.com>
2020-07-29 11:36:26 -0300

    feat: split and export slider baseStyle

--- packages/theme/src/components/slider.ts
+++ packages/theme/src/components/slider.ts
@@ -1,95 +1,5 @@
 import { mode, orient, multiStyleConfig } from "@chakra-ui/theme-tools"
 
-const slider = multiStyleConfig({
-  parts: {
-    container: "the slider wrapper",
-    thumb: "the slider thumb or handle",
-    track: "the slider outer track",
-    filledTrack: "the slider inner/filled track",
-  },
-
-  baseStyle: function (props) {
-    const { orientation, colorScheme: c } = props
-
-    return {
-      container: {
-        _disabled: {
-          opacity: 0.6,
-          cursor: "default",
-          pointerEvents: "none",
-        },
-        ...orient({
-          orientation,
-          vertical: { h: "100%" },
-          horizontal: { w: "100%" },
-        }),
-      },
-      track: {
-        borderRadius: "sm",
-        bg: mode("gray.200", "whiteAlpha.200")(props),
-        _disabled: {
-          bg: mode("gray.300", "whiteAlpha.300")(props),
-        },
-      },
-      thumb: {
-        zIndex: 1,
-        borderRadius: "full",
-        bg: "white",
-        boxShadow: "base",
-        border: "1px solid",
-        borderColor: "transparent",
-        transition: "transform 0.2s",
-        _focus: { boxShadow: "outline" },
-        _disabled: { bg: "gray.300" },
-        ...thumbOrientation(props),
-      },
-      filledTrack: {
-        bg: mode(`${c}.500`, `${c}.200`)(props),
-      },
-    }
-  },
-
-  sizes: {
-    lg: function (props) {
-      return {
-        thumb: { w: "16px", h: "16px" },
-        track: orient({
-          orientation: props.orientation,
-          horizontal: { h: "4px" },
-          vertical: { w: "4px" },
-        }),
-      }
-    },
-
-    md: function (props) {
-      return {
-        thumb: { w: "14px", h: "14px" },
-        track: orient({
-          orientation: props.orientation,
-          horizontal: { h: "4px" },
-          vertical: { w: "4px" },
-        }),
-      }
-    },
-
-    sm: function (props) {
-      return {
-        thumb: { w: "10px", h: "10px" },
-        track: orient({
-          orientation: props.orientation,
-          horizontal: { h: "2px" },
-          vertical: { w: "2px" },
-        }),
-      }
-    },
-  },
-
-  defaultProps: {
-    size: "md",
-    colorScheme: "blue",
-  },
-})
-
 function thumbOrientation(props: Record<string, any>) {
   return orient({
     orientation: props.orientation,
@@ -108,6 +18,134 @@ function thumbOrientation(props: Record<string, any>) {
       },
     },
   })
+}
+
+const parts = {
+  container: "the slider wrapper",
+  thumb: "the slider thumb or handle",
+  track: "the slider outer track",
+  filledTrack: "the slider inner/filled track",
+}
+
+// @ts-ignore
+const baseStyleContainer = function (props) {
+  const { orientation } = props
+
+  return {
+    _disabled: {
+      opacity: 0.6,
+      cursor: "default",
+      pointerEvents: "none",
+    },
+    ...orient({
+      orientation,
+      vertical: { h: "100%" },
+      horizontal: { w: "100%" },
+    }),
+  }
+}
+
+// @ts-ignore
+const baseStyleTrack = function (props) {
+  return {
+    borderRadius: "sm",
+    bg: mode("gray.200", "whiteAlpha.200")(props),
+    _disabled: {
+      bg: mode("gray.300", "whiteAlpha.300")(props),
+    },
+  }
+}
+
+// @ts-ignore
+const baseStyleThumb = function (props) {
+  return {
+    zIndex: 1,
+    borderRadius: "full",
+    bg: "white",
+    boxShadow: "base",
+    border: "1px solid",
+    borderColor: "transparent",
+    transition: "transform 0.2s",
+    _focus: { boxShadow: "outline" },
+    _disabled: { bg: "gray.300" },
+    ...thumbOrientation(props),
+  }
+}
+
+// @ts-ignore
+const baseStyleFilledTrack = function (props) {
+  const { colorScheme: c } = props
+
+  return {
+    bg: mode(`${c}.500`, `${c}.200`)(props),
+  }
+}
+
+// @ts-ignore
+const baseStyle = (props) => ({
+  container: baseStyleContainer(props),
+  track: baseStyleTrack(props),
+  thumb: baseStyleThumb(props),
+  filledTrack: baseStyleFilledTrack(props),
+})
+
+const sizes = {
+  // @ts-ignore
+  lg: function (props) {
+    return {
+      thumb: { w: "16px", h: "16px" },
+      track: orient({
+        orientation: props.orientation,
+        horizontal: { h: "4px" },
+        vertical: { w: "4px" },
+      }),
+    }
+  },
+  // @ts-ignore
+  md: function (props) {
+    return {
+      thumb: { w: "14px", h: "14px" },
+      track: orient({
+        orientation: props.orientation,
+        horizontal: { h: "4px" },
+        vertical: { w: "4px" },
+      }),
+    }
+  },
+  // @ts-ignore
+  sm: function (props) {
+    return {
+      thumb: { w: "10px", h: "10px" },
+      track: orient({
+        orientation: props.orientation,
+        horizontal: { h: "2px" },
+        vertical: { w: "2px" },
+      }),
+    }
+  },
+}
+
+const defaultProps = {
+  size: "md",
+  colorScheme: "blue",
+}
+
+const slider = multiStyleConfig({
+  parts,
+  baseStyle,
+  sizes,
+  // @ts-ignore
+  defaultProps,
+})
+
+export const sliderStyles = {
+  parts,
+  sizes,
+  baseStyleContainer,
+  baseStyleFilledTrack,
+  baseStyleThumb,
+  baseStyleTrack,
+  defaultProps,
 }
 
 export default slider
