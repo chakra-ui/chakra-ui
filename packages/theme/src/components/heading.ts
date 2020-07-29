220378ee63f08257ee83258c54b1a80195446228
Héricles <hericles.me@gmail.com>
2020-07-29 09:37:39 -0300

    feat: split and export heading baseStyle

--- packages/theme/src/components/heading.ts
+++ packages/theme/src/components/heading.ts
@@ -1,28 +1,41 @@
 import { styleConfig } from "@chakra-ui/theme-tools"
+import { extraProps } from "@chakra-ui/system/dist/types/system.utils"
+
+const baseStyle = {
+  fontFamily: "heading",
+  lineHeight: "shorter",
+  fontWeight: "bold",
+}
+
+const sizes = {
+  "2xl": {
+    fontSize: ["4xl", null, "5xl"],
+  },
+  xl: {
+    fontSize: ["3xl", null, "4xl"],
+  },
+  lg: {
+    fontSize: ["2xl", null, "3xl"],
+  },
+  md: { fontSize: "xl" },
+  sm: { fontSize: "md" },
+  xs: { fontSize: "sm" },
+}
+
+const defaultProps = {
+  size: "xl",
+}
 
 const heading = styleConfig({
-  baseStyle: {
-    fontFamily: "heading",
-    lineHeight: "shorter",
-    fontWeight: "bold",
-  },
-  sizes: {
-    "2xl": {
-      fontSize: ["4xl", null, "5xl"],
-    },
-    xl: {
-      fontSize: ["3xl", null, "4xl"],
-    },
-    lg: {
-      fontSize: ["2xl", null, "3xl"],
-    },
-    md: { fontSize: "xl" },
-    sm: { fontSize: "md" },
-    xs: { fontSize: "sm" },
-  },
-  defaultProps: {
-    size: "xl",
-  },
+  baseStyle,
+  sizes,
+  // @ts-ignore
+  defaultProps,
 })
+
+export const headingStyles = {
+  baseStyle,
+  sizes,
+}
 
 export default heading
