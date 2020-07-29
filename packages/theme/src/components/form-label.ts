e5014de09dff334e842102f2a155c5795110e312
Héricles <hericles.me@gmail.com>
2020-07-29 09:33:39 -0300

    feat: export form-label baseStyle

--- packages/theme/src/components/form-label.ts
+++ packages/theme/src/components/form-label.ts
@@ -1,17 +1,23 @@
 import { styleConfig } from "@chakra-ui/theme-tools"
 
-const label = styleConfig({
-  baseStyle: {
-    fontSize: "md",
-    mr: 3,
-    mb: 2,
-    fontWeight: "medium",
-    transition: "all 0.2s",
-    opacity: 1,
-    _disabled: {
-      opacity: 0.4,
-    },
+const baseStyle = {
+  fontSize: "md",
+  mr: 3,
+  mb: 2,
+  fontWeight: "medium",
+  transition: "all 0.2s",
+  opacity: 1,
+  _disabled: {
+    opacity: 0.4,
   },
+}
+
+const label = styleConfig({
+  baseStyle,
 })
+
+export const formLabelStyles = {
+  baseStyle,
+}
 
 export default label
