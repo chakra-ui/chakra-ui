f8babf8199811e8b09505212698df1d99ff22de0
Sage <joseshegs@gmail.com>
2020-07-31 05:28:46 +0400

    chore: change creator or author and update config

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,52 @@
 const withMdxEnhanced = require("next-mdx-enhanced")
+const path = require("path")
+const execa = require("execa")
+const fromUnixTime = require("date-fns/fromUnixTime")
+const format = require("date-fns/format")
+
+/**
+ * Gets the last edited timestamp and author from git
+ * using `git log`
+ *
+ * %an = author name
+ * %ct = committer date, UNIX timestamp
+ *
+ * @see https://git-scm.com/docs/git-log
+ */
+async function getLastEdited(filePath) {
+  try {
+    const { stdout } = await execa("git", [
+      "log",
+      "-1",
+      "--format=%ct, %an",
+      filePath,
+    ])
+    return getTimestampAndAuthor(stdout)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const GIT_COMMIT_TIMESTAMP_AUTHOR_REGEX = /^(\d+), (.+)$/
+
+/**
+ * Format the last edited timestamp and author from git output
+ */
+function getTimestampAndAuthor(str) {
+  if (!str) return null
+
+  const temp = str.match(GIT_COMMIT_TIMESTAMP_AUTHOR_REGEX)
+
+  if (!temp || temp.length < 3) return null
+
+  const [_, timestamp, author] = temp
+  const dateStr = fromUnixTime(+timestamp)
+
+  return {
+    date: format(dateStr, "MMMM dd, yyyy"),
+    author,
+  }
+}
 
 module.exports = withMdxEnhanced({
   layoutPath: "layouts",
@@ -15,8 +63,15 @@ module.exports = withMdxEnhanced({
   ],
   rehypePlugins: [],
   extendFrontMatter: {
-    process: (_, frontmatter) => ({
-      slug: frontmatter.__resourcePath.replace(".mdx", ""),
-    }),
+    process: async (_, frontmatter) => {
+      const { __resourcePath: mdxPath } = frontmatter
+      const filePath = path.join(process.cwd(), "pages", mdxPath)
+      const lastEdited = await getLastEdited(filePath)
+
+      return {
+        slug: mdxPath.replace(".mdx", ""),
+        lastEdited,
+      }
+    },
   },
 })(/* your normal nextjs config */)
--- website/utils.js
+++ website/utils.js
@@ -1,55 +1,8 @@
-const fs = require("fs").promises
+const fs = require("fs")
 const path = require("path")
-const _ = require("lodash/fp")
-const { format, parseISO } = require("date-fns/fp")
-const simpleGit = require("simple-git")
 const { Octokit } = require("@octokit/rest")
 
-const git = simpleGit({ baseDir: path.join(process.cwd(), "..") })
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
-
-const collections = ["main", "theming", "layout", "form", "components", "hooks"]
-const compareCollections = (
-  { fields: { collection: a } },
-  { fields: { collection: b } },
-) => {
-  const aIndex = collections.indexOf(a)
-  const bIndex = collections.indexOf(b)
-
-  if (aIndex === bIndex) return 0
-  if (aIndex > bIndex) return 1
-  if (aIndex < bIndex) return -1
-}
-
-const groupByCollection = _.groupBy("fields.collection")
-const orderByOrderThenTitle = _.orderBy(
-  ["frontmatter.order", "frontmatter.title"],
-  ["asc", "asc"],
-)
-
-const sortPostNodes = (nodes) => {
-  const collections = groupByCollection(nodes)
-  const sortedCollectionNodes = _.values(collections).map(orderByOrderThenTitle)
-  const flattened = _.flatten(_.values(sortedCollectionNodes))
-  const allSorted = flattened.sort(compareCollections)
-
-  return allSorted
-}
-
-const getRelativePagePath = (fileAbsolutePath, source) => {
-  if (!fileAbsolutePath) return
-
-  const regex = new RegExp(`website/${source}/.*`)
-  const match = fileAbsolutePath.match(regex)
-  return match ? match[0] : null
-}
-
-const orderByCommitAuthorDate = _.orderBy(["commit.author.date"], ["asc"])
-const getCommitAuthorDetails = _.pick([
-  "commit.author.name",
-  "author.avatar_url",
-  "author.html_url",
-])
 
 const getNodeContributors = async (node) => {
   const relativePath = getRelativePagePath(node.fileAbsolutePath)
@@ -105,47 +58,16 @@ const getOrgMembers = async () => {
   return await Promise.all(sorted.map(getMemberData))
 }
 
-/**
- * Get all commits made to a specific path.
- */
-const getPathCommits = async (path) => {
-  // --follow allows git to follow a file's history across renames/moves
-  // -- makes sure log knows that path is for a file
-  const { all: commits } = await git.log(["--follow", "--", path])
-  return commits
-}
-
-/** Convert a date string to the "MMMM DD, YYYY" format.  */
-const formatDateString = _.compose(format("MMMM dd, yyyy"), parseISO)
-
-/**
- * Get the creation and last updated dates for a file. Uses `git` commit
- * history.
- */
-const getRelativePathHistoryDates = async (path) => {
-  const commits = await getPathCommits(path)
-
-  const firstCommit = _.last(commits)
-  const latestCommit = _.first(commits)
-
-  return {
-    createdAt: formatDateString(firstCommit.date),
-    updatedAt: formatDateString(latestCommit.date),
-  }
-}
-
-const readAllContributorsRc = async () => {
+const readAllContributorsRc = () => {
   const rcPath = path.resolve("..", ".all-contributorsrc")
-  const contributorsRcData = await fs.readFile(rcPath, "utf-8")
+  const contributorsRcData = fs.readFileSync(rcPath, "utf-8")
   const { contributors } = JSON.parse(contributorsRcData)
   return contributors
 }
 
 module.exports = {
-  sortPostNodes,
   getRelativePagePath,
   getNodeContributors,
   getOrgMembers,
   readAllContributorsRc,
-  getRelativePathHistoryDates,
 }
