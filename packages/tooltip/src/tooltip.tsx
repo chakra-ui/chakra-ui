c57c0c79ae24e8295bf4d901dc85a7888ee59db9
Gerrit Alex <admin@gerritalex.de>
2020-07-31 22:51:53 +0200

    refactor(typings): tooltip

--- packages/tooltip/src/tooltip.tsx
+++ packages/tooltip/src/tooltip.tsx
@@ -5,6 +5,7 @@ import {
   ThemingProps,
   useStyleConfig,
   omitThemingProps,
+  forwardRef,
 } from "@chakra-ui/system"
 import { isString, omit, pick, __DEV__, mergeRefs } from "@chakra-ui/utils"
 import { VisuallyHidden } from "@chakra-ui/visually-hidden"
@@ -48,10 +49,7 @@ export type TooltipProps = PropsOf<typeof chakra.div> &
  * @see Docs     https://chakra-ui.com/components/tooltip
  * @see WAI-ARIA https://www.w3.org/TR/wai-aria-practices/#tooltip
  */
-export const Tooltip = React.forwardRef(function Tooltip(
-  props: TooltipProps,
-  ref: React.Ref<any>,
-) {
+export const Tooltip: React.FC<TooltipProps> = forwardRef((props, ref) => {
   const styles = useStyleConfig("Tooltip", props)
   const realProps = omitThemingProps(props)
 
