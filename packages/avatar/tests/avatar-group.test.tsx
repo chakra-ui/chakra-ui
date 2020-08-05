617a336b93935a9183b4f6141fcee04a0f40e252
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:03:15 +0200

    test(avatar-group): add a11y test, streamline tests

--- packages/avatar/tests/avatar-group.test.tsx
+++ packages/avatar/tests/avatar-group.test.tsx
@@ -1,28 +1,38 @@
 import * as React from "react"
-import { render } from "@chakra-ui/test-utils"
+import { render, testA11y, screen } from "@chakra-ui/test-utils"
 import { Avatar, AvatarGroup } from "../src"
 
-test("AvatarGroup renders correctly", () => {
-  const tools = render(
-    <AvatarGroup>
-      <Avatar />
-    </AvatarGroup>,
-  )
-  expect(tools.asFragment()).toMatchSnapshot()
-})
+describe("<AvatarGroup />", () => {
+  test("AvatarGroup renders correctly", () => {
+    const tools = render(
+      <AvatarGroup>
+        <Avatar />
+      </AvatarGroup>,
+    )
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
 
-test("renders a number avatar showing count of truncated avatars", () => {
-  const tools = render(
-    <AvatarGroup max={2}>
-      <Avatar />
-      <Avatar />
-      <Avatar />
-      <Avatar />
-      <Avatar />
-    </AvatarGroup>,
-  )
-  const moreLabel = tools.getByText("+3")
-  expect(moreLabel).toBeInTheDocument()
+  it("passes a11y test", async () => {
+    await testA11y(
+      <AvatarGroup>
+        <Avatar />
+      </AvatarGroup>,
+    )
+  })
+
+  test("renders a number avatar showing count of truncated avatars", () => {
+    render(
+      <AvatarGroup max={2}>
+        <Avatar />
+        <Avatar />
+        <Avatar />
+        <Avatar />
+        <Avatar />
+      </AvatarGroup>,
+    )
+    const moreLabel = screen.getByText("+3")
+    expect(moreLabel).toBeInTheDocument()
+  })
 })
 
 test("does not render a number avatar showing count of truncated avatars if max is equal to avatars given", async () => {
