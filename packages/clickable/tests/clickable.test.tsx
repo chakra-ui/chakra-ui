94b0b57dc0499d6268af660729e4067276095457
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:05:36 +0200

    test(clickable): add a11y test, streamline tests

--- packages/clickable/tests/clickable.test.tsx
+++ packages/clickable/tests/clickable.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { render, userEvent, fireEvent } from "@chakra-ui/test-utils"
+import { render, userEvent, fireEvent, screen } from "@chakra-ui/test-utils"
 import { chakra, forwardRef } from "@chakra-ui/system"
 import { ClickableProps } from "../stories/use-clickable.stories"
 import { useClickable } from "../src"
@@ -9,9 +9,10 @@ const Clickable: React.FC<ClickableProps> = forwardRef((props, ref) => {
   return <chakra.button display="inline-flex" {...clickable} />
 })
 
-test("should render correctly", () => {
-  const tools = render(<Clickable>clickable</Clickable>)
-  expect(tools.getByText("clickable")).toMatchInlineSnapshot(`
+describe("<Clickable />", () => {
+  test("should render correctly", () => {
+    render(<Clickable>clickable</Clickable>)
+    expect(screen.getByText("clickable")).toMatchInlineSnapshot(`
     .emotion-0 {
       display: -webkit-inline-box;
       display: -webkit-inline-flex;
@@ -26,15 +27,15 @@ test("should render correctly", () => {
       clickable
     </button>
   `)
-})
+  })
 
-test("should render when disabled and focusable", () => {
-  const tools = render(
-    <Clickable isDisabled isFocusable>
-      clickable
-    </Clickable>,
-  )
-  expect(tools.getByText("clickable")).toMatchInlineSnapshot(`
+  test("should render when disabled and focusable", () => {
+    render(
+      <Clickable isDisabled isFocusable>
+        clickable
+      </Clickable>,
+    )
+    expect(screen.getByText("clickable")).toMatchInlineSnapshot(`
     .emotion-0 {
       display: -webkit-inline-box;
       display: -webkit-inline-flex;
@@ -50,56 +51,57 @@ test("should render when disabled and focusable", () => {
       clickable
     </button>
   `)
-})
+  })
 
-test("should click correctly", () => {
-  const fn = jest.fn()
-  const tools = render(<Clickable onClick={fn}>clickable</Clickable>)
-  const clickable = tools.getByText("clickable")
+  test("should click correctly", () => {
+    const fn = jest.fn()
+    render(<Clickable onClick={fn}>clickable</Clickable>)
+    const clickable = screen.getByText("clickable")
 
-  expect(fn).toHaveBeenCalledTimes(0)
-  userEvent.click(clickable)
-  expect(fn).toHaveBeenCalledTimes(1)
-})
+    expect(fn).toHaveBeenCalledTimes(0)
+    userEvent.click(clickable)
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
 
-test("should not click if disabled", () => {
-  const fn = jest.fn()
+  test("should not click if disabled", () => {
+    const fn = jest.fn()
 
-  const tools = render(
-    <Clickable onClick={fn} isDisabled>
-      clickable
-    </Clickable>,
-  )
+    render(
+      <Clickable onClick={fn} isDisabled>
+        clickable
+      </Clickable>,
+    )
 
-  const clickable = tools.getByText("clickable")
-  userEvent.click(clickable)
-  expect(fn).toHaveBeenCalledTimes(0)
-})
+    const clickable = screen.getByText("clickable")
+    userEvent.click(clickable)
+    expect(fn).toHaveBeenCalledTimes(0)
+  })
 
-test("should not focus if disabled", () => {
-  const tools = render(<Clickable isDisabled>clickable</Clickable>)
-  const clickable = tools.getByText("clickable")
+  test("should not focus if disabled", () => {
+    render(<Clickable isDisabled>clickable</Clickable>)
+    const clickable = screen.getByText("clickable")
 
-  expect(clickable).not.toHaveFocus()
-  fireEvent.focus(clickable)
-  expect(clickable).not.toHaveFocus()
-})
+    expect(clickable).not.toHaveFocus()
+    fireEvent.focus(clickable)
+    expect(clickable).not.toHaveFocus()
+  })
 
-test("non-native: should click on press `space` or `enter`", () => {
-  const fn = jest.fn()
-  const tools = render(
-    <Clickable as="div" onClick={fn}>
-      clickable
-    </Clickable>,
-  )
+  test("non-native: should click on press `space` or `enter`", () => {
+    const fn = jest.fn()
+    render(
+      <Clickable as="div" onClick={fn}>
+        clickable
+      </Clickable>,
+    )
 
-  const clickable = tools.getByText("clickable")
+    const clickable = screen.getByText("clickable")
 
-  fireEvent.keyDown(clickable, { key: "Enter" })
-  expect(fn).toHaveBeenCalledTimes(1)
+    fireEvent.keyDown(clickable, { key: "Enter" })
+    expect(fn).toHaveBeenCalledTimes(1)
 
-  fireEvent.keyDown(clickable, { key: " " })
-  fireEvent.keyUp(clickable, { key: " " })
+    fireEvent.keyDown(clickable, { key: " " })
+    fireEvent.keyUp(clickable, { key: " " })
 
-  expect(fn).toHaveBeenCalledTimes(2)
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
 })
