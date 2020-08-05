cb237af1c49be0e13d7fc70e359c31cb17d5e4cc
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:04:02 +0200

    test(button-group): add a11y test, streamline tests

--- packages/button/tests/button-group.test.tsx
+++ packages/button/tests/button-group.test.tsx
@@ -1,15 +1,28 @@
 import * as React from "react"
-import { render } from "@chakra-ui/test-utils"
+import { render, testA11y } from "@chakra-ui/test-utils"
 import { ButtonGroup, Button } from "../src"
 
-test("ButtonGroup renders correctly", () => {
-  const { asFragment } = render(
-    <ButtonGroup>
-      <Button>Button 1</Button>
-      <Button>Button 2</Button>
-      <Button>Button 3</Button>
-      <Button>Button 4</Button>
-    </ButtonGroup>,
-  )
-  expect(asFragment()).toMatchSnapshot()
+describe("<ButtonGroup />", () => {
+  test("ButtonGroup renders correctly", () => {
+    const { asFragment } = render(
+      <ButtonGroup>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+        <Button>Button 4</Button>
+      </ButtonGroup>,
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("passes a11y test", async () => {
+    await testA11y(
+      <ButtonGroup>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+        <Button>Button 4</Button>
+      </ButtonGroup>,
+    )
+  })
 })
