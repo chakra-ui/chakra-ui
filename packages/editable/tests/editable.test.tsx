015fc5a27a9e40844d2cbca82fae8196b7d0cee5
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:06:31 +0200

    test(editable): add a11y test, streamline tests

--- packages/editable/tests/editable.test.tsx
+++ packages/editable/tests/editable.test.tsx
@@ -1,204 +1,221 @@
 import * as React from "react"
-import { render, userEvent, fireEvent } from "@chakra-ui/test-utils"
+import {
+  render,
+  userEvent,
+  fireEvent,
+  screen,
+  testA11y,
+} from "@chakra-ui/test-utils"
 import { Editable, EditableInput, EditablePreview } from "../src"
 
-test("should match snapshot", () => {
-  const tools = render(
-    <Editable defaultValue="testing">
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
+describe("<Editable />", () => {
+  test("matches snapshot", () => {
+    const { asFragment } = render(
+      <Editable defaultValue="testing">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
 
-  expect(tools.asFragment()).toMatchSnapshot()
-})
+    expect(asFragment()).toMatchSnapshot()
+  })
 
-test("should render properly", () => {
-  const tools = render(
-    <Editable defaultValue="testing">
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
+  test("renders without crashing", () => {
+    render(
+      <Editable defaultValue="testing">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
 
-  const preview = tools.getByTestId("preview")
-  const input = tools.getByTestId("input")
+    const preview = screen.getByTestId("preview")
+    const input = screen.getByTestId("input")
 
-  expect(input).toHaveAttribute("hidden")
-  expect(preview).toHaveTextContent("testing")
-})
+    expect(input).toHaveAttribute("hidden")
+    expect(preview).toHaveTextContent("testing")
+  })
 
-test("uncontrolled: handles callbacks correctly", async () => {
-  const onChange = jest.fn()
-  const onCancel = jest.fn()
-  const onSubmit = jest.fn()
-  const onEdit = jest.fn()
+  it("passes a11y test", async () => {
+    await testA11y(
+      <Editable defaultValue="testing">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
+  })
 
-  const tools = render(
-    <Editable
-      onChange={onChange}
-      onCancel={onCancel}
-      onSubmit={onSubmit}
-      onEdit={onEdit}
-      defaultValue="Hello "
-    >
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
-  const preview = tools.getByTestId("preview")
-  const input = tools.getByTestId("input")
+  test("uncontrolled: handles callbacks correctly", async () => {
+    const onChange = jest.fn()
+    const onCancel = jest.fn()
+    const onSubmit = jest.fn()
+    const onEdit = jest.fn()
 
-  // calls `onEdit` when preview is focused
-  fireEvent.focus(preview)
-  expect(onEdit).toHaveBeenCalled()
-
-  // calls `onChange` with input on change
-  userEvent.type(input, "World")
-  expect(onChange).toHaveBeenCalledWith("World")
-
-  // calls `onCancel` with previous value when "esc" pressed
-  fireEvent.keyDown(input, { key: "Escape" })
-  expect(onCancel).toHaveBeenCalledWith("Hello ")
-
-  fireEvent.focus(preview)
-
-  // calls `onChange` with input on change
-  userEvent.type(input, "World")
-  expect(onChange).toHaveBeenCalledWith("World")
-
-  // calls `onSubmit` with previous value when "enter" pressed after cancelling
-  fireEvent.keyDown(input, { key: "Enter" })
-  expect(onSubmit).toHaveBeenCalledWith("World")
-})
-
-test("controlled: handles callbacks correctly", () => {
-  const onChange = jest.fn()
-  const onCancel = jest.fn()
-  const onSubmit = jest.fn()
-  const onEdit = jest.fn()
-
-  const Component = () => {
-    const [value, setValue] = React.useState("Hello ")
-    return (
+    render(
       <Editable
-        onChange={(val) => {
-          setValue(val)
-          onChange(val)
-        }}
+        onChange={onChange}
         onCancel={onCancel}
         onSubmit={onSubmit}
         onEdit={onEdit}
-        value={value}
+        defaultValue="Hello "
       >
         <EditablePreview data-testid="preview" />
         <EditableInput data-testid="input" />
-      </Editable>
+      </Editable>,
     )
-  }
+    const preview = screen.getByTestId("preview")
+    const input = screen.getByTestId("input")
 
-  const tools = render(<Component />)
-  const preview = tools.getByTestId("preview")
-  const input = tools.getByTestId("input")
+    // calls `onEdit` when preview is focused
+    fireEvent.focus(preview)
+    expect(onEdit).toHaveBeenCalled()
 
-  // calls `onEdit` when preview is focused
-  fireEvent.focus(preview)
-  expect(onEdit).toHaveBeenCalled()
+    // calls `onChange` with input on change
+    userEvent.type(input, "World")
+    expect(onChange).toHaveBeenCalledWith("World")
 
-  // calls `onChange` with new input on change
-  // since we calld `focus(..)` first, editable will focus and select the text
-  // typing will clear the values in input and add the next text.
-  userEvent.type(input, "World")
-  expect(onChange).toHaveBeenCalledWith("World")
+    // calls `onCancel` with previous value when "esc" pressed
+    fireEvent.keyDown(input, { key: "Escape" })
+    expect(onCancel).toHaveBeenCalledWith("Hello ")
 
-  // calls `onSubmit` with `value`
-  fireEvent.keyDown(input, { key: "Enter" })
-  expect(onSubmit).toHaveBeenCalledWith("World")
+    fireEvent.focus(preview)
 
-  expect(input).not.toBeVisible()
-  fireEvent.focus(preview)
+    // calls `onChange` with input on change
+    userEvent.type(input, "World")
+    expect(onChange).toHaveBeenCalledWith("World")
 
-  // update the input value
-  userEvent.type(input, "Rasengan")
+    // calls `onSubmit` with previous value when "enter" pressed after cancelling
+    fireEvent.keyDown(input, { key: "Enter" })
+    expect(onSubmit).toHaveBeenCalledWith("World")
+  })
 
-  // press `Escape`
-  fireEvent.keyDown(input, { key: "Escape" })
+  test("controlled: handles callbacks correctly", () => {
+    const onChange = jest.fn()
+    const onCancel = jest.fn()
+    const onSubmit = jest.fn()
+    const onEdit = jest.fn()
 
-  // calls `onCancel` with previous `value`
-  expect(onSubmit).toHaveBeenCalledWith("World")
-})
+    const Component = () => {
+      const [value, setValue] = React.useState("Hello ")
+      return (
+        <Editable
+          onChange={(val) => {
+            setValue(val)
+            onChange(val)
+          }}
+          onCancel={onCancel}
+          onSubmit={onSubmit}
+          onEdit={onEdit}
+          value={value}
+        >
+          <EditablePreview data-testid="preview" />
+          <EditableInput data-testid="input" />
+        </Editable>
+      )
+    }
 
-test("handles preview and input callbacks", () => {
-  const onFocus = jest.fn()
-  const onBlur = jest.fn()
-  const onChange = jest.fn()
-  const onKeyDown = jest.fn()
+    render(<Component />)
+    const preview = screen.getByTestId("preview")
+    const input = screen.getByTestId("input")
 
-  const tools = render(
-    <Editable defaultValue="Hello ">
-      <EditablePreview onFocus={onFocus} data-testid="preview" />
-      <EditableInput
-        onBlur={onBlur}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        data-testid="input"
-      />
-    </Editable>,
-  )
-  const preview = tools.getByTestId("preview")
-  const input = tools.getByTestId("input")
+    // calls `onEdit` when preview is focused
+    fireEvent.focus(preview)
+    expect(onEdit).toHaveBeenCalled()
 
-  // calls `onFocus` when preview is focused
-  fireEvent.focus(preview)
-  expect(onFocus).toHaveBeenCalled()
+    // calls `onChange` with new input on change
+    // since we calld `focus(..)` first, editable will focus and select the text
+    // typing will clear the values in input and add the next text.
+    userEvent.type(input, "World")
+    expect(onChange).toHaveBeenCalledWith("World")
 
-  // calls `onChange` when input is changed
-  userEvent.type(input, "World")
-  expect(onChange).toHaveBeenCalled()
+    // calls `onSubmit` with `value`
+    fireEvent.keyDown(input, { key: "Enter" })
+    expect(onSubmit).toHaveBeenCalledWith("World")
 
-  // calls `onKeyDown` when key is pressed in input
-  fireEvent.keyDown(input, { key: "Escape" })
-  expect(onKeyDown).toHaveBeenCalled()
+    expect(input).not.toBeVisible()
+    fireEvent.focus(preview)
 
-  expect(input).toHaveAttribute("hidden")
-})
+    // update the input value
+    userEvent.type(input, "Rasengan")
 
-test("has the proper aria attributes", () => {
-  const tools = render(
-    <Editable defaultValue="">
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
-  const preview = tools.getByTestId("preview")
-  const input = tools.getByTestId("input")
+    // press `Escape`
+    fireEvent.keyDown(input, { key: "Escape" })
 
-  // preview and input do not have aria-disabled when `Editable` is not disabled
-  expect(preview).not.toHaveAttribute("aria-disabled")
-  expect(input).not.toHaveAttribute("aria-disabled")
+    // calls `onCancel` with previous `value`
+    expect(onSubmit).toHaveBeenCalledWith("World")
+  })
 
-  tools.rerender(
-    <Editable isDisabled defaultValue="">
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
+  test("handles preview and input callbacks", () => {
+    const onFocus = jest.fn()
+    const onBlur = jest.fn()
+    const onChange = jest.fn()
+    const onKeyDown = jest.fn()
 
-  // preview and input have aria-disabled when `Editable` is disabled
-  expect(preview).toHaveAttribute("aria-disabled", "true")
-  expect(input).toHaveAttribute("aria-disabled", "true")
-})
+    render(
+      <Editable defaultValue="Hello ">
+        <EditablePreview onFocus={onFocus} data-testid="preview" />
+        <EditableInput
+          onBlur={onBlur}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          data-testid="input"
+        />
+      </Editable>,
+    )
+    const preview = screen.getByTestId("preview")
+    const input = screen.getByTestId("input")
 
-test("can submit on blur", () => {
-  const onSubmit = jest.fn()
-  const tools = render(
-    <Editable submitOnBlur onSubmit={onSubmit} defaultValue="testing">
-      <EditablePreview data-testid="preview" />
-      <EditableInput data-testid="input" />
-    </Editable>,
-  )
-  const input = tools.getByTestId("input")
+    // calls `onFocus` when preview is focused
+    fireEvent.focus(preview)
+    expect(onFocus).toHaveBeenCalled()
 
-  fireEvent.blur(input)
-  expect(onSubmit).toHaveBeenCalledWith("testing")
+    // calls `onChange` when input is changed
+    userEvent.type(input, "World")
+    expect(onChange).toHaveBeenCalled()
+
+    // calls `onKeyDown` when key is pressed in input
+    fireEvent.keyDown(input, { key: "Escape" })
+    expect(onKeyDown).toHaveBeenCalled()
+
+    expect(input).toHaveAttribute("hidden")
+  })
+
+  test("has the proper aria attributes", () => {
+    const { rerender } = render(
+      <Editable defaultValue="">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
+    const preview = screen.getByTestId("preview")
+    const input = screen.getByTestId("input")
+
+    // preview and input do not have aria-disabled when `Editable` is not disabled
+    expect(preview).not.toHaveAttribute("aria-disabled")
+    expect(input).not.toHaveAttribute("aria-disabled")
+
+    rerender(
+      <Editable isDisabled defaultValue="">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
+
+    // preview and input have aria-disabled when `Editable` is disabled
+    expect(preview).toHaveAttribute("aria-disabled", "true")
+    expect(input).toHaveAttribute("aria-disabled", "true")
+  })
+
+  test("can submit on blur", () => {
+    const onSubmit = jest.fn()
+    render(
+      <Editable submitOnBlur onSubmit={onSubmit} defaultValue="testing">
+        <EditablePreview data-testid="preview" />
+        <EditableInput data-testid="input" />
+      </Editable>,
+    )
+    const input = screen.getByTestId("input")
+
+    fireEvent.blur(input)
+    expect(onSubmit).toHaveBeenCalledWith("testing")
+  })
 })
