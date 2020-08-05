18d6d374446e53a059141e2586db8d51a3cbe1ac
Gerrit Alex <admin@gerritalex.de>
2020-08-05 23:02:53 +0200

    test(alert): add a11y test, streamline tests

--- packages/alert/tests/alert.test.tsx
+++ packages/alert/tests/alert.test.tsx
@@ -1,39 +1,38 @@
 import * as React from "react"
-import { render, axe } from "@chakra-ui/test-utils"
+import { render, testA11y, screen } from "@chakra-ui/test-utils"
 import { Alert, AlertDescription, AlertIcon, AlertTitle } from "../src"
 
-test("should have no accessibility issue", async () => {
-  const tools = render(
-    <Alert>
-      <AlertIcon />
-      <AlertTitle>Alert title</AlertTitle>
-      <AlertDescription>Alert description</AlertDescription>
-    </Alert>,
-  )
+describe("<Alert />", () => {
+  test("should render correctly", () => {
+    const tools = render(
+      <Alert>
+        <AlertIcon />
+        <AlertTitle>Alert title</AlertTitle>
+        <AlertDescription>Alert description</AlertDescription>
+      </Alert>,
+    )
+    expect(tools.asFragment()).toMatchSnapshot()
+  })
 
-  const results = await axe(tools.container)
-  expect(results).toHaveNoViolations()
-})
+  it("passes a11y test", async () => {
+    await testA11y(
+      <Alert>
+        <AlertIcon />
+        <AlertTitle>Alert title</AlertTitle>
+        <AlertDescription>Alert description</AlertDescription>
+      </Alert>,
+    )
+  })
 
-test("should render correctly", () => {
-  const tools = render(
-    <Alert>
-      <AlertIcon />
-      <AlertTitle>Alert title</AlertTitle>
-      <AlertDescription>Alert description</AlertDescription>
-    </Alert>,
-  )
-  expect(tools.asFragment()).toMatchSnapshot()
-})
+  test("should have role='alert'", () => {
+    render(
+      <Alert>
+        <AlertIcon />
+        <AlertTitle>Alert title</AlertTitle>
+        <AlertDescription>Alert description</AlertDescription>
+      </Alert>,
+    )
 
-test("should have role='alert'", () => {
-  const tools = render(
-    <Alert>
-      <AlertIcon />
-      <AlertTitle>Alert title</AlertTitle>
-      <AlertDescription>Alert description</AlertDescription>
-    </Alert>,
-  )
-  const alert = tools.getByRole("alert")
-  expect(alert).toBeInTheDocument()
+    expect(screen.getByRole("alert")).toBeInTheDocument()
+  })
 })
