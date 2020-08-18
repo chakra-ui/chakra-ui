5dc1ef6f18fbc6216f2759bfe86245eaafbed468
Tim Kolberger <tim@kolberger.eu>
2020-08-18 23:37:27 +0200

    refactor(standalone-toast): export everything from use-toast

--- packages/toast/src/index.ts
+++ packages/toast/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./use-toast"
-export { default, createStandaloneToast } from "./use-toast"
+export * from "./use-toast"
 export { toast } from "./toast.class"
 export * from "./toast.types"
