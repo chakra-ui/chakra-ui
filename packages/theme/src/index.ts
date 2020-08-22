47ea2ee70d939fa6cae06d0faa63553b55170c81
Segun Adebayo <joseshegs@gmail.com>
2020-08-23 01:09:27 +0400

    chore: remove color mode config from theme

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,21 +1,11 @@
 import foundations from "./foundations"
 import components from "./components"
 import styles from "./styles"
-import { ColorModeOptions } from "@chakra-ui/system"
-
-/**
- * Color mode config
- */
-const config: ColorModeOptions = {
-  useSystemColorMode: false,
-  initialColorMode: "light",
-}
 
 export const theme = {
   ...foundations,
   components,
   styles,
-  config,
 }
 
 export type Theme = typeof theme
