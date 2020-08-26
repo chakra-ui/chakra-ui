ab3a0c7574f252cf618e0c0464352743c82222a6
Mark Chandler <mark@chanchan.io>
2020-08-26 17:41:57 -0400

    perf(website): remove unused buttons.js

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -20,7 +20,6 @@ const App = ({ Component, pageProps }) => {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
         <meta name="theme-color" content="#319795" />
-        <script async defer src="https://buttons.github.io/buttons.js" />
       </Head>
       <ChakraProvider resetCSS theme={theme} portalZIndex={40}>
         <DefaultSeo {...siteConfig.seo} />
