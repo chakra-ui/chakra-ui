c50b5e50e9376a1feaa14ffc3c01ace1050eb396
Mark Chandler <mark@chanchan.io>
2020-08-26 17:46:45 -0400

    perf(website): add preconnect for google and cloudflare

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -19,6 +19,8 @@ const App = ({ Component, pageProps }) => {
         <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://static.cloudflareinsights.com" />
         <meta name="theme-color" content="#319795" />
       </Head>
       <ChakraProvider resetCSS theme={theme} portalZIndex={40}>
