b1feeaf874da347e9bf564d13598cd8f596e9b07
Tim Kolberger <tim@kolberger.eu>
2020-09-05 22:48:22 +0200

    feat(extend-theme): evaluate function results in extendTheme

---
+++ packages/core/src/extend-theme.ts
@@ -0,0 +1,25 @@
+import defaultTheme, { Theme } from "@chakra-ui/theme"
+import { isFunction, mergeWith } from "@chakra-ui/utils"
+
+export function extendTheme<T extends Theme | Record<string, any>>(
+  overrides: T,
+) {
+  function customizer(sourceThunk: unknown, overrideThunk: unknown) {
+    if (isFunction(sourceThunk)) {
+      return (...args: unknown[]) => {
+        const sourceValue = sourceThunk(...args)
+
+        const overrideValue = isFunction(overrideThunk)
+          ? overrideThunk(...args)
+          : overrideThunk
+
+        return mergeWith(sourceValue, overrideValue, customizer)
+      }
+    }
+
+    // fallback to default behavior
+    return undefined
+  }
+
+  return mergeWith(defaultTheme, overrides, customizer)
+}
