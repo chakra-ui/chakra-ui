da5efba719c8cdb1242e739530d722110eff9965
Gerrit Alex <admin@gerritalex.de>
2020-09-13 23:14:56 +0200

    test(themetools): add createBreakpoints test

---
+++ packages/theme-tools/tests/breakpoints.test.ts
@@ -0,0 +1,103 @@
+import { createBreakpoints } from "../src"
+
+const defaultBreakpoints = {
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+}
+
+describe("createBreakpoints", () => {
+  test("returns an array", () => {
+    expect(createBreakpoints(defaultBreakpoints)).toBeInstanceOf(Array)
+  })
+
+  test("sorts by value size", () => {
+    const reversedDefaults = Object.fromEntries(
+      Object.entries(defaultBreakpoints).reverse(),
+    )
+
+    const expectedResult = createBreakpoints(defaultBreakpoints)
+
+    const result = createBreakpoints(reversedDefaults)
+
+    expect(result).toEqual(expect.arrayContaining(expectedResult))
+
+    Object.entries(result).forEach(([key, value]) => {
+      expect(result[key]).toBe(value)
+    })
+  })
+
+  test("accepts further breakpoints", () => {
+    const xxl = "116em"
+
+    const extendedBreakpoints = {
+      ...defaultBreakpoints,
+      xxl,
+    }
+
+    const expectedResult = [
+      extendedBreakpoints.sm,
+      extendedBreakpoints.md,
+      extendedBreakpoints.lg,
+      extendedBreakpoints.xl,
+      xxl,
+    ]
+
+    const result = createBreakpoints(extendedBreakpoints)
+
+    expect(result).toEqual(expect.arrayContaining(expectedResult))
+
+    Object.entries(extendedBreakpoints).forEach(([key, value]) => {
+      expect(result[key]).toBe(value)
+    })
+  })
+
+  test("works with px", () => {
+    const breakpoints = {
+      sm: "300px",
+      md: "480px",
+      lg: "620px",
+      xl: "800px",
+    }
+
+    const expectedResult = [
+      breakpoints.sm,
+      breakpoints.md,
+      breakpoints.lg,
+      breakpoints.xl,
+    ]
+
+    const result = createBreakpoints(breakpoints)
+
+    expect(result).toEqual(expect.arrayContaining(expectedResult))
+
+    Object.entries(breakpoints).forEach(([key, value]) => {
+      expect(result[key]).toBe(value)
+    })
+  })
+
+  test("works with rem", () => {
+    const breakpoints = {
+      sm: "30rem",
+      md: "48rem",
+      lg: "62rem",
+      xl: "80rem",
+    }
+
+    const expectedResult = [
+      breakpoints.sm,
+      breakpoints.md,
+      breakpoints.lg,
+      breakpoints.xl,
+    ]
+
+    const result = createBreakpoints(breakpoints)
+
+    expect(result).toEqual(expect.arrayContaining(expectedResult))
+
+    Object.entries(breakpoints).forEach(([key, value]) => {
+      expect(result[key]).toBe(value)
+    })
+  })
+})
