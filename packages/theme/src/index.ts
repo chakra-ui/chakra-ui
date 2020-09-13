2b788d15dff332d06d5e1f772f1b1e00a59a6f1a
Gerrit Alex <admin@gerritalex.de>
2020-09-13 23:15:15 +0200

    chore(theme): reexport from theme-tools

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,6 +2,12 @@ import foundations from "./foundations"
 import components from "./components"
 import styles from "./styles"
 
+export {
+  Breakpoints,
+  BaseBreakpointConfig,
+  createBreakpoints,
+} from "@chakra-ui/theme-tools"
+
 export const theme = {
   ...foundations,
   components,
