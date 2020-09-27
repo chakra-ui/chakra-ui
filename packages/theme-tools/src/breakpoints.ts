4228e53f175d51805de28f504f6da21057512e77
Gerrit Alex <admin@gerritalex.de>
2020-09-27 18:11:38 +0200

    fix(themetools): object.entries node v10 compat

--- packages/theme-tools/src/breakpoints.ts
+++ packages/theme-tools/src/breakpoints.ts
@@ -11,11 +11,12 @@ export type WithBase<T> = T & { base: "0em" }
 export const createBreakpoints = <T extends BaseBreakpointConfig>(
   config: T,
 ): Breakpoints<T> => {
-  const sorted = Object.fromEntries(
-    Object.entries({ base: "0em", ...config }).sort((a, b) =>
-      parseInt(a[1]) > parseInt(b[1]) ? 1 : -1,
-    ),
-  ) as WithBase<T>
+  const sorted = Object.entries({ base: "0em", ...config })
+    .sort((a, b) => (parseInt(a[1]) > parseInt(b[1]) ? 1 : -1))
+    .reduce((carry, [key, value]) => {
+      carry[key] = value
+      return carry
+    }, {}) as WithBase<T>
 
   return Object.assign(Object.values(sorted), sorted)
 }
