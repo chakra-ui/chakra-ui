5ff03544fb9a3af4a6d4479d5d6784d9654b6005
Segun Adebayo <joseshegs@gmail.com>
2020-10-04 15:09:57 +0400

    chore: remove unused functions in popper

--- packages/popper/src/popper.utils.ts
+++ packages/popper/src/popper.utils.ts
@@ -1,44 +1,5 @@
 import { Placement } from "@popperjs/core"
 
-const oppositeDirections = {
-  top: "bottom",
-  bottom: "top",
-  right: "left",
-  left: "right",
-}
-
-type Direction = keyof typeof oppositeDirections
-
-export const getOppositePosition = (position: Direction) =>
-  oppositeDirections[position]
-
-const splitPlacement = (placement: Placement) =>
-  placement.split("-") as Direction[]
-
-export function getArrowStyles(
-  placement: Placement | undefined,
-  arrowSize: number,
-  arrowShadowColor?: string,
-): React.CSSProperties {
-  if (typeof placement !== "string") return {}
-
-  const [position] = splitPlacement(placement)
-  const oppositePosition = getOppositePosition(position)
-
-  if (!oppositePosition) return {}
-
-  return {
-    [oppositePosition]: `-${arrowSize / 2}px`,
-    width: arrowSize,
-    height: arrowSize,
-    position: "absolute",
-    transform: "rotate(45deg)",
-    boxShadow: arrowShadowColor
-      ? getBoxShadow(placement, arrowShadowColor)
-      : undefined,
-  }
-}
-
 export function getBoxShadow(placement: Placement, color: string) {
   if (placement.includes("top")) {
     return `2px 2px 2px 0 ${color}`
