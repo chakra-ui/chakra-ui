e8ed5982be32b621d0862e214da3d6778c40b116
Segun Adebayo <joseshegs@gmail.com>
2020-10-10 20:36:08 +0400

    feat: add scale-fade

---
+++ packages/transition/src/scale-fade.tsx
@@ -0,0 +1,78 @@
+import { forwardRef, PropsOf } from "@chakra-ui/system"
+import { cx, __DEV__ } from "@chakra-ui/utils"
+import { AnimatePresence, motion } from "framer-motion"
+import * as React from "react"
+import { MotionVariants } from "./__utils"
+
+export const ScaleFadeMotionVariants: MotionVariants<"enter" | "exit"> = {
+  exit: (props: ScaleFadeOptions) => ({
+    opacity: 0,
+    ...(props.reverse
+      ? { scale: props.initialScale }
+      : { transitionEnd: { scale: props.initialScale } }),
+    transition: {
+      duration: 0.1,
+      easings: "easeOut",
+    },
+  }),
+  enter: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.25,
+      ease: [0.4, 0, 0.2, 1],
+    },
+  },
+}
+
+export interface ScaleFadeOptions {
+  /**
+   * If `true`, the collapse will unmount when `isOpen={false}` and animation is done
+   */
+  unmountOnExit?: boolean
+  /**
+   * If `true`, the content will slide in
+   */
+  in?: boolean
+  /**
+   * The offset on the horizontal or `x` axis
+   */
+  initialScale?: number
+  reverse?: boolean
+}
+
+interface ScaleFadeProps extends PropsOf<typeof motion.div>, ScaleFadeOptions {}
+
+export const ScaleFade = forwardRef<ScaleFadeProps, "div">((props, ref) => {
+  const {
+    unmountOnExit,
+    in: isOpen,
+    reverse = true,
+    className,
+    initialScale = 0.95,
+    ...rest
+  } = props
+
+  const shouldExpand = unmountOnExit ? isOpen && unmountOnExit : true
+
+  return (
+    <AnimatePresence custom={{ initialScale, reverse }}>
+      {shouldExpand && (
+        <motion.div
+          ref={ref}
+          initial="exit"
+          className={cx("chakra-offset-slide", className)}
+          animate={isOpen || unmountOnExit ? "enter" : "exit"}
+          exit="exit"
+          variants={ScaleFadeMotionVariants}
+          custom={{ initialScale, reverse }}
+          {...rest}
+        />
+      )}
+    </AnimatePresence>
+  )
+})
+
+if (__DEV__) {
+  ScaleFade.displayName = "ScaleFade"
+}
--- packages/transition/stories/transition.stories.tsx
+++ packages/transition/stories/transition.stories.tsx
@@ -1,12 +1,13 @@
 import { useBoolean } from "@chakra-ui/hooks"
 import * as React from "react"
 import { SlideFade } from "../src/slide-fade"
+import { ScaleFade } from "../src/scale-fade"
 
 export default {
   title: "Transition / Offset Slide",
 }
 
-export const Basic = () => {
+export const slideFade = () => {
   const [open, { toggle }] = useBoolean(false)
   return (
     <>
@@ -31,6 +32,33 @@ export const Basic = () => {
         with desktop publishing software like Aldus PageMaker including versions
         of Lorem Ipsum.
       </SlideFade>
+    </>
+  )
+}
+
+export const scaleFade = () => {
+  const [open, { toggle }] = useBoolean(false)
+  return (
+    <>
+      <button onClick={toggle}>Toggle Scale</button>
+      <ScaleFade
+        in={open}
+        style={{
+          maxWidth: 400,
+          background: "red",
+          padding: 30,
+        }}
+      >
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </ScaleFade>
     </>
   )
 }
