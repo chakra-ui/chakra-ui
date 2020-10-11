0f3c2c7e8d9cc9219a1669cdaf46f1831b45ec7b
Keshav Bohra <keshav2016@gmail.com>
2020-10-11 19:49:33 +0530

    docs: fixed few spellings

--- packages/collapse/stories/collapse.stories.tsx
+++ packages/collapse/stories/collapse.stories.tsx
@@ -18,7 +18,7 @@ export function SampleSpring() {
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
+          remaining essentially unchanged. It was popularized in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
