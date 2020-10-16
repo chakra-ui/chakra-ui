5f06f2e5cc747183c69720b44418ddcda1bdc515
Andrew Ellis <awellis89@gmail.com>
2020-10-15 18:27:15 -0600

    fix(utils): properly filter undefined object values

--- packages/utils/src/object.ts
+++ packages/utils/src/object.ts
@@ -97,7 +97,7 @@ export function objectFilter<T extends Dict>(object: T, fn: FilterFn<T>) {
 }
 
 export const filterUndefined = (object: Dict) =>
-  objectFilter(object, (val) => val !== null)
+  objectFilter(object, (val) => val !== null && val !== undefined)
 
 export const objectKeys = <T extends Dict>(obj: T) =>
   (Object.keys(obj) as unknown) as (keyof T)[]
