413531d990f82d9d582db6378e9553b2965e8b97
Gerrit Alex <admin@gerritalex.de>
2020-10-23 23:40:35 +0200

    fix: linting styled-system types

--- packages/styled-system/src/css-get-unit.ts
+++
@@ -1,6 +0,0 @@
-declare module "css-get-unit" {
-  /**
-   * Gets unit from the CSS value without verifying. If there is no unit it will return `null`
-   */
-  export default function unit(val: string): string | null
-}
--- packages/styled-system/src/types.d.ts
+++ packages/styled-system/src/types.d.ts
@@ -1,3 +1,10 @@
+declare module "css-get-unit" {
+  /**
+   * Gets unit from the CSS value without verifying. If there is no unit it will return `null`
+   */
+  export default function unit(val: string): string | null
+}
+
 declare module "@styled-system/core" {
   import * as CSS from "csstype"
 
