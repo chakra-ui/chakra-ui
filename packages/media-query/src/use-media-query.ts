c075af573fe8587b9b916ef15aacceee50f85258
Gerrit Alex <admin@gerritalex.de>
2020-10-25 18:23:24 +0100

    fix: use-media-query initial state

--- packages/media-query/src/use-media-query.ts
+++ packages/media-query/src/use-media-query.ts
@@ -13,7 +13,9 @@ export function useMediaQuery(query: string | string[]): boolean[] {
   const isSupported = isBrowser && "matchMedia" in window
 
   const [matches, setMatches] = React.useState(
-    queries.map((query) => (isSupported ? !!window.matchMedia(query) : false)),
+    queries.map((query) =>
+      isSupported ? !!window.matchMedia(query).matches : false,
+    ),
   )
 
   useSafeLayoutEffect(() => {
