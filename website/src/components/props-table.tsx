f0758b9c6d0c66614a96a05b19cc01849c8823db
Tim Kolberger <tim@kolberger.eu>
2020-10-31 15:41:30 +0100

    feat(doc-gen): update more prop tables

--- website/src/components/props-table.tsx
+++ website/src/components/props-table.tsx
@@ -4,8 +4,27 @@ import * as ComponentProps from "@chakra-ui/props-docs"
 import MDXComponents from "./mdx-components"
 import { useMemo } from "react"
 
-export type PropsTableProps = { of: string }
-const PropsTable = ({ of }: PropsTableProps) => {
+export type PropsTableProps = {
+  /**
+   * displayName of the target component
+   */
+  of: string
+  /**
+   * prop names to omit
+   */
+  omit?: string[] | null
+  /**
+   * Render only given prop names
+   * Has precedence over `omit`
+   */
+  only?: string[] | null
+}
+
+const PropsTable = ({
+  of,
+  omit = ["isTruncated", "layerStyle", "noOfLines", "textStyle"],
+  only,
+}: PropsTableProps) => {
   const info: { props: Record<string, any> } = ComponentProps[of]
   if (!info || !info.props) {
     return null
@@ -13,8 +32,18 @@ const PropsTable = ({ of }: PropsTableProps) => {
 
   const entries = useMemo(
     () =>
-      Object.entries(info.props).sort(([a], [b]) => String(a).localeCompare(b)),
-    [info.props],
+      Object.entries(info.props)
+        .filter(([key]) => {
+          if (Array.isArray(only)) {
+            return only.includes(key)
+          }
+          if (Array.isArray(omit)) {
+            return !omit.includes(key)
+          }
+          return true
+        })
+        .sort(([a], [b]) => String(a).localeCompare(b)),
+    [info.props, omit, only],
   )
 
   return (
