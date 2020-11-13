2f230f87b405d2528e86e11b6f18cf4855a5fb0a
Tim Kolberger <tim@kolberger.eu>
2020-11-13 23:20:15 +0100

    refactor(blog): use LinkBox

--- website/src/components/blog-post-card.tsx
+++ website/src/components/blog-post-card.tsx
@@ -3,31 +3,15 @@ import {
   Box,
   Heading,
   HStack,
+  LinkBox,
   Tag,
   Text,
   useColorModeValue,
-  VStack,
 } from "@chakra-ui/react"
 import format from "date-fns/format"
 import parseISO from "date-fns/parseISO"
 import Link from "next/link"
 import { BlogPost } from "utils/get-blog-posts"
-
-// TODO replace with `breakout` prop
-// see https://github.com/chakra-ui/chakra-ui/pull/1717
-const breakoutLinkStyle = {
-  position: "static",
-  "&::before": {
-    content: "''",
-    cursor: "inherit",
-    display: "block",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-  },
-}
 
 export type BlogPostCardProps = {
   post: BlogPost
@@ -40,45 +24,46 @@ export const BlogPostCard: React.FC<BlogPostCardProps> = ({ post }) => {
   const readableDate = format(parseISO(post.date), "MMMM dd, yyyy")
 
   return (
-    <VStack
-      as="article"
-      position="relative"
-      p={4}
-      border="1px"
-      borderColor={borderColor}
-      borderRadius="md"
-      alignItems="flex-start"
-    >
-      <header>
-        <Heading as="h2" size="md">
-          <Link href={post.slug}>
-            <Box as="a" __css={breakoutLinkStyle} tabIndex={0} cursor="pointer">
-              {post.title}
-            </Box>
-          </Link>
-        </Heading>
-        <Box as="aside" fontSize="xs">
-          <Text as="span" color={secondaryColor}>
-            {readableDate}
-          </Text>
-          <Text as="span" color={secondaryColor} px={1} role="none">
-            |
-          </Text>
-          <Text as="span" color={secondaryColor}>
-            {post.readTimeMinutes} min read
-          </Text>
-        </Box>
-      </header>
-      <Text>{post.excerpt}</Text>
-      <footer>
-        <HStack as="ul" flexWrap="wrap">
-          {post.tags.map((tag) => (
-            <Tag as="li" key={tag} size="sm">
-              {tag}
-            </Tag>
-          ))}
-        </HStack>
-      </footer>
-    </VStack>
+    <Link href={post.slug} passHref>
+      <LinkBox
+        href={post.slug}
+        breakout
+        tabIndex={0}
+        cursor="pointer"
+        position="relative"
+        p={4}
+        border="1px"
+        borderColor={borderColor}
+        borderRadius="md"
+        alignItems="flex-start"
+      >
+        <header>
+          <Heading as="h2" size="md">
+            {post.title}
+          </Heading>
+          <Box as="aside" fontSize="xs">
+            <Text as="span" color={secondaryColor}>
+              {readableDate}
+            </Text>
+            <Text as="span" color={secondaryColor} px={1} role="none">
+              |
+            </Text>
+            <Text as="span" color={secondaryColor}>
+              {post.readTimeMinutes} min read
+            </Text>
+          </Box>
+        </header>
+        <Text>{post.excerpt}</Text>
+        <footer>
+          <HStack as="ul" flexWrap="wrap">
+            {post.tags.map((tag) => (
+              <Tag as="li" key={tag} size="sm">
+                {tag}
+              </Tag>
+            ))}
+          </HStack>
+        </footer>
+      </LinkBox>
+    </Link>
   )
 }
