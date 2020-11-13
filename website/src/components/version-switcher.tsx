01932d92b92934b6239cac53c7b3d921bd088c17
Segun Adebayo <joseshegs@gmail.com>
2020-11-13 12:24:58 +0400

    docs: update version switcher

--- website/src/components/version-switcher.tsx
+++ website/src/components/version-switcher.tsx
@@ -1,22 +1,35 @@
 import { Select, SelectProps, useColorModeValue } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 import React from "react"
 
+const versions = [
+  { label: "v1.0", url: "https://chakra-ui.com" },
+  { label: "v0.8", url: "https://v0.chakra-ui.com" },
+]
+
+const v1Url = versions[0].url
+
 function VersionSwitcher(props: SelectProps) {
+  const router = useRouter()
+
   return (
     <Select
       mr="1rem"
       variant="unstyled"
       fontWeight="semibold"
       color={useColorModeValue("gray.600", "whiteAlpha.600")}
-      defaultValue="chakra-ui.com"
+      value={v1Url}
       aria-label="Documentation version picker"
       onChange={(e) => {
-        window.location.href = e.target.value
+        router.push(e.target.value)
       }}
       {...props}
     >
-      <option value="chakra-ui.com">v1.0</option>
-      <option value="v0.chakra-ui.com">v0.8</option>
+      {versions.map(({ label, url }) => (
+        <option key={url} value={url}>
+          {label}
+        </option>
+      ))}
     </Select>
   )
 }
