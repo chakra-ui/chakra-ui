a1b9c07f13e21ee833e5052fa6d84ef2b35cc1bb
Jannes Mingram <zyclotrop.j@gmail.com>
2020-11-25 01:03:06 +1100

    chore(container): correct changeset and fix linting

--- packages/theme/src/components/container.ts
+++ packages/theme/src/components/container.ts
@@ -1,11 +1,10 @@
 const baseStyle = {
-    w: "100%",
-    mx: "auto",
-    maxW:"60ch",
-    px: "1rem",
+  w: "100%",
+  mx: "auto",
+  maxW: "60ch",
+  px: "1rem",
 }
 
 export default {
-    baseStyle,
+  baseStyle,
 }
-  
