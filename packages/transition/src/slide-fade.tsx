c4b14644ae80af1a3f77b3fa960172dee5c8183f
Joao Victor <joao.victor@rarolabs.com.br>
2020-11-29 19:46:43 -0300

    fix: adding offset type to number | string

--- packages/transition/src/slide-fade.tsx
+++ packages/transition/src/slide-fade.tsx
@@ -57,12 +57,12 @@ export interface SlideFadeProps extends HTMLMotionProps<"div"> {
    * The offset on the horizontal or `x` axis
    * @default 0
    */
-  offsetX?: string
+  offsetX?: string | number
   /**
    * The offset on the vertical or `y` axis
    * @default 8
    */
-  offsetY?: string
+  offsetY?: string | number
   /**
    * If `true`, the element will be transitioned back to the offset when it leaves.
    * Otherwise, it'll only fade out
