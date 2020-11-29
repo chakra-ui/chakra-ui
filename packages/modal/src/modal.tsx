73ada268564ee0b1cf8690aa88f03102e523748d
Jozef Mikuláš <dodo@redred.sk>
2020-11-29 02:50:23 +0100

    feat: added theming to ModalCloseButton

--- packages/modal/src/modal.tsx
+++ packages/modal/src/modal.tsx
@@ -496,12 +496,12 @@ export const ModalCloseButton = forwardRef<CloseButtonProps, "button">(
 
     const _className = cx("chakra-modal__close-btn", className)
 
+    const styles = useStyles()
+
     return (
       <CloseButton
         ref={ref}
-        position="absolute"
-        top="8px"
-        right="12px"
+        __css={styles.closeButton}
         className={_className}
         onClick={callAllHandlers(onClick, (event) => {
           event.stopPropagation()
