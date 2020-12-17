793bc9fa2f15579aa25bba2a46867c3e34212bf3
Segun Adebayo <joseshegs@gmail.com>
2020-12-17 12:56:49 +0400

    chore: upgrade framer-motion

--- packages/accordion/src/accordion.tsx
+++ packages/accordion/src/accordion.tsx
@@ -81,9 +81,10 @@ if (__DEV__) {
 
 type AccordionItemContext = Omit<UseAccordionItemReturn, "htmlProps">
 
-const [AccordionItemProvider, useAccordionItemContext] = createContext<
-  AccordionItemContext
->({
+const [
+  AccordionItemProvider,
+  useAccordionItemContext,
+] = createContext<AccordionItemContext>({
   name: "AccordionItemContext",
   errorMessage:
     "useAccordionItemContext: `context` is undefined. Seems you forgot to wrap the accordion item parts in `<AccordionItem />` ",
