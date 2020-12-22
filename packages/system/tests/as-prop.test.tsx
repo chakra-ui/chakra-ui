ac15f09237d801d68c5784dcad13515f3b1ef215
Tim Kolberger <tim@kolberger.eu>
2020-12-22 19:52:35 +0100

    test(typescript-autocomplete): add ChakraComponent with additional prop test

--- packages/system/tests/as-prop.test.tsx
+++ packages/system/tests/as-prop.test.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { chakra, ChakraComponent } from ".."
 
 /**
- * These tests should fail
+ * These tests should fail while type checking
  * if the typings for `ChakraComponent` change
  * and create a type regression
  */
@@ -12,7 +12,7 @@ describe("`as` prop typings", () => {
   type CompWithRequiredProps = { thisIsARequiredProp: boolean }
   const CompWithRequired = (props: CompWithRequiredProps) => null
 
-  it("should has correct types for the chakra factory", () => {
+  it("should have correct types for the chakra factory", () => {
     const div = <chakra.div />
     const divWithAsTag = <chakra.div as="img" src="/this-is-the-way.webp" />
     const divWithAsComp = <chakra.div as={Comp} />
@@ -24,7 +24,7 @@ describe("`as` prop typings", () => {
     expect(true).toBe(true)
   })
 
-  it("should has correct types for the ChakraComponent", () => {
+  it("should have correct types for the ChakraComponent", () => {
     const Div: ChakraComponent<"div"> = (props) => <chakra.div {...props} />
     const CustomComp: ChakraComponent<typeof Comp> = (props) => (
       <chakra.div as={Comp} {...props} />
@@ -34,6 +34,30 @@ describe("`as` prop typings", () => {
     const renderedCustomCompWithRequired = (
       <CustomCompWithRequired thisIsARequiredProp />
     )
+
+    // make jest happy
+    expect(true).toBe(true)
+  })
+
+  it("should have correct types for the ChakraComponent with additional props", () => {
+    const AdditionalPropComp: ChakraComponent<
+      "div",
+      { additionalProp: boolean }
+    > = ({ additionalProp, ...restProps }) => <chakra.div {...restProps} />
+
+    const renderedAdditionPropComp = <AdditionalPropComp additionalProp />
+
+    // make jest happy
+    expect(true).toBe(true)
+  })
+
+  it("should have correct types for the ChakraComponent with optional additional props", () => {
+    const OptionalAdditionalPropComp: ChakraComponent<
+      "div",
+      { additionalProp?: boolean }
+    > = ({ additionalProp, ...restProps }) => <chakra.div {...restProps} />
+
+    const renderedAdditionPropComp = <OptionalAdditionalPropComp />
 
     // make jest happy
     expect(true).toBe(true)
