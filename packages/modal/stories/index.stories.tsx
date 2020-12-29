6174fca363e7629b491a25c78c5b384216eff4b9
Anurag <hazru.anurag@gmail.com>
2020-12-29 13:12:43 +0530

    chore: remove multiple modal stories export

--- packages/modal/stories/index.stories.tsx
+++ packages/modal/stories/index.stories.tsx
@@ -2,7 +2,6 @@ import { PortalManager } from "@chakra-ui/portal"
 import * as React from "react"
 
 export * from "./modal.stories"
-export * from "./modal.stories"
 export * from "./drawer.stories"
 export * from "./focus-ref.stories"
 
