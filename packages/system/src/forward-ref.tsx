f3354bf416e905bec193624eee964a04ff9fc7b3
Tim Kolberger <tim@kolberger.eu>
2021-01-01 16:35:47 +0100

    feat(typescript-autocomplete): improve forward-ref types & move types to system.types

--- packages/system/src/forward-ref.tsx
+++ packages/system/src/forward-ref.tsx
@@ -3,39 +3,16 @@
  * for creating the base type definitions upon which we improved on
  */
 import * as React from "react"
+import { As, ComponentWithAs, PropsOf, RightJoinProps } from "./system.types"
 
-type OmitCommonProps<
-  Target,
-  OmitAdditionalProps extends keyof any = never
-> = Omit<Target, "transition" | "as" | "color" | OmitAdditionalProps>
-
-type RightJoinProps<
-  SourceProps extends object = {},
-  OverrideProps extends object = {}
-> = OmitCommonProps<SourceProps, keyof OverrideProps> & OverrideProps
-
-export type ComponentWithAs<
-  Component extends React.ElementType,
-  Props extends object = {}
-> = {
-  <AliasedComponent extends React.ElementType>(
-    props: RightJoinProps<React.ComponentProps<Component>, Props> &
-      RightJoinProps<React.ComponentProps<AliasedComponent>, Props> & {
-        as?: AliasedComponent
-      },
-  ): JSX.Element
-
-  displayName?: string
-  propTypes?: React.WeakValidationMap<any>
-  contextTypes?: React.ValidationMap<any>
-  defaultProps?: Partial<any>
-  id?: string
-}
-
-export function forwardRef<
-  Props extends object,
-  Component extends React.ElementType
->(component: React.ForwardRefRenderFunction<any, any>) {
+export function forwardRef<Props extends object, Component extends As>(
+  component: React.ForwardRefRenderFunction<
+    any,
+    RightJoinProps<PropsOf<Component>, Props> & {
+      as?: As
+    }
+  >,
+) {
   return (React.forwardRef(component) as unknown) as ComponentWithAs<
     Component,
     Props
--- packages/system/src/system.types.tsx
+++ packages/system/src/system.types.tsx
@@ -6,7 +6,6 @@ import {
 import { Dict } from "@chakra-ui/utils"
 import { Interpolation } from "@emotion/react"
 import * as React from "react"
-import { ComponentWithAs } from "./forward-ref"
 
 export interface ThemingProps {
   variant?: string
@@ -67,6 +66,43 @@ export type As<Props = any> = React.ElementType<Props>
  */
 export type PropsOf<T extends As> = React.ComponentPropsWithoutRef<T> & {
   as?: As
+}
+
+export type OmitCommonProps<
+  Target,
+  OmitAdditionalProps extends keyof any = never
+> = Omit<Target, "transition" | "as" | "color" | OmitAdditionalProps>
+
+export type RightJoinProps<
+  SourceProps extends object = {},
+  OverrideProps extends object = {}
+> = OmitCommonProps<SourceProps, keyof OverrideProps> & OverrideProps
+
+export type MergeWithAs<
+  ComponentProps extends object,
+  AsProps extends object,
+  AdditionalProps extends object = {},
+  AsComponent extends As = As
+> = RightJoinProps<ComponentProps, AdditionalProps> &
+  RightJoinProps<AsProps, AdditionalProps> & {
+    as?: AsComponent
+  }
+
+export type ComponentWithAs<Component extends As, Props extends object = {}> = {
+  <AsComponent extends As>(
+    props: MergeWithAs<
+      React.ComponentProps<Component>,
+      React.ComponentProps<AsComponent>,
+      Props,
+      AsComponent
+    >,
+  ): JSX.Element
+
+  displayName?: string
+  propTypes?: React.WeakValidationMap<any>
+  contextTypes?: React.ValidationMap<any>
+  defaultProps?: Partial<any>
+  id?: string
 }
 
 export interface ChakraComponent<T extends As, P = {}>
