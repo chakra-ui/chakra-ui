edff186b86a1f7dcf97dfd46cb96782098b35cca
Segun Adebayo <joseshegs@gmail.com>
2021-01-17 12:21:12 +0400

    feat: add missing grid template and row start-end props

--- packages/styled-system/src/config/flexbox.ts
+++ packages/styled-system/src/config/flexbox.ts
@@ -10,75 +10,187 @@ const config: Config = {
   flexWrap: true,
   flexDirection: true,
   flex: true,
+  flexFlow: true,
   flexGrow: true,
   flexShrink: true,
   flexBasis: t.sizes("flexBasis"),
   justifySelf: true,
   alignSelf: true,
   order: true,
+  placeItems: true,
+  placeContent: true,
+  placeSelf: true,
   flexDir: t.prop("flexDirection"),
 }
 
-/**
- * Types for flexbox related CSS properties
- */
 export interface FlexboxProps {
   /**
-   * The CSS `align-items` property
+   * The CSS `align-items` property.
+   *
+   * It defines the `align-self` value on all direct children as a group.
+   *
+   * - In Flexbox, it controls the alignment of items on the Cross Axis.
+   * - In Grid Layout, it controls the alignment of items on the Block Axis within their grid area.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/align-items)
    */
   alignItems?: ResponsiveValue<CSS.Property.AlignItems>
   /**
-   * The CSS `align-content` property
+   * The CSS `align-content` property.
+   *
+   * It defines the distribution of space between and around
+   * content items along a flexbox's cross-axis or a grid's block axis.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/align-content)
    */
   alignContent?: ResponsiveValue<CSS.Property.AlignContent>
   /**
-   * The CSS `justify-items` property
+   * The CSS `justify-items` property.
+   *
+   * It defines the default `justify-self` for all items of the box,
+   * giving them all a default way of justifying each box
+   * along the appropriate axis.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/justify-items)
    */
   justifyItems?: ResponsiveValue<CSS.Property.JustifyItems>
   /**
-   * The CSS `justify-content` property
+   * The CSS `justify-content` property.
+   *
+   * It defines how the browser distributes space between and around content items
+   * along the main-axis of a flex container, and the inline axis of a grid container.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/justify-content)
    */
   justifyContent?: ResponsiveValue<CSS.Property.JustifyContent>
   /**
-   * The CSS `flex-wrap` property
+   * The CSS `flex-wrap` property.
+   *
+   * It defines whether flex items are forced onto one line or
+   * can wrap onto multiple lines. If wrapping is allowed,
+   * it sets the direction that lines are stacked.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-wrap)
    */
   flexWrap?: ResponsiveValue<CSS.Property.FlexWrap>
   /**
-   * The CSS `flex-basis` property
+   * The CSS `flex-flow` property.
+   *
+   * It is a shorthand property for `flex-direction` and `flex-wrap`.
+   * It specifies the direction of a flex container, as well as its wrapping behavior.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-flow)
+   */
+  flexFlow?: ResponsiveValue<CSS.Property.FlexFlow>
+  /**
+   * The CSS `flex-basis` property.
+   *
+   * It defines the initial main size of a flex item.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-basis)
    */
   flexBasis?: ResponsiveValue<CSS.Property.FlexBasis<Length>>
   /**
-   * The CSS `flex-direction` property
+   * The CSS `flex-direction` property.
+   *
+   * It defines how flex items are placed in the flex container
+   * defining the main axis and the direction (normal or reversed).
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-direction)
    */
   flexDirection?: ResponsiveValue<CSS.Property.FlexDirection>
   /**
-   * The CSS `flex-direction` property
+   * The CSS `flex-direction` property.
+   *
+   * It defines how flex items are placed in the flex container
+   * defining the main axis and the direction (normal or reversed).
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-direction)
    */
   flexDir?: ResponsiveValue<CSS.Property.FlexDirection>
   /**
-   * The CSS `flex` property
+   * The CSS `flex` property.
+   *
+   * It defines how a flex item will grow or shrink
+   * to fit the space available in its flex container.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex)
    */
   flex?: ResponsiveValue<CSS.Property.Flex<Length>>
   /**
-   * The CSS `justify-self` property
+   * The CSS `justify-self` property.
+   *
+   * It defines the way a box is justified inside its
+   * alignment container along the appropriate axis.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-flow)
    */
   justifySelf?: ResponsiveValue<CSS.Property.JustifySelf>
   /**
-   * The CSS `align-self` property
+   * The CSS `align-self` property.
+   *
+   * It works like `align-items`, but applies only to a
+   * single flexbox item, instead of all of them.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/align-self)
    */
   alignSelf?: ResponsiveValue<CSS.Property.AlignSelf>
   /**
-   * The CSS `order` property
+   * The CSS `order` property.
+   *
+   * It defines the order to lay out an item in a flex or grid container.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/order)
    */
   order?: ResponsiveValue<CSS.Property.Order>
   /**
-   * The CSS `flex-grow` property
+   * The CSS `flex-grow` property.
+   *
+   * It defines how much a flexbox item should grow
+   * if there's space available.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-grow)
    */
   flexGrow?: ResponsiveValue<CSS.Property.FlexGrow>
   /**
-   * The CSS `flex-shrink` property
+   * The CSS `flex-shrink` property.
+   *
+   * It defines how much a flexbox item should shrink
+   * if there's not enough space available.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/flex-shrink)
    */
   flexShrink?: ResponsiveValue<CSS.Property.FlexShrink>
+  /**
+   * The CSS `place-items` property.
+   *
+   * It allows you to align items along both the block and
+   * inline directions at once (i.e. the align-items and justify-items properties)
+   * in a relevant layout system such as `Grid` or `Flex`.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/place-items)
+   */
+  placeItems?: ResponsiveValue<CSS.Property.PlaceItems>
+  /**
+   * The CSS `place-content` property.
+   *
+   * It allows you to align content along both the block and
+   * inline directions at once (i.e. the align-content and justify-content properties)
+   * in a relevant layout system such as Grid or Flexbox.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/place-content)
+   */
+  placeContent?: ResponsiveValue<CSS.Property.PlaceContent>
+  /**
+   * The CSS `place-self` property.
+   *
+   * It allows you to align an individual item in both the block and
+   * inline directions at once (i.e. the align-self and justify-self properties)
+   * in a relevant layout system such as Grid or Flexbox.
+   *
+   * @see [Mozilla Docs](https://developer.mozilla.org/docs/Web/CSS/place-self)
+   */
+  placeSelf?: ResponsiveValue<CSS.Property.PlaceSelf>
 }
 
 export const flexbox = system(config)
