53ff5581f00a0dde71969f96ea58ace342f5312c
Jozef Mikuláš <jmikulas.sk@gmail.com>
2021-01-25 03:02:50 +0100

    feat(system): use media queries for responsive variants

--- packages/system/src/use-style-config.ts
+++ packages/system/src/use-style-config.ts
@@ -1,14 +1,15 @@
-import { SystemStyleObject } from "@chakra-ui/styled-system"
+import { ResponsiveValue, SystemStyleObject } from "@chakra-ui/styled-system"
 import {
-  useBreakpointValue,
-  sanitizeResponsivePropValue,
-} from "@chakra-ui/media-query"
-import {
+  createMediaQueries,
   filterUndefined,
+  isArray,
+  isCustomBreakpoint,
   memoizedGet as get,
   mergeWith,
-  runIfFn,
+  objectToArrayNotation,
   omit,
+  runIfFn,
+  sanitizeResponsivePropValue,
 } from "@chakra-ui/utils"
 import { useMemo, useRef } from "react"
 import isEqual from "react-fast-compare"
@@ -33,18 +34,12 @@ export function useStyleConfig(themeKey: any, props: any = {}, opts: any = {}) {
   const { theme, colorMode } = useChakra()
   const themeStyleConfig = get(theme, `components.${themeKey}`)
   const styleConfig = styleConfigProp || themeStyleConfig
-
-  const orientation = useBreakpointValue(
-    sanitizeResponsivePropValue(props.orientation),
-  )
-  const size = useBreakpointValue(sanitizeResponsivePropValue(props.size))
-  const variant = useBreakpointValue(sanitizeResponsivePropValue(props.variant))
+  const { breakpoints } = theme
 
   const mergedProps = mergeWith(
     { theme, colorMode },
     styleConfig?.defaultProps ?? {},
     filterUndefined(omit(rest, ["children"])),
-    { orientation, size, variant },
   )
 
   /**
@@ -57,17 +52,21 @@ export function useStyleConfig(themeKey: any, props: any = {}, opts: any = {}) {
     if (styleConfig) {
       const baseStyles = runIfFn(styleConfig.baseStyle ?? {}, mergedProps)
 
-      const variants = runIfFn(
-        styleConfig.variants?.[mergedProps.variant] ?? {},
-        mergedProps,
-      )
+      const variantStyles = resolveResponsivePropStyles({
+        breakpoints,
+        responsiveValue: mergedProps.variant,
+        responsiveStyles: styleConfig.variants ?? {},
+        props: mergedProps,
+      })
 
-      const sizes = runIfFn(
-        styleConfig.sizes?.[mergedProps.size] ?? {},
-        mergedProps,
-      )
+      const sizeStyles = resolveResponsivePropStyles({
+        breakpoints,
+        responsiveValue: mergedProps.size,
+        responsiveStyles: styleConfig.sizes ?? {},
+        props: mergedProps,
+      })
 
-      const styles = mergeWith({}, baseStyles, sizes, variants)
+      const styles = mergeWith({}, baseStyles, sizeStyles, variantStyles)
 
       if (opts?.isMultiPart && styleConfig.parts) {
         styleConfig.parts.forEach((part: string) => {
@@ -83,9 +82,62 @@ export function useStyleConfig(themeKey: any, props: any = {}, opts: any = {}) {
     }
 
     return stylesRef.current
-  }, [styleConfig, mergedProps, opts?.isMultiPart])
+  }, [styleConfig, breakpoints, mergedProps, opts?.isMultiPart])
 }
 
 export function useMultiStyleConfig(themeKey: string, props: any) {
   return useStyleConfig(themeKey, props, { isMultiPart: true })
+}
+
+interface ResolveResponsivePropStylesOptions {
+  responsiveValue: ResponsiveValue<string>
+  responsiveStyles: Record<string, SystemStyleObject>
+  breakpoints: string[]
+  props: Record<string, any>
+}
+
+function resolveResponsivePropStyles({
+  responsiveValue,
+  responsiveStyles,
+  breakpoints,
+  props,
+}: ResolveResponsivePropStylesOptions) {
+  const sanitizedValue = sanitizeResponsivePropValue(responsiveValue)
+
+  // ["base", "sm", "md", "lg", "xl", "2xl"]
+  const breakpointKeys = Object.keys(breakpoints).filter(isCustomBreakpoint)
+
+  const responsiveArray = isArray(sanitizedValue)
+    ? sanitizedValue
+    : objectToArrayNotation(sanitizedValue, breakpointKeys)
+
+  const mediaQueries = createMediaQueries(breakpoints)
+
+  const resolvedStyles = Object.fromEntries(
+    responsiveArray.flatMap((name, breakpointIndex) => {
+      if (name == null) {
+        return []
+      }
+
+      const styles = runIfFn(responsiveStyles[name as string] ?? {}, props)
+
+      const { minWidth } = mediaQueries[breakpointIndex]
+
+      const nextValueBreakpointIndex = responsiveArray.findIndex(
+        (value, index) => index > breakpointIndex && value != null,
+      )
+
+      const mediaMaxWidth =
+        nextValueBreakpointIndex !== -1
+          ? mediaQueries[nextValueBreakpointIndex - 1].mediaMaxWidth
+          : null
+      const maxWidthQuery =
+        mediaMaxWidth != null ? ` and (max-width: ${mediaMaxWidth})` : ``
+      const mediaQuery = `@media (min-width: ${minWidth})${maxWidthQuery}`
+
+      return [[mediaQuery, styles]]
+    }),
+  )
+
+  return resolvedStyles
 }
