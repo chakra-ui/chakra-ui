8ac5175087c87d72822c2a018b4e4a93e17a2d70
Tim Kolberger <tim@kolberger.eu>
2021-01-30 22:25:18 +0100

    chore: move build, test and lint to separate job

--- tooling/cli/test/format-with-prettier.test.ts
+++ tooling/cli/test/format-with-prettier.test.ts
@@ -1,6 +1,6 @@
 import { formatWithPrettierIfAvailable } from "../src/utils/format-with-prettier"
 
-describe.skip("Format With Prettier", () => {
+describe("Format With Prettier", () => {
   it("should format with prettier", async () => {
     const content =
       "export interface ThemeTypings { fonts: 'test1'|'test2'|'test3'}"
