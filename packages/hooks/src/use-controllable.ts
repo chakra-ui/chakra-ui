a9afde320b862276299259a00aa4200644131f2e
Mark Chandler <mark@chanchan.io>
2021-02-15 14:29:40 -0500

    WIP

--- packages/hooks/src/use-controllable.ts
+++ packages/hooks/src/use-controllable.ts
@@ -1,11 +1,73 @@
+import { Machine, assign } from "xstate"
+import { useMachine } from "@xstate/react"
 import { runIfFn } from "@chakra-ui/utils"
 import * as React from "react"
 import { useCallbackRef } from "./use-callback-ref"
 
+interface ControllablePropSchema {
+  states: {
+    unknown: {}
+    controlled: {}
+    uncontrolled: {}
+  }
+}
+interface ControllablePropContext<T> {
+  value?: T
+}
+
+type ControllablePropEvent<T> = { type: "SYNC"; prop?: T; state: T }
+
+const createControllablePropMachine = <T>() =>
+  Machine<
+    ControllablePropContext<T>,
+    ControllablePropSchema,
+    ControllablePropEvent<T>
+  >(
+    {
+      id: "controllable-prop",
+      initial: "unknown",
+      on: {
+        SYNC: [
+          { target: "controlled", cond: "hasProp" },
+          { target: "uncontrolled" },
+        ],
+      },
+      states: {
+        unknown: {},
+        controlled: {
+          entry: "setPropAsValue",
+        },
+        uncontrolled: {
+          entry: "setStateAsValue",
+        },
+      },
+    },
+    {
+      guards: {
+        hasProp: (_context, event) => typeof event.prop !== "undefined",
+      },
+      actions: {
+        setPropAsValue: assign({
+          value: (_context, event) => event.prop,
+        }),
+        setStateAsValue: assign({
+          value: (_context, event) => event.state,
+        }),
+      },
+    },
+  )
+
 export function useControllableProp<T>(prop: T | undefined, state: T) {
-  const isControlled = prop !== undefined
-  const value = isControlled && typeof prop !== "undefined" ? prop : state
-  return [isControlled, value] as const
+  const [currentState, send] = useMachine(createControllablePropMachine())
+
+  React.useEffect(() => {
+    send({ type: "SYNC", prop, state })
+  }, [send, prop, state])
+
+  return [
+    currentState.matches("controlled"),
+    currentState.context.value,
+  ] as const
 }
 
 export interface UseControllableStateProps<T> {
@@ -26,6 +88,55 @@ export interface UseControllableStateProps<T> {
    */
   name?: string
 }
+
+interface ControllableStateSchema {
+  states: {
+    init: {}
+    controlled: {}
+    uncontrolled: {}
+  }
+}
+
+interface ControllableStateContext<T> {
+  state: T
+  defaultValue: T
+}
+
+type ControllableStateEvent<T> = SyncEvent<T> | UpdateEvent<T>
+type SyncEvent<T> = { type: "SYNC"; value?: T; defaultValue: T }
+type UpdateEvent<T> = { type: "UPDATE"; value: T }
+
+const createControllableStateMachine = <T>({
+  value,
+  defaultValue,
+}: {
+  value?: T
+  defaultValue: T
+}) =>
+  Machine<
+    ControllableStateContext<T>,
+    ControllableStateSchema,
+    ControllableStateEvent<T>
+  >(
+    {
+      id: "controllable-state",
+      initial: "init",
+      context: {
+        defaultValue,
+        state: value !== undefined ? value : defaultValue,
+      },
+      states: {
+        init: {
+          always: [{ target: "controlled", cond: "hasValue" }],
+        },
+        controlled: {},
+        uncontrolled: {},
+      },
+    },
+    {
+      actions: {},
+    },
+  )
 
 /**
  * React hook for using controlling component state.
