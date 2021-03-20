96eb7c7515c4ae970e9072a1adc3780a92c8efb5
Segun Adebayo <joseshegs@gmail.com>
2021-03-21 02:22:32 +0400

    revert: jest config update for popper

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  verbose: true,
   preset: "ts-jest",
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)?$": "ts-jest/dist",
   },
-  transformIgnorePatterns: ["/node_modules/(?!@popperjs/.*)"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   globals: {
     "ts-jest": {
