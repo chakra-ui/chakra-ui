3b0741af29d6d340b623b0aa94e67924a79e65ef
codebender828 <jbakebwa@163.com>
2021-03-27 09:42:15 +0800

    chore: move react types to react-utils package

--- packages/react-utils/src/index.ts
+++ packages/react-utils/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./react-utils"
 export * from "./dom"
+export * from "./types"
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -18,43 +18,4 @@ export type FunctionArguments<T extends Function> = T extends (
 
 export type Dict<T = any> = Record<string, T>
 
-export type MaybeRenderProp<P> =
-  | React.ReactNode
-  | ((props: P) => React.ReactNode)
-
 export type Booleanish = boolean | "true" | "false"
-
-export type StringOrNumber = string | number
-
-export type HTMLProps<T = any> = Omit<
-  React.HTMLAttributes<T>,
-  "color" | "width" | "height"
-> &
-  React.RefAttributes<T>
-
-export type PropGetter<T extends HTMLElement = any, P = {}> = (
-  props?: Merge<HTMLProps<T>, P>,
-  ref?: React.Ref<any> | React.RefObject<any>,
-) => Merge<HTMLProps<T>, P>
-
-export type EventKeys =
-  | "ArrowDown"
-  | "ArrowUp"
-  | "ArrowLeft"
-  | "ArrowRight"
-  | "Enter"
-  | "Space"
-  | "Tab"
-  | "Backspace"
-  | "Control"
-  | "Meta"
-  | "Home"
-  | "End"
-  | "PageDown"
-  | "PageUp"
-  | "Delete"
-  | "Escape"
-  | " "
-  | "Shift"
-
-export type EventKeyMap = Partial<Record<EventKeys, React.KeyboardEventHandler>>
