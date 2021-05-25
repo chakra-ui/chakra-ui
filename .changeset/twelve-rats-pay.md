21ca6725d50c091b2a0e91e625d10b7221359db5
Segun Adebayo <joseshegs@gmail.com>
2021-05-25 16:52:08 +0400

    chore(machine): change warn to error for invalid state node

---
+++ .changeset/twelve-rats-pay.md
@@ -0,0 +1,5 @@
+---
+"@chakra-ui/machine": patch
+---
+
+Remove warning for self transitions and add error for invalid state nodes
--- packages/machine/src/machine.ts
+++ packages/machine/src/machine.ts
@@ -667,19 +667,16 @@ export class Machine<
     type TransitionDfn = S.TransitionDefinition<TContext, TState, TEvent>
 
     const event = toEvent(evt) as TEvent
+
     if (!stateNode && !this.config.on) {
-      console.warn("[machine]: state doesn't have a definition")
-      return
+      throw new Error("[machine]: state node has no definition")
     }
 
     const _transition =
       stateNode?.on?.[event.type] ?? this.config.on?.[event.type]
 
     const transition = toTransition(_transition, this.state.current)
-    if (!transition) {
-      console.warn("[machine]: Could not determine transition")
-      return
-    }
+    if (!transition) return
 
     let next = this.getNextState(event, transition as TransitionDfn)
     //@ts-ignore
