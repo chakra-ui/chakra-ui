bcc75546c86a0452987552a683ef76f765b008d4
Daniel Schulz <daniel@schulz.codes>
2021-08-03 03:50:50 +0200

    chore(docs): switch to next-mdx-remote

--- website/pages/changelog.tsx
+++ website/pages/changelog.tsx
@@ -1,8 +1,38 @@
 import React from "react"
-import MarkdownContent from "../../CHANGELOG.md"
+import { MDXRemote } from "next-mdx-remote"
+import { MDXComponents } from "components/mdx-components"
+import { InferGetStaticPropsType } from "next"
+import Layout from "layouts"
+import { loadMdx } from "utils/load-mdx"
 
-function Changelog() {
-  return <MarkdownContent />
+function Changelog({
+  mdxSource,
+  frontMatter,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout frontMatter={frontMatter}>
+      <MDXRemote {...mdxSource} components={MDXComponents} />
+    </Layout>
+  )
 }
 
+export async function getStaticProps() {
+  const changelogPath = ["..", "CHANGELOG.md"].join("/")
+  const page = await loadMdx(changelogPath)
+  if (!page) {
+    console.warn(`No changelog found`)
+  }
+
+  const { mdxSource: processedMdxSource, ...processedFrontmatter } = page
+  // const string = renderToString(
+  //   <MDXRemote {...mdxSource} components={MDXComponents} />,
+  // )
+  // console.log(getHeadings(string))
+  return {
+    props: {
+      mdxSource: processedMdxSource,
+      frontMatter: processedFrontmatter,
+    },
+  }
+}
 export default Changelog
