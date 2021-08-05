aed13a0bee2e6754dd9562be1b4fb5d24a43957d
Segun Adebayo <joseshegs@gmail.com>
2021-08-05 19:55:33 +0400

    chore: update tooling pkgs

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = {
   plugins: [
     "@chakra-ui/babel-plugin",
     "babel-plugin-annotate-pure-calls",
-    "@babel/plugin-proposal-class-properties",
-    ["@babel/plugin-proposal-private-methods", { loose: false }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
   presets: [
     "@babel/preset-typescript",
@@ -22,17 +21,3 @@ module.exports = {
     ],
   ],
 }
-
-/**
-* Though the "loose" option was set to "true" in your @babel/preset-env config,
-it will not be used for @babel/plugin-proposal-private-property-in-object since
-the "loose" mode option was set to "false" for @babel/plugin-proposal-private-methods.
-
-The "loose" option must be the same for
-@babel/plugin-proposal-class-properties,
-@babel/plugin-proposal-private-methods,
-@babel/plugin-proposal-private-property-in-object (when they are enabled):
-
-you can silence this warning by explicitly adding
-        ["@babel/plugin-proposal-private-property-in-object", { "loose": false }]
-*/
