48d4dc2d49a24f41783aca16eff18625064f9105
Segun Adebayo <joseshegs@gmail.com>
2021-09-11 15:06:07 +0400

    chore: revert and update component

--- website/src/components/mdx-components/egghead-course-banner/index.tsx
+++ website/src/components/mdx-components/egghead-course-banner/index.tsx
@@ -1,65 +1,61 @@
 import {
+  Box,
+  Button,
+  DarkMode,
   Heading,
-  Text,
-  VStack,
-  HStack,
   Image,
+  Flex,
   LinkBox,
   LinkOverlay,
+  Text,
 } from "@chakra-ui/react"
+import { FaPlayCircle } from "react-icons/fa"
 
 const EggheadCourseBanner = ({ href }) => {
   return (
-    <LinkBox role="group">
-      <VStack
-        transitionProperty="transform"
-        transitionDuration="250ms"
-        transitionTimingFunction="ease-out"
-        _groupHover={{
-          transform: "scale(1.03, 1.03)",
-        }}
-        p={{ base: 6, lg: 16 }}
-        spacing={12}
-        alignItems="flex-start"
-        rounded="lg"
-        bg="gray.900"
-        mt={6}
-      >
-        <VStack spacing={4} color="gray.300" alignItems="flex-start">
+    <LinkBox role="group" mt="10">
+      <Flex align="center" rounded="3xl" bg="gray.900" padding="8">
+        <Image
+          display={{ base: "none", lg: "block" }}
+          flexShrink={0}
+          w="20"
+          mr={{ lg: "10" }}
+          src="/egghead-banner/egghead.svg"
+          alt="Egghead Logo"
+        />
+        <Box>
           <LinkOverlay href={href} target="_blank" color="white">
-            <Heading fontSize={{ base: "3xl", lg: "5xl" }}>
-              Build a Modern User Interface with Chakra UI
-            </Heading>
+            <Flex align="center" mb="4">
+              <Heading size="md">
+                Build a Modern User Interface with Chakra UI
+              </Heading>
+              <Image
+                display={{ lg: "none" }}
+                ml="3"
+                flexShrink={0}
+                w="10"
+                src="/egghead-banner/egghead.svg"
+                alt="Egghead Logo"
+              />
+            </Flex>
           </LinkOverlay>
-          <Text>
-            In this course, you will learn how to create a flexible user
-            interface using Chakra UI.
+          <Text mb="5" color="gray.400">
+            In this free course, you will learn the basics of Chakra UI and how
+            to build well-designed, accessible user interfaces with speed!
           </Text>
-          <Text>
-            You will learn to use Chakra UI's default style props and components
-            to easily build out your application, but you will also see how you
-            can easily override the defaults or even create custom components to
-            suit the needs of your application.
-          </Text>
-        </VStack>
-        <HStack spacing={{ base: 4, lg: 12 }}>
-          <Image
-            w={{ base: 12, lg: 20 }}
-            src="/egghead-banner/chakra.svg"
-            alt="Chakra Logo"
-          />
-          <Image
-            w={{ base: 12, lg: 20 }}
-            src="/egghead-banner/egghead.svg"
-            alt="Egghead Logo"
-          />
-          <Image
-            w={{ base: 12, lg: 20 }}
-            src="/egghead-banner/lazar.png"
-            alt="Lazar's Avatar"
-          />
-        </HStack>
-      </VStack>
+          <DarkMode>
+            <Button
+              as="a"
+              target="_blank"
+              href="https://egghead.io/courses/build-a-modern-user-interface-with-chakra-ui-fac68106"
+              leftIcon={<FaPlayCircle />}
+              color="white"
+            >
+              Start learning
+            </Button>
+          </DarkMode>
+        </Box>
+      </Flex>
     </LinkBox>
   )
 }
