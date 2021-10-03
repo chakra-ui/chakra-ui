236efcbf562a966d5a3fcd0a778ee404b379d41d
Tim Kolberger <tim@kolberger.eu>
2021-10-03 17:03:59 +0200

    fix(cli): resolve ts-config from dirname

--- tooling/cli/src/scripts/read-theme-file.worker.ts
+++ tooling/cli/src/scripts/read-theme-file.worker.ts
@@ -30,8 +30,9 @@ async function importTheme(path: string) {
 async function readTheme(themeFilePath: string) {
   const cwd = process.cwd()
   const absoluteThemePath = path.join(cwd, themeFilePath)
+  const absoluteThemeDir = path.dirname(absoluteThemePath)
 
-  const tsConfig = tsConfigPaths.loadConfig(absoluteThemePath)
+  const tsConfig = tsConfigPaths.loadConfig(absoluteThemeDir)
   if (tsConfig.resultType === "success") {
     tsNode.register({
       // use the TS projects own tsconfig file
