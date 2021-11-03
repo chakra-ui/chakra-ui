b7f06a0c052ae24dd50ea989345623ca5502a2c8
Segun Adebayo <joseshegs@gmail.com>
2021-11-04 01:16:35 +0400

    chore: add src to pkg files

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   plugins: [
     "@chakra-ui/babel-plugin",
-    "@babel/plugin-proposal-class-properties",
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
-    "@babel/preset-env",
+    [
+      "@babel/preset-env",
+      {
+        bugfixes: true,
+        modules: false,
+        loose: true,
+        targets: "> 1%, not dead, not ie 11, not op_mini all",
+      },
+    ],
   ],
 }
