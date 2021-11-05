f9bdeec0e274bb1f12f475f7e67441ca80c00520
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-11-05 12:48:01 -0700

    fix(select): allow icon spacing to be customized via theme
    Fixes #4938
    Fixes #1146

--- packages/select/tests/select.test.tsx
+++ packages/select/tests/select.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { render, testA11y } from "@chakra-ui/test-utils"
 import { FormControl } from "@chakra-ui/form-control"
+import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react"
 import { Select } from "../src"
 
 test("should pass a11y check", async () => {
@@ -67,3 +68,60 @@ test("renders in disabled state if wrapped by FormControl with isDisabled=true",
   expect(select).toBeDisabled()
   expect(iconWrapper).toHaveAttribute("data-disabled", "")
 })
+
+describe.each(Object.keys(theme.components.Select.sizes))(
+  "icon spacing for '%s' size",
+  (size) => {
+    const defaultSpacing = "2rem"
+
+    test("defaults icon spacing in theme", () => {
+      const { container } = render(<Select size={size} />)
+      const select = container.querySelector("select") as HTMLElement
+
+      expect(select).toHaveStyle({ "padding-inline-end": defaultSpacing })
+    })
+
+    test("defaults icon spacing in component", () => {
+      const theme = extendTheme({
+        components: {
+          Select: {
+            sizes: {
+              lg: { field: { paddingInlineEnd: null, px: null } },
+              md: { field: { paddingInlineEnd: null, px: null } },
+              sm: { field: { paddingInlineEnd: null, px: null } },
+              xs: { field: { paddingInlineEnd: null, px: null } },
+            },
+          },
+        },
+      })
+      const { container } = render(
+        <ChakraProvider theme={theme}>
+          <Select />
+        </ChakraProvider>,
+      )
+      const select = container.querySelector("select") as HTMLElement
+
+      expect(select).toHaveStyle({ "padding-inline-end": defaultSpacing })
+    })
+
+    test("allows icon spacing to be overridden in theme", () => {
+      const sizes = {
+        lg: { field: { paddingInlineEnd: "8px" } },
+        md: { field: { paddingInlineEnd: "6px" } },
+        sm: { field: { paddingInlineEnd: "4px" } },
+        xs: { field: { paddingInlineEnd: "2px" } },
+      }
+      const theme = extendTheme({ components: { Select: { sizes } } })
+      const { container } = render(
+        <ChakraProvider theme={theme}>
+          <Select size={size} />
+        </ChakraProvider>,
+      )
+      const select = container.querySelector("select") as HTMLElement
+
+      expect(select).toHaveStyle({
+        "padding-inline-end": sizes[size].field.paddingInlineEnd,
+      })
+    })
+  },
+)
