fdb08cc58d6b37bb46b812d0c66555c72a0f55bf
Ross Moody <fiftyfivehis@gmail.com>
2021-11-23 13:01:31 -0800

    Update packages/toast/src/use-toast.tsx
    Co-authored-by: Segun Adebayo <joseshegs@gmail.com>

--- packages/toast/src/use-toast.tsx
+++ packages/toast/src/use-toast.tsx
@@ -218,7 +218,7 @@ export function useToast(options?: UseToastOptions) {
       toggleColorMode,
       defaultOptions: toastOptions.current,
     })
-  }, [theme, setColorMode, toggleColorMode, colorMode, toastOptions])
+  }, [theme, setColorMode, toggleColorMode, colorMode])
 }
 
 export default useToast
