94e4e4d3de2dda2027caff4b20b1b612287a8bb8
Tim Kolberger <tim@kolberger.eu>
2021-12-27 10:47:53 +0100

    fix(cli): replace runtime require of prettier with import (#5290)

--- tooling/cli/test/format-with-prettier.test.ts
+++ tooling/cli/test/format-with-prettier.test.ts
@@ -14,18 +14,4 @@ describe("Format With Prettier", () => {
       "
     `)
   })
-
-  it("should not fail if prettier is not available", async () => {
-    jest.mock("prettier", () => {
-      throw new Error("module not found")
-    })
-
-    const content =
-      "export interface ThemeTypings { fonts: 'test1'|'test2'|'test3'}"
-    const pretty = await formatWithPrettierIfAvailable(content)
-
-    expect(pretty).toMatchInlineSnapshot(
-      `"export interface ThemeTypings { fonts: 'test1'|'test2'|'test3'}"`,
-    )
-  })
 })
