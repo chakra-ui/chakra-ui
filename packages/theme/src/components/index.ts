cf5d1cfec04453bb24aaba5d9cb16ee1a76ee509
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2022-01-11 14:29:24 +0100

    refactor(theme): Simplify exports
    Remove more than half the code by using more modern syntax.

--- packages/theme/src/components/index.ts
+++ packages/theme/src/components/index.ts
@@ -1,83 +1,41 @@
-import Accordion from "./accordion"
-import Alert from "./alert"
-import Avatar from "./avatar"
-import Badge from "./badge"
-import Breadcrumb from "./breadcrumb"
-import Button from "./button"
-import Checkbox from "./checkbox"
-import CloseButton from "./close-button"
-import Code from "./code"
-import Container from "./container"
-import Divider from "./divider"
-import Drawer from "./drawer"
-import Editable from "./editable"
-import Form from "./form"
-import FormLabel from "./form-label"
-import Heading from "./heading"
-import Input from "./input"
-import Kbd from "./kbd"
-import Link from "./link"
-import List from "./list"
-import Menu from "./menu"
-import Modal from "./modal"
-import NumberInput from "./number-input"
-import PinInput from "./pin-input"
-import Popover from "./popover"
-import Progress from "./progress"
-import Radio from "./radio"
-import Select from "./select"
-import Skeleton from "./skeleton"
-import SkipLink from "./skip-link"
-import Slider from "./slider"
-import Spinner from "./spinner"
-import Stat from "./stat"
-import Switch from "./switch"
-import Table from "./table"
-import Tabs from "./tabs"
-import Tag from "./tag"
-import Textarea from "./textarea"
-import Tooltip from "./tooltip"
-import FormError from "./form-error"
+export { default as Accordion } from "./accordion"
+export { default as Alert } from "./alert"
+export { default as Avatar } from "./avatar"
+export { default as Badge } from "./badge"
+export { default as Breadcrumb } from "./breadcrumb"
+export { default as Button } from "./button"
+export { default as Checkbox } from "./checkbox"
+export { default as CloseButton } from "./close-button"
+export { default as Code } from "./code"
+export { default as Container } from "./container"
+export { default as Divider } from "./divider"
+export { default as Drawer } from "./drawer"
+export { default as Editable } from "./editable"
+export { default as Form } from "./form"
+export { default as FormError } from "./form-error"
+export { default as FormLabel } from "./form-label"
+export { default as Heading } from "./heading"
+export { default as Input } from "./input"
+export { default as Kbd } from "./kbd"
+export { default as Link } from "./link"
+export { default as List } from "./list"
+export { default as Menu } from "./menu"
+export { default as Modal } from "./modal"
+export { default as NumberInput } from "./number-input"
+export { default as PinInput } from "./pin-input"
+export { default as Popover } from "./popover"
+export { default as Progress } from "./progress"
+export { default as Radio } from "./radio"
+export { default as Select } from "./select"
+export { default as Skeleton } from "./skeleton"
+export { default as SkipLink } from "./skip-link"
+export { default as Slider } from "./slider"
+export { default as Spinner } from "./spinner"
+export { default as Stat } from "./stat"
+export { default as Switch } from "./switch"
+export { default as Table } from "./table"
+export { default as Tabs } from "./tabs"
+export { default as Tag } from "./tag"
+export { default as Textarea } from "./textarea"
+export { default as Tooltip } from "./tooltip"
 
-export default {
-  Accordion,
-  Alert,
-  Avatar,
-  Badge,
-  Breadcrumb,
-  Button,
-  Checkbox,
-  CloseButton,
-  Code,
-  Container,
-  Divider,
-  Drawer,
-  Editable,
-  Form,
-  FormLabel,
-  Heading,
-  Input,
-  Kbd,
-  Link,
-  List,
-  Menu,
-  Modal,
-  NumberInput,
-  PinInput,
-  Popover,
-  Progress,
-  Radio,
-  Select,
-  Skeleton,
-  SkipLink,
-  Slider,
-  Spinner,
-  Stat,
-  Switch,
-  Table,
-  Tabs,
-  Tag,
-  Textarea,
-  Tooltip,
-  FormError,
-}
