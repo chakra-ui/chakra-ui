fb74521bc6c27274f1c5b673617f6c5bc0d6df90
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2022-03-01 23:08:07 +0100

    feat(prose): added mode element styles

--- packages/prose/stories/prose.stories.tsx
+++ packages/prose/stories/prose.stories.tsx
@@ -7,7 +7,7 @@ export default {
   title: "Components / Typography / Prose",
   decorators: [
     (Story: any) => (
-      <Container mt="40px">
+      <Container w="full" maxW="container.lg" mt="40px">
         <Story />
       </Container>
     ),
@@ -36,12 +36,121 @@ export const basic = () => (
         interdum. Fusce vitae massa non risus congue tincidunt. Pellentesque
         maximus elit quis eros lobortis dictum.
       </p>
+      <hr />
       <p>
         Fusce placerat ipsum vel sollicitudin imperdiet. Morbi vulputate non
         diam at consequat. Donec vitae sem eu arcu auctor scelerisque vel in
         turpis. Pellentesque dapibus justo dui, quis egestas sapien porttitor
         in.
       </p>
+      <h3>Blockquotes</h3>
+      <blockquote>This is a good looking blockquote!</blockquote>
+      <p>And it can span into multiple lines:</p>
+      <blockquote>
+        Fusce placerat ipsum vel sollicitudin imperdiet. Morbi vulputate non
+        diam at consequat. Donec vitae sem eu arcu auctor scelerisque vel in
+        turpis. Pellentesque dapibus justo dui, quis egestas sapien porttitor
+        in.
+      </blockquote>
+      <p>
+        There&apos;s also <strong>strong</strong>, <b>b</b>, <em>em</em> support
+        as well! But, let&apos;s display some code!
+      </p>
+      <h3>Code</h3>
+      <pre>
+        <code>
+          {`<article>
+  <h4>Title Heading 4</h4>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at dolor nec ex rutrum semper. Praesent ultricies purus eget lectus
+    tristique egestas ac in lacus. Nulla eleifend lorem risus, sit amet dictum nisi gravida eget. Suspendisse odio sem, scelerisque congue
+    luctus nec, scelerisque ultrices orci. Praesent tincidunt, risus ut commodo cursus, ligula orci tristique justo, vitae sollicitudin lacus risus dictum orci.
+  </p>
+</article>`}
+        </code>
+      </pre>
+      <p>
+        Sometimes you&apos;d want to use <code>inline code</code>, and that
+        would be just fine!
+      </p>
+      <h3>Links</h3>
+      <p>
+        If you want to learn more,{" "}
+        <a href="https://chakra-ui.com">check out our docsite</a>!
+      </p>
+      <h3>Figure</h3>
+      <p>
+        Fusce placerat ipsum vel sollicitudin imperdiet. Morbi vulputate non
+        diam at consequat. Donec vitae sem eu arcu auctor scelerisque vel in
+        turpis. Pellentesque dapibus justo dui, quis egestas sapien porttitor
+        in.
+      </p>
+      <figure>
+        <img
+          alt="Lagos, Nigeria"
+          src="https://images.unsplash.com/photo-1593717191400-84f38ee95485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2048&q=100"
+        />
+        <figcaption>Lagos, Nigeria</figcaption>
+      </figure>
+      <p>
+        Fusce placerat ipsum vel sollicitudin imperdiet. Morbi vulputate non
+        diam at consequat. Donec vitae sem eu arcu auctor scelerisque vel in
+        turpis. Pellentesque dapibus justo dui, quis egestas sapien porttitor
+        in.
+      </p>
+      <h3>Lists</h3>
+      <p>Let's look at some unordered lists. Things to buy:</p>
+      <ul>
+        <li>Milk</li>
+        <li>Eggs</li>
+        <li>Bread</li>
+        <li>Chakra UI Pro license</li>
+      </ul>
+      <p>And some ordered lists. Things to do:</p>
+      <ol>
+        <li>Pay the bills</li>
+        <li>Walk the dog</li>
+        <li>Take out trash</li>
+      </ol>
+      <h3>Tables</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Role</th>
+            <th>GitHub Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Segun Adebayo</td>
+            <td>Founder</td>
+            <td>segunadebayo</td>
+          </tr>
+          <tr>
+            <td>Tim Kolberger</td>
+            <td>TypeScript Wizard</td>
+            <td>TimKolberger</td>
+          </tr>
+          <tr>
+            <td>Lazar Nikolov</td>
+            <td>Trouble maker</td>
+            <td>nikolovlazar</td>
+          </tr>
+          <tr>
+            <td>Folasade Agbaje</td>
+            <td>Queen</td>
+            <td>estheragbaje</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>Name</th>
+            <th>Role</th>
+            <th>GitHub Profile</th>
+          </tr>
+        </tfoot>
+      </table>
     </Prose>
   </>
 )
--- packages/theme/src/components/prose.ts
+++ packages/theme/src/components/prose.ts
@@ -1,4 +1,4 @@
-import type { SystemStyleFunction } from "@chakra-ui/theme-tools"
+import { mode, SystemStyleFunction } from "@chakra-ui/theme-tools"
 
 import { getComponentBaseStyle } from ".."
 
@@ -9,26 +9,136 @@ const baseStyle: SystemStyleFunction = (props) => {
   return {
     h1: {
       ...headingBase,
-      fontSize: "7xl",
+      fontSize: { base: "4xl", md: "5xl" },
+      mb: { base: 8, md: 10 },
+      fontWeight: 800,
     },
     h2: {
       ...headingBase,
-      fontSize: "5xl",
+      fontSize: { base: "2xl", md: "3xl" },
+      mt: { base: 12, md: 14 },
+      mb: { base: 6, md: 8 },
     },
     h3: {
       ...headingBase,
-      fontSize: "3xl",
+      fontSize: { base: "xl", md: "2xl" },
+      fontWeight: 600,
+      mb: { base: 3, md: 4 },
     },
     h4: {
       ...headingBase,
-      fontSize: "xl",
+      fontSize: { base: "md", md: "lg" },
+      fontWeight: 600,
+      mb: 2,
     },
     p: {
       fontFamily: "body",
       fontWeight: "normal",
       fontSize: "md",
       lineHeight: 6,
-      marginY: 4,
+      my: 4,
+    },
+    a: {
+      textDecoration: "underline",
+      fontWeight: "semibold",
+    },
+    hr: {
+      my: { base: 12, md: 14 },
+      borderColor: mode("gray.200", "gray.700")(props),
+    },
+    blockquote: {
+      paddingStart: 4,
+      my: { base: 6, md: 8 },
+      borderStartWidth: "4px",
+      borderStartColor: mode("gray.200", "gray.600")(props),
+      fontStyle: "italic",
+      fontWeight: "semibold",
+    },
+    pre: {
+      p: 4,
+      rounded: "md",
+      bg: mode("gray.800", "gray.700")(props),
+      color: "gray.50",
+      overflow: "auto",
+
+      code: {
+        fontWeight: "normal",
+      },
+    },
+    code: {
+      fontWeight: "semibold",
+    },
+    figure: {
+      my: 8,
+
+      figcaption: {
+        color: mode("gray.500", "gray.400")(props),
+        mt: 3,
+      },
+    },
+    ul: {
+      paddingStart: 6,
+
+      li: {
+        paddingStart: 2,
+        my: 3,
+
+        "::marker": {
+          color: mode("gray.300", "gray.500")(props),
+        },
+      },
+    },
+    ol: {
+      paddingStart: 6,
+
+      li: {
+        paddingStart: 2,
+        my: 3,
+
+        "::marker": {
+          color: mode("gray.600", "gray.400")(props),
+        },
+      },
+    },
+    table: {
+      width: "full",
+      my: 8,
+      textAlign: "start",
+
+      thead: {
+        borderBottomWidth: "1px",
+        borderBottomColor: mode("gray.300", "gray.600")(props),
+      },
+
+      th: {
+        textAlign: "inherit",
+        fontWeight: 600,
+        p: { base: 2, md: 3 },
+      },
+
+      td: {
+        p: { base: 2, md: 3 },
+        verticalAlign: "baseline",
+      },
+
+      tbody: {
+        tr: {
+          borderBottomWidth: "1px",
+          borderBottomColor: mode("gray.200", "gray.700")(props),
+
+          ":last-of-type": {
+            borderBottomWidth: "0px",
+            borderBottomColor: "transparent",
+          },
+        },
+      },
+
+      tfoot: {
+        tr: {
+          borderTopWidth: "1px",
+          borderTopColor: mode("gray.300", "gray.600")(props),
+        },
+      },
     },
   }
 }
