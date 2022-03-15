72d8eaaa1364d7f0c644192f70c7a0e24953eb72
ambar <ambar.lee@gmail.com>
2022-03-15 21:25:18 +0800

    fix: memoize switch object in useBoolean

--- packages/hooks/src/use-boolean.ts
+++ packages/hooks/src/use-boolean.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { useState } from "react"
 
 type InitialState = boolean | (() => boolean)
 
@@ -9,18 +9,11 @@ type InitialState = boolean | (() => boolean)
  */
 export function useBoolean(initialState: InitialState = false) {
   const [value, setValue] = useState(initialState)
+  const [switches] = useState(() => ({
+    on: () => setValue(true),
+    off: () => setValue(false),
+    toggle: () => setValue(prev => !prev),
+  }))
 
-  const on = useCallback(() => {
-    setValue(true)
-  }, [])
-
-  const off = useCallback(() => {
-    setValue(false)
-  }, [])
-
-  const toggle = useCallback(() => {
-    setValue((prev) => !prev)
-  }, [])
-
-  return [value, { on, off, toggle }] as const
+  return [value, switches] as const
 }
