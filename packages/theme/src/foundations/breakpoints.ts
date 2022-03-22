4b95b7c83c6ab58f13f639b6ee12c60fdad77b5b
Max Friedrich <max@friedrich.dev>
2022-03-22 13:08:35 +0100

    refactor: remove deprecated createBreakpoints function

--- packages/theme/src/foundations/breakpoints.ts
+++ packages/theme/src/foundations/breakpoints.ts
@@ -1,14 +1,12 @@
-import { createBreakpoints } from "@chakra-ui/theme-tools"
-
 /**
  * Breakpoints for responsive design
  */
-const breakpoints = createBreakpoints({
+const breakpoints = {
   sm: "30em",
   md: "48em",
   lg: "62em",
   xl: "80em",
   "2xl": "96em",
-})
+}
 
 export default breakpoints
