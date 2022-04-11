d706e15cf00d73c0f635defc95e561b2f85b624f
Tim Kolberger <tim@kolberger.eu>
2022-04-11 15:56:22 +0200

    chore: fix .mjs imports in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,4 +12,16 @@ module.exports = {
   typescript: {
     reactDocgen: false,
   },
+  webpackFinal: (config) => {
+    // https://github.com/polkadot-js/extension/issues/621#issuecomment-759341776
+    // framer-motion uses the .mjs notation and we need to include it so that webpack will
+    // transpile it for us correctly (enables using a CJS module inside an ESM).
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    })
+    // Return the altered config
+    return config
+  },
 }
