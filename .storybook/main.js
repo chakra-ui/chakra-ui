f7401160eca5488546dda58a548dba5460997530
Segun Adebayo <joseshegs@gmail.com>
2022-04-11 16:15:30 +0400

    chore: update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,3 @@
-const path = require("path")
-
-const toPath = (_path) => path.join(process.cwd(), _path)
-
 module.exports = {
   stories: ["../packages/**/stories/*.stories.tsx"],
   addons: [
@@ -10,20 +6,10 @@ module.exports = {
     "@storybook/addon-storysource",
     "storybook-addon-performance/register",
   ],
+  features: {
+    emotionAlias: false,
+  },
   typescript: {
     reactDocgen: false,
-  },
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
-        },
-      },
-    }
   },
 }
