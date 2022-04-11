aa4359b5c866afb0728f247ced3ced0dbdc303da
pomSense <35952778+pomSense@users.noreply.github.com>
2022-04-10 22:35:08 -0700

    refactor: simplified PopoverTrigger typing, and using ReactNode for children type

--- packages/popover/src/popover.tsx
+++ packages/popover/src/popover.tsx
@@ -81,16 +81,14 @@ export interface PopoverTriggerProps {
    * The React child to use as the
    * trigger for the popover
    */
-  children: React.ReactChild
+  children: React.ReactNode
 }
 
 /**
  * PopoverTrigger opens the popover's content. It must be an interactive element
  * such as `button` or `a`.
  */
-export const PopoverTrigger: React.FC<
-  React.PropsWithChildren<PopoverTriggerProps>
-> = (props) => {
+export const PopoverTrigger = (props: PopoverTriggerProps) => {
   // enforce a single child
   const child: any = React.Children.only(props.children)
   const { getTriggerProps } = usePopoverContext()
