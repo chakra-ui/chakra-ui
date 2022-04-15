edd0f537d66477848a36698b4b51f34f0666b08a
Tim Kolberger <tim@kolberger.eu>
2022-04-15 18:05:23 +0200

    feat: update templates

--- tooling/cra-template-typescript/template/src/index.tsx
+++ tooling/cra-template-typescript/template/src/index.tsx
@@ -1,16 +1,18 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
-import ReactDOM from "react-dom"
+import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+
+root.render(
   <React.StrictMode>
     <ColorModeScript />
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
 )
 
 // If you want your app to work offline and load faster, you can change
