f218a02a72ba930c252f0a6a92f9fe18cdbab16c
Tim Kolberger <tim@kolberger.eu>
2022-04-15 18:05:23 +0200

    feat: update templates

--- tooling/cra-template/template/src/index.js
+++ tooling/cra-template/template/src/index.js
@@ -1,16 +1,18 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <StrictMode>
     <ColorModeScript />
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
