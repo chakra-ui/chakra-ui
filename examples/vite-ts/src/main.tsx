b75f2b0e742ecd089c3e562317bb2e7e223b191f
Segun Adebayo <joseshegs@gmail.com>
2022-04-21 12:53:17 +0400

    chore: add vite example

---
+++ examples/vite-ts/src/main.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
