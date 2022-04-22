84ff1455a7d98198523cca485ec685613fed7f5d
Segun Adebayo <joseshegs@gmail.com>
2022-04-22 14:16:54 +0400

    chore: use storybook addon in our stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,13 @@
 module.exports = {
+  core: { builder: "webpack5" },
+  features: { emotionAlias: false },
+  typescript: { reactDocgen: false },
   stories: ["../packages/**/stories/*.stories.tsx"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-toolbars",
     "@storybook/addon-storysource",
     "storybook-addon-performance/register",
+    "@chakra-ui/storybook-addon",
   ],
-  features: {
-    emotionAlias: false,
-  },
-  typescript: {
-    reactDocgen: false,
-  },
-  webpackFinal: (config) => {
-    // https://github.com/polkadot-js/extension/issues/621#issuecomment-759341776
-    // framer-motion uses the .mjs notation and we need to include it so that webpack will
-    // transpile it for us correctly (enables using a CJS module inside an ESM).
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    })
-    // Return the altered config
-    return config
-  },
 }
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,81 +1,12 @@
-import {
-  ChakraProvider,
-  extendTheme,
-  Flex,
-  IconButton,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react"
-import { Parameters, StoryContext } from "@storybook/react"
+import { Parameters } from "@storybook/react"
 import * as React from "react"
-import { FaMoon, FaSun } from "react-icons/fa"
-import { withPerformance } from "storybook-addon-performance"
 
-/**
- * Add global context for RTL-LTR switching
- */
-export const globalTypes = {
-  direction: {
-    name: "Direction",
-    description: "Direction for layout",
-    defaultValue: "LTR",
-    toolbar: {
-      icon: "globe",
-      items: ["LTR", "RTL"],
-    },
-  },
-}
-
-const ColorModeToggleBar = () => {
-  const { toggleColorMode } = useColorMode()
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
-  const nextMode = useColorModeValue("dark", "light")
-
-  return (
-    <Flex justify="flex-end" mb={4}>
-      <IconButton
-        size="md"
-        fontSize="lg"
-        aria-label={`Switch to ${nextMode} mode`}
-        variant="ghost"
-        color="current"
-        marginLeft="2"
-        onClick={toggleColorMode}
-        icon={<SwitchIcon />}
-      />
-    </Flex>
-  )
-}
-
-const withChakra = (StoryFn: Function, context: StoryContext) => {
-  const { direction } = context.globals
-  const dir = direction.toLowerCase()
-
-  React.useEffect(() => {
-    document.documentElement.dir = dir
-  }, [dir])
-
+const withChakra = (StoryFn: Function) => {
   return (
     <React.StrictMode>
-      <ChakraProvider
-        theme={extendTheme({
-          direction: dir,
-          semanticTokens: {
-            colors: {
-              semantic: {
-                default: "red.500",
-                _light: "red.500",
-                _dark: "blue.400",
-              },
-            },
-          },
-        })}
-      >
-        <div dir={dir} id="story-wrapper" style={{ minHeight: "100vh" }}>
-          <ColorModeToggleBar />
-          <StoryFn />
-        </div>
-      </ChakraProvider>
+      <div id="story-wrapper" style={{ minHeight: "100vh" }}>
+        <StoryFn />
+      </div>
     </React.StrictMode>
   )
 }
@@ -89,4 +20,4 @@ export const parameters: Parameters = {
   },
 }
 
-export const decorators = [withChakra, withPerformance]
+export const decorators = [withChakra]
