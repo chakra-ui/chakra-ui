dd4fbf3a2beb2386eff77dd2ea52e8d2b328a650
Tim Kolberger <tim@kolberger.eu>
2022-04-22 20:54:32 +0200

    chore: dog feed storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,23 +5,12 @@ module.exports = {
     "@storybook/addon-toolbars",
     "@storybook/addon-storysource",
     "storybook-addon-performance/register",
+    "@chakra-ui/storybook-addon",
   ],
   features: {
     emotionAlias: false,
   },
   typescript: {
     reactDocgen: false,
-  },
-  webpackFinal: (config) => {
-    // https://github.com/polkadot-js/extension/issues/621#issuecomment-759341776
-    // framer-motion uses the .mjs notation and we need to include it so that webpack will
-    // transpile it for us correctly (enables using a CJS module inside an ESM).
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    })
-    // Return the altered config
-    return config
   },
 }
