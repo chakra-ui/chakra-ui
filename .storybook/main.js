61175ecedc7753c1ab4ef99e79e30c9374ea287d
Tim Kolberger <tim@kolberger.eu>
2022-04-24 00:00:59 +0200

    chore: use getThemingArgTypes in button story

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-toolbars",
+    "@storybook/addon-controls",
     "@storybook/addon-storysource",
     "storybook-addon-performance/register",
     "@chakra-ui/storybook-addon",
