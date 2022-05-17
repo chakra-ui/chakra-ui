60a4730940ba5737e9cdf7b757db9902cb8b17d4
Segun Adebayo <joseshegs@gmail.com>
2022-05-17 13:39:41 +0400

    chore: update

--- packages/theme/src/semantic-tokens.ts
+++ packages/theme/src/semantic-tokens.ts
@@ -1,6 +1,6 @@
 export const semanticTokens = {
   colors: {
-    "chakra-body-text": { _light: "gray.800", _dark: "whiteAlpha.800" },
+    "chakra-body-text": { _light: "gray.800", _dark: "whiteAlpha.900" },
     "chakra-body-bg": { _light: "white", _dark: "gray.800" },
     "chakra-border-color": { _light: "gray.200", _dark: "whiteAlpha.300" },
     "chakra-placeholder-color": { _light: "gray.500", _dark: "whiteAlpha.400" },
