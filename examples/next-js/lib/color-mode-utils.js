d40a360b357f12e12788e84c3ca52db3e633d4ae
Segun Adebayo <joseshegs@gmail.com>
2022-05-17 10:46:05 +0400

    chore: add utility for cookie storage manager

---
+++ examples/next-js/lib/color-mode-utils.js
@@ -0,0 +1,16 @@
+import { setCookies, getCookie } from "cookies-next"
+import { STORAGE_KEY } from "@chakra-ui/react"
+
+export const colorModeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+}
+
+export const ensureColorMode = (ctx) => {
+  let cookie = getCookie(STORAGE_KEY, ctx)
+  if (!cookie) {
+    cookie = colorModeConfig.initialColorMode
+    setCookies(STORAGE_KEY, cookie, ctx)
+  }
+  return ctx.req.headers.cookie
+}
