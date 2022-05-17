b5c814c181ba6e22dc901264dfc8aa998f3da3b1
Segun Adebayo <joseshegs@gmail.com>
2022-05-17 12:40:40 +0400

    ci: update script

--- scripts/twitter-write.ts
+++ scripts/twitter-write.ts
@@ -1,7 +1,8 @@
-import { generateTweetArtifact, postTweet } from "./utils/twitter-utils"
+import { readFileSync } from "fs"
+import { postTweet } from "./utils/twitter-utils"
 
 async function main() {
-  const artifact = await generateTweetArtifact()
+  const artifact = JSON.parse(readFileSync(".tweetrc", "utf8"))
   const result = await postTweet(artifact)
   console.log("Tweeted to @chakra_ui account:", result.data.id)
 }
