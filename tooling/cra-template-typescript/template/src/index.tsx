a339c3622a3c9b2953704602723b1832c6ff2749
fred <fredckl.dev@gmail.com>
2022-05-22 10:22:20 +0200

    Fix ts error

--- tooling/cra-template-typescript/template/src/index.tsx
+++ tooling/cra-template-typescript/template/src/index.tsx
@@ -5,22 +5,29 @@ import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
+(() => {
+  try {
+    const container = document.getElementById("root")
+    if (!container) throw new Error('Failed to find the root element');
+    const root = ReactDOM.createRoot(container)
 
-root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <App />
-  </React.StrictMode>,
-)
+    root.render(
+      <React.StrictMode>
+        <ColorModeScript />
+        <App />
+      </React.StrictMode>,
+    )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister()
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://cra.link/PWA
+    serviceWorker.unregister()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals()
+  } catch (err) {
+    console.log(err)
+  }
+})()
