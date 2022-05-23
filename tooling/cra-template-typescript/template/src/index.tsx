5116ea39f35ffc9f53d1e8d2d765763c2ab24f95
Segun Adebayo <joseshegs@gmail.com>
2022-05-23 15:10:49 +0400

    chore: update

--- tooling/cra-template-typescript/template/src/index.tsx
+++ tooling/cra-template-typescript/template/src/index.tsx
@@ -5,29 +5,25 @@ import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
-(() => {
-  try {
-    const container = document.getElementById("root")
-    if (!container) throw new Error('Failed to find the root element');
-    const root = ReactDOM.createRoot(container)
 
-    root.render(
-      <React.StrictMode>
-        <ColorModeScript />
-        <App />
-      </React.StrictMode>,
-    )
+const container = document.getElementById("root")
+if (!container) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(container)
 
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: https://cra.link/PWA
-    serviceWorker.unregister()
+root.render(
+  <React.StrictMode>
+    <ColorModeScript />
+    <App />
+  </React.StrictMode>,
+)
 
-    // If you want to start measuring performance in your app, pass a function
-    // to log results (for example: reportWebVitals(console.log))
-    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-    reportWebVitals()
-  } catch (err) {
-    console.log(err)
-  }
-})()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
+
