bde2b2d9ed0c5c2bcf3d36f3bace155a850d5e59
Segun Adebayo <joseshegs@gmail.com>
2022-06-13 12:28:48 +0400

    chore: update packages based on manypkg

--- packages/shared-configs/tsup/tsup.config.ts
+++ packages/shared-configs/tsup/tsup.config.ts
@@ -5,14 +5,9 @@ const defaultConfig: Options = {
   target: "node14",
   dts: {
     resolve: true,
-    // build types for `src/index.ts` only
-    // otherwise `Options` will not be exported by `tsup`,
-    // not sure how this happens, probably a bug in rollup-plugin-dts
     entry: "./src/index.ts",
   },
   clean: true,
-  sourcemap: true,
-  external: ["react"],
   format: ["esm", "cjs"],
   treeshake: true,
 }
