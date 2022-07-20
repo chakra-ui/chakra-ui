10124f42fa0b024261d618dcd9fd53c5e19d6457
Segun Adebayo <joseshegs@gmail.com>
2022-07-20 10:02:19 +0400

    chore: update package scripts

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -1,41 +1,21 @@
 import findPackages from "find-packages"
 import { promises as fs } from "fs"
 
-function modifyDeps(deps: Record<string, string>) {
-  const result: Record<string, string> = {}
-  for (const key in deps) {
-    result[key] = key.startsWith("@chakra-ui/") ? "workspace:*" : deps[key]
-  }
-  return result
-}
-
 async function main() {
-  const pkgs = await findPackages(process.cwd())
+  const pkgs = await findPackages("packages")
   await Promise.all(
     pkgs.map(async (pkg) => {
-      const dependencies = modifyDeps(pkg.manifest.dependencies!)
-      const devDependencies = modifyDeps(pkg.manifest.devDependencies!)
-      let newPkg = {
+      let tsConfig = {
         ...pkg.manifest,
-        dependencies,
-        devDependencies,
-      }
-
-      if (Object.keys(newPkg.devDependencies).length === 0) {
-        const { devDependencies, ...newPkgs } = newPkg
-        //@ts-ignore
-        newPkg = newPkgs
-      }
-
-      if (Object.keys(newPkg.dependencies).length === 0) {
-        const { dependencies, ...newPkgs } = newPkg
-        //@ts-ignore
-        newPkg = newPkgs
+        scripts: {
+          ...pkg.manifest.scripts,
+          typecheck: "tsc --noEmit",
+        },
       }
 
       return fs.writeFile(
         `${pkg.dir}/package.json`,
-        JSON.stringify(newPkg, null, 2),
+        JSON.stringify(tsConfig, null, 2),
       )
     }),
   )
