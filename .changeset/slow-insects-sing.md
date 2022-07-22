fcbf5e7a0855a32add9843f572103aa849f7202a
Daniel Schulz <daniel@schulz.codes>
2022-07-22 10:51:17 +0200

    fix(layout): highlight works with empty query (#6357)

---
+++ .changeset/slow-insects-sing.md
@@ -0,0 +1,5 @@
+---
+"@chakra-ui/layout": patch
+---
+
+Adjust `Highlight` to not show any highlights if `query` is empty
--- packages/layout/src/highlight.tsx
+++ packages/layout/src/highlight.tsx
@@ -23,12 +23,21 @@ const escapeRegexp = (term: string): string =>
   term.replace(/[|\\{}()[\]^$+*?.-]/g, (char: string) => `\\${char}`)
 
 function buildRegex(query: string[]) {
-  const _query = query.map((text) => escapeRegexp(text.trim()))
+  const _query = query
+    .filter((text) => text.length !== 0)
+    .map((text) => escapeRegexp(text.trim()))
+  if (!_query.length) {
+    return null
+  }
+
   return new RegExp(`(${_query.join("|")})`, "ig")
 }
 
 function highlightWords({ text, query }: Options): Chunk[] {
   const regex = buildRegex(Array.isArray(query) ? query : [query])
+  if (!regex) {
+    return []
+  }
   const result = text.split(regex).filter(Boolean)
   return result.map((str) => ({ text: str, match: regex.test(str) }))
 }
