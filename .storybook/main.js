ab3ce72179f1d6cffe9a3a07ea7e04e1e4c4b870
Segun Adebayo <joseshegs@gmail.com>
2022-07-23 14:58:29 +0400

    fix: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,25 @@
+const path = require("path")
+
 module.exports = {
   core: {
     builder: "@storybook/builder-webpack5",
     disableTelemetry: true,
   },
-  stories: ["../packages/accordion/stories/*.stories.tsx"],
+  stories: ["../packages/**/stories/*.stories.tsx"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-essentials",
     "@storybook/addon-storysource",
     "@chakra-ui/storybook-addon",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@chakra-ui/react": path.resolve(__dirname, "../packages/react"),
+    }
+    config.resolve.extensions.push(".ts", ".tsx")
+    return config
+  },
   typescript: {
     reactDocgen: false,
   },
