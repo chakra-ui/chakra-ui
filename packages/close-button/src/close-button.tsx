8f18a74fab9d8b223ac15923822dbc834fb72a76
Segun Adebayo <joseshegs@gmail.com>
2022-08-04 17:58:34 +0400

    refactor: close button src

--- packages/close-button/src/close-button.tsx
+++ packages/close-button/src/close-button.tsx
@@ -9,16 +9,17 @@ import {
   HTMLChakraProps,
 } from "@chakra-ui/system"
 import { __DEV__ } from "@chakra-ui/utils"
-import * as React from "react"
 
-const CloseIcon: React.FC<IconProps> = (props) => (
-  <Icon focusable="false" aria-hidden {...props}>
-    <path
-      fill="currentColor"
-      d="M.439,21.44a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,1,0,2.122-2.121L14.3,12.177a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.44L12.177,9.7a.25.25,0,0,1-.354,0L2.561.44A1.5,1.5,0,0,0,.439,2.561L9.7,11.823a.25.25,0,0,1,0,.354Z"
-    />
-  </Icon>
-)
+function CloseIcon(props: IconProps) {
+  return (
+    <Icon focusable="false" aria-hidden {...props}>
+      <path
+        fill="currentColor"
+        d="M.439,21.44a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,1,0,2.122-2.121L14.3,12.177a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.44L12.177,9.7a.25.25,0,0,1-.354,0L2.561.44A1.5,1.5,0,0,0,.439,2.561L9.7,11.823a.25.25,0,0,1,0,.354Z"
+      />
+    </Icon>
+  )
+}
 
 export interface CloseButtonProps
   extends HTMLChakraProps<"button">,
@@ -36,7 +37,7 @@ export interface CloseButtonProps
  * like Alerts, Toasts, Drawers and Modals.
  */
 export const CloseButton = forwardRef<CloseButtonProps, "button">(
-  (props, ref) => {
+  function CloseButton(props, ref) {
     const styles = useStyleConfig("CloseButton", props)
     const { children, isDisabled, __css, ...rest } = omitThemingProps(props)
 
