68fe0cf4b41a4ac8fb7b8d33b2efe8654b1c11c7
Segun Adebayo <joseshegs@gmail.com>
2022-08-04 16:12:10 +0400

    refactor: breadcrumb src

---
+++ packages/breadcrumb/src/breadcrumb-separator.tsx
@@ -0,0 +1,43 @@
+import {
+  chakra,
+  forwardRef,
+  HTMLChakraProps,
+  SystemStyleObject,
+} from "@chakra-ui/system"
+import { __DEV__ } from "@chakra-ui/utils"
+import { useBreadcrumbStyles } from "./breadcrumb-context"
+
+export interface BreadcrumbSeparatorProps extends HTMLChakraProps<"div"> {
+  /**
+   * @type SystemStyleObject["mx"]
+   */
+  spacing?: SystemStyleObject["mx"]
+}
+/**
+ * React component that separates each breadcrumb link
+ */
+
+export const BreadcrumbSeparator = forwardRef<BreadcrumbSeparatorProps, "span">(
+  function BreadcrumbSeparator(props, ref) {
+    const { spacing, ...rest } = props
+
+    const styles = useBreadcrumbStyles()
+    const separatorStyles: SystemStyleObject = {
+      mx: spacing,
+      ...styles.separator,
+    }
+
+    return (
+      <chakra.span
+        ref={ref}
+        role="presentation"
+        {...rest}
+        __css={separatorStyles}
+      />
+    )
+  },
+)
+
+if (__DEV__) {
+  BreadcrumbSeparator.displayName = "BreadcrumbSeparator"
+}
--- packages/breadcrumb/src/index.ts
+++ packages/breadcrumb/src/index.ts
@@ -1,1 +1,7 @@
-export * from "./breadcrumb"
+export { Breadcrumb, type BreadcrumbProps } from "./breadcrumb"
+export { BreadcrumbItem, type BreadcrumbItemProps } from "./breadcrumb-item"
+export {
+  BreadcrumbSeparator,
+  type BreadcrumbSeparatorProps,
+} from "./breadcrumb-separator"
+export { BreadcrumbLink, type BreadcrumbLinkProps } from "./breadcrumb-link"
