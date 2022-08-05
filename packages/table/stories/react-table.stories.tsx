b3fb69d2ed971647fb648c8f34ab0ae563f75818
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2022-08-05 15:13:37 +0900

    chore: update npm packages in examples, tooling and stories (#6339)
    * chore: update gatsby packages
    
    * chore: update vite packages
    
    * chore: remove unused package
    
    * chore: react-table is now @tanstack/react-table
    
    * chore: update web-vitals
    
    * chore: update @tanstack/react-table
    
    * chore: update to vite 3.0.2
    
    * chore: update to vite 3.0.3
    
    * chore: update to vite 3.0.4
    
    * chore: update to @tanstack/react-table 8.5.10

--- packages/table/stories/react-table.stories.tsx
+++ packages/table/stories/react-table.stories.tsx
@@ -1,95 +1,102 @@
 /* eslint-disable react/jsx-key */
 import * as React from "react"
-import { Column, useTable } from "react-table"
-import { useMemo } from "react"
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  type ColumnDef,
+  type RowData,
+} from "@tanstack/react-table"
 import { Button, ButtonGroup, IconButton } from "@chakra-ui/button"
 import { StarIcon } from "@chakra-ui/icons"
-import { Table, TableCellProps, Tbody, Td, Th, Thead, Tr } from "../src"
+import { Table, Tbody, Td, Th, Thead, Tr } from "../src"
+
+declare module "@tanstack/react-table" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ColumnMeta<TData extends RowData, TValue> {
+    isNumeric: boolean
+  }
+}
 
 export default {
   title: "Components / Data Display / Table / React Table",
 }
 
+type Data = {
+  col1: string
+  col2: number
+}
+
+const data: Data[] = [
+  { col1: "Hello", col2: 1.23 },
+  { col1: "react-table", col2: 3.14 },
+  { col1: "whatever", col2: 100 },
+]
+
+const columns: ColumnDef<Data>[] = [
+  {
+    accessorKey: "col1",
+    header: "Column 1",
+  },
+  {
+    accessorKey: "col2",
+    header: "Column 2",
+    meta: { isNumeric: true },
+  },
+  {
+    header: "Action",
+    cell: ({ row }) => (
+      <ButtonGroup size="sm" isAttached>
+        <Button minW="15ch" justifyContent="flex-start">
+          Open {row.original["col1"]}
+        </Button>
+        <IconButton
+          icon={<StarIcon color="yellow.500" />}
+          aria-label="Add to favorites"
+          variant="outline"
+        />
+      </ButtonGroup>
+    ),
+  },
+]
+
 export const ReactTable = () => {
-  const data = useMemo(
-    () => [
-      {
-        col1: "Hello",
-        col2: 1.23,
-      },
-      {
-        col1: "react-table",
-        col2: 3.14,
-      },
-      {
-        col1: "whatever",
-        col2: 100,
-      },
-    ],
-    [],
-  )
-
-  type ChakraColumn = Column & TableCellProps
-  const columns: ChakraColumn[] = useMemo(
-    () => [
-      {
-        Header: "Column 1",
-        accessor: "col1",
-      },
-      {
-        Header: "Column 2",
-        accessor: "col2",
-        isNumeric: true,
-      },
-      {
-        Header: "Action",
-        Cell: ({ row }: any) => (
-          <ButtonGroup size="sm" isAttached>
-            <Button minW="15ch" justifyContent="flex-start">
-              Open {row.original["col1"]}
-            </Button>
-            <IconButton
-              icon={<StarIcon color="yellow.500" />}
-              aria-label="Add to favorites"
-              variant="outline"
-            />
-          </ButtonGroup>
-        ),
-      },
-    ],
-    [],
-  )
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data })
+  const { getHeaderGroups, getRowModel } = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
 
   return (
-    <Table {...getTableProps()}>
+    <Table>
       <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <Th {...column.getHeaderProps()}>{column.render("Header")}</Th>
+        {getHeaderGroups().map((headerGroup) => (
+          <Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <Th key={header.id} colSpan={header.colSpan}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+              </Th>
             ))}
           </Tr>
         ))}
       </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <Td
-                  {...cell.getCellProps()}
-                  isNumeric={(cell.column as ChakraColumn).isNumeric}
-                >
-                  {cell.render("Cell")}
-                </Td>
-              ))}
-            </Tr>
-          )
-        })}
+
+      <Tbody>
+        {getRowModel().rows.map((row) => (
+          <Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <Td
+                key={cell.id}
+                isNumeric={cell.column.columnDef.meta?.isNumeric}
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </Td>
+            ))}
+          </Tr>
+        ))}
       </Tbody>
     </Table>
   )
