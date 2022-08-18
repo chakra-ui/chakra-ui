1ac9c3f49dcd08afda47bc8354139d097a3938a5
anubra266 <anubra266@gmail.com>
2022-08-18 13:47:38 +0100

    chore: refactor

--- utilities/shared-utils/src/index.ts
+++ utilities/shared-utils/src/index.ts
@@ -1,16 +1,10 @@
 export const cx = (...classNames: any[]) => classNames.filter(Boolean).join(" ")
 
-// Object assertions
 export function isObject(value: any): value is Record<string, any> {
   const type = typeof value
   return (
     value != null &&
     (type === "object" || type === "function") &&
-    !isArray(value)
+    !Array.isArray(value)
   )
-}
-
-// Array assertions
-export function isArray<T>(value: any): value is Array<T> {
-  return Array.isArray(value)
 }
