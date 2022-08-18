4d5c0c466366ae0c8b96c5b4e2ed26a09112e471
anubra266 <anubra266@gmail.com>
2022-08-18 14:15:36 +0100

    chore(shared-utils): refacttor

--- utilities/shared-utils/src/index.ts
+++ utilities/shared-utils/src/index.ts
@@ -12,37 +12,17 @@ export function isObject(value: any): value is Record<string, any> {
   )
 }
 
-export function once<T extends AnyFunction>(fn?: T | null) {
-  let result: any
-
-  return function func(this: any, ...args: Parameters<T>) {
-    if (fn) {
-      result = fn.apply(this, args)
-      fn = null
-    }
-
-    return result
-  }
-}
-
 type MessageOptions = {
   condition: boolean
   message: string
 }
 
-export const warn = /* @__PURE__ */ once((options: MessageOptions) => () => {
+export const warn = /* @__PURE__ */ (options: MessageOptions) => () => {
   const { condition, message } = options
   if (condition && __DEV__) {
     console.warn(message)
   }
-})
-
-export const error = /* @__PURE__ */ once((options: MessageOptions) => () => {
-  const { condition, message } = options
-  if (condition && __DEV__) {
-    console.error(message)
-  }
-})
+}
 
 export function runIfFn<T, U>(
   valueOrFn: T | ((...fnArgs: U[]) => T),
@@ -62,8 +42,6 @@ export function callAllHandlers<T extends (event: any) => void>(
   }
 }
 
-export function isFunction<T extends Function = Function>(
+export const isFunction = <T extends Function = Function>(
   value: any,
-): value is T {
-  return typeof value === "function"
-}
+): value is T => typeof value === "function"
