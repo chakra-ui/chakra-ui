30edd2e8efdcb187129a83953daeefb5daf038b4
anubra266 <anubra266@gmail.com>
2022-08-19 15:00:15 +0100

    chore: add changeset

--- utilities/dom-utils/src/dom.ts
+++ utilities/dom-utils/src/dom.ts
@@ -33,14 +33,6 @@ export function getEventWindow(event: Event): typeof globalThis {
   return ((event as UIEvent).view ?? window) as unknown as typeof globalThis
 }
 
-export function canUseDOM(): boolean {
-  return !!(
-    typeof window !== "undefined" &&
-    window.document &&
-    window.document.createElement
-  )
-}
-
 export function isBrowser() {
   return Boolean(globalThis?.document)
 }
