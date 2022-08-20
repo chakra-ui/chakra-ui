2f14632c143cbda912291d112899e9cd32ebf195
Segun Adebayo <joseshegs@gmail.com>
2022-08-20 22:27:27 +0400

    refactor: visually hidden

--- packages/visually-hidden/src/visually-hidden.tsx
+++ packages/visually-hidden/src/visually-hidden.tsx
@@ -1,20 +1,5 @@
 import { chakra } from "@chakra-ui/system"
-
-/**
- * Styles to visually hide an element
- * but make it accessible to screen-readers
- */
-export const visuallyHiddenStyle: React.CSSProperties = {
-  border: "0px",
-  clip: "rect(0px, 0px, 0px, 0px)",
-  height: "1px",
-  width: "1px",
-  margin: "-1px",
-  padding: "0px",
-  overflow: "hidden",
-  whiteSpace: "nowrap",
-  position: "absolute",
-}
+import { visuallyHiddenStyle } from "./visually-hidden.style"
 
 /**
  * Visually hidden component used to hide
