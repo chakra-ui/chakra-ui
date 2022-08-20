fa69685872cddd82a6e3399f3fd6843ed3b13066
Segun Adebayo <joseshegs@gmail.com>
2022-08-20 19:21:04 +0400

    fix: types

--- packages/transition/src/index.ts
+++ packages/transition/src/index.ts
@@ -1,16 +1,16 @@
 export { Collapse } from "./collapse"
 export type { CollapseProps, ICollapse, CollapseOptions } from "./collapse"
 
-export { Fade } from "./fade"
+export { Fade, fadeConfig } from "./fade"
 export type { FadeProps } from "./fade"
 
-export { ScaleFade } from "./scale-fade"
+export { ScaleFade, scaleFadeConfig } from "./scale-fade"
 export type { ScaleFadeProps } from "./scale-fade"
 
 export { Slide } from "./slide"
-export type { SlideDirection, SlideProps } from "./slide"
+export type { SlideDirection, SlideProps, SlideOptions } from "./slide"
 
-export { SlideFade } from "./slide-fade"
+export { SlideFade, slideFadeConfig } from "./slide-fade"
 export type { SlideFadeProps } from "./slide-fade"
 
 export { TransitionEasings as EASINGS } from "./transition-utils"
