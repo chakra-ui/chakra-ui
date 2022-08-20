db97eb14472afb1a829ab7cd74eff6a149146496
Segun Adebayo <joseshegs@gmail.com>
2022-08-20 17:44:58 +0400

    refactor: popper

--- packages/popper/src/index.ts
+++ packages/popper/src/index.ts
@@ -1,3 +1,4 @@
 export type { Placement, PlacementWithLogical } from "./popper.placement"
-export * from "./use-popper"
+export { usePopper } from "./use-popper"
+export type { UsePopperProps, UsePopperReturn } from "./use-popper"
 export { cssVars as popperCSSVars } from "./utils"
