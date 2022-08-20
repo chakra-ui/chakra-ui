f57210c824678087f51c613bf4bfdbdb2ede92f3
Segun Adebayo <joseshegs@gmail.com>
2022-08-20 17:03:29 +0400

    refactor: transition

--- packages/transition/src/index.ts
+++ packages/transition/src/index.ts
@@ -1,6 +1,16 @@
-export * from "./collapse"
-export * from "./fade"
-export * from "./scale-fade"
-export * from "./slide"
-export * from "./slide-fade"
+export { Collapse } from "./collapse"
+export type { CollapseProps, ICollapse, CollapseOptions } from "./collapse"
+
+export { Fade } from "./fade"
+export type { FadeProps } from "./fade"
+
+export { ScaleFade } from "./scale-fade"
+export type { ScaleFadeProps } from "./scale-fade"
+
+export { Slide } from "./slide"
+export type { SlideDirection, SlideProps } from "./slide"
+
+export { SlideFade } from "./slide-fade"
+export type { SlideFadeProps } from "./slide-fade"
+
 export { TransitionEasings as EASINGS } from "./transition-utils"
