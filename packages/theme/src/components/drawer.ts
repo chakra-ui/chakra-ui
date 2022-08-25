33209871d2a497e0ed869b68deeaf6ebd6e2c331
Segun Adebayo <joseshegs@gmail.com>
2022-08-25 11:39:40 +0400

    refactor: drawer

--- packages/theme/src/components/drawer.ts
+++ packages/theme/src/components/drawer.ts
@@ -49,28 +49,28 @@ const baseStyleDialog = defineStyle((props) => {
 })
 
 const baseStyleHeader = defineStyle({
-  px: 6,
-  py: 4,
+  px: "6",
+  py: "4",
   fontSize: "xl",
   fontWeight: "semibold",
 })
 
 const baseStyleCloseButton = defineStyle({
   position: "absolute",
-  top: 2,
-  insetEnd: 3,
+  top: "2",
+  insetEnd: "3",
 })
 
 const baseStyleBody = defineStyle({
-  px: 6,
-  py: 2,
-  flex: 1,
+  px: "6",
+  py: "2",
+  flex: "1",
   overflow: "auto",
 })
 
 const baseStyleFooter = defineStyle({
-  px: 6,
-  py: 4,
+  px: "6",
+  py: "4",
 })
 
 const baseStyle = definePartsStyle((props) => ({
