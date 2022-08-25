4eb795b8d827367205279a3598e2fc33d8a99cf7
Segun Adebayo <joseshegs@gmail.com>
2022-08-25 11:41:52 +0400

    refactor: select

--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -28,7 +28,7 @@ const baseStyleIcon = defineStyle({
   insetEnd: "0.5rem",
   position: "relative",
   color: "currentColor",
-  fontSize: "1.25rem",
+  fontSize: "xl",
   _disabled: {
     opacity: 0.5,
   },
@@ -40,7 +40,7 @@ const baseStyle = definePartsStyle((props) => ({
 }))
 
 const iconSpacing = defineStyle({
-  paddingInlineEnd: "2rem",
+  paddingInlineEnd: "8",
 })
 
 const sizes = {
@@ -72,7 +72,7 @@ const sizes = {
       ...iconSpacing,
     },
     icon: {
-      insetEnd: "0.25rem",
+      insetEnd: "1",
     },
   },
 }
