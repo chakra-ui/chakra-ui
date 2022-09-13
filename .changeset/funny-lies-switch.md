7086d509eb05386fa4708a2d4cd2937375a147d0
anubra266 <anubra266@gmail.com>
2022-09-13 16:08:02 +0100

    fix(stack): prevent crashing when there's a lot of children

---
+++ .changeset/funny-lies-switch.md
@@ -0,0 +1,5 @@
+---
+"@chakra-ui/layout": patch
+---
+
+Prevent Stack from crashing when there's a lot of chldren
--- packages/components/layout/src/stack/stack.tsx
+++ packages/components/layout/src/stack/stack.tsx
@@ -98,32 +98,46 @@ export const Stack = forwardRef<StackProps, "div">((props, ref) => {
   const hasDivider = !!divider
   const shouldUseChildren = !shouldWrapChildren && !hasDivider
 
-  const validChildren = getValidChildren(children)
+  const validChildren = useMemo(() => getValidChildren(children), [children])
 
-  const clones = shouldUseChildren
-    ? validChildren
-    : validChildren.map((child, index) => {
-        // Prefer provided child key, fallback to index
-        const key = typeof child.key !== "undefined" ? child.key : index
-        const isLast = index + 1 === validChildren.length
-        const wrappedChild = <StackItem key={key}>{child}</StackItem>
-        const _child = shouldWrapChildren ? wrappedChild : child
+  const clones = useMemo(
+    () =>
+      shouldUseChildren
+        ? validChildren
+        : validChildren.map((child, index) => {
+            // Prefer provided child key, fallback to index
+            const key = typeof child.key !== "undefined" ? child.key : index
+            const isLast = index + 1 === validChildren.length
+            const wrappedChild = <StackItem key={key}>{child}</StackItem>
+            const _child = shouldWrapChildren ? wrappedChild : child
 
-        if (!hasDivider) return _child
+            if (!hasDivider) return _child
 
-        const clonedDivider = cloneElement(divider as React.ReactElement<any>, {
-          __css: dividerStyle,
-        })
+            const clonedDivider = cloneElement(
+              divider as React.ReactElement<any>,
+              {
+                __css: dividerStyle,
+              },
+            )
 
-        const _divider = isLast ? null : clonedDivider
+            const _divider = isLast ? null : clonedDivider
 
-        return (
-          <Fragment key={key}>
-            {_child}
-            {_divider}
-          </Fragment>
-        )
-      })
+            return (
+              <Fragment key={key}>
+                {_child}
+                {_divider}
+              </Fragment>
+            )
+          }),
+    [
+      divider,
+      dividerStyle,
+      hasDivider,
+      shouldUseChildren,
+      shouldWrapChildren,
+      validChildren,
+    ],
+  )
 
   const _className = cx("chakra-stack", className)
 
