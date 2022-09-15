42b3dd2a9ebf8a84f14530273f7309556fd32376
Christian Schröter <dev@cschroeter.net>
2022-09-15 13:51:18 +0200

    build: save cmd

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -5,16 +5,20 @@ async function main() {
   const pkgs = await findPackages("packages")
   await Promise.all(
     pkgs.map(async (pkg) => {
-      // check if src contains tsx files
       const files = await fs.readdir(`${pkg.dir}/src`)
       const tsx = files.some((f) => f.endsWith(".tsx"))
 
       let data = {
         ...pkg.manifest,
+        main: "dist/index.cjs.js",
+        module: "dist/index.esm.js",
         scripts: {
           ...pkg.manifest.scripts,
-          prepack: "clean-package",
-          postpack: "clean-package restore",
+          build: !tsx
+            ? "tsup src/index.ts --dts"
+            : "JSX=1 tsup src/index.ts --dts",
+          "build:fast": !tsx ? "tsup src/index.ts" : "JSX=1 tsup src/index.ts",
+          dev: "pnpm build -- --watch",
         },
       }
 
