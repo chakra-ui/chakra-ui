5accf48cdfce7f707d8ee67fdf62b93e380dd266
Christian Schröter <dev@cschroeter.net>
2022-09-19 11:08:53 +0200

    chore: wip

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -5,34 +5,28 @@ async function main() {
   const pkgs = await findPackages("packages")
   await Promise.all(
     pkgs.map(async (pkg) => {
+      // check if src contains tsx files
+      const files = await fs.readdir(`${pkg.dir}/src`)
+      const tsx = files.some((f) => f.endsWith(".tsx"))
+
       let data = {
         ...pkg.manifest,
-        devDependencies: {
-          ...pkg.manifest.devDependencies,
-          "clean-package": "2.1.1",
+        main: "dist/index.cjs.js",
+        module: "dist/index.esm.js",
+        scripts: {
+          ...pkg.manifest.scripts,
+          build: !tsx
+            ? "tsup src/index.ts --dts"
+            : "JSX=1 tsup src/index.ts --dts",
+          "build:fast": !tsx ? "tsup src/index.ts" : "JSX=1 tsup src/index.ts",
+          dev: "pnpm build -- --watch",
         },
       }
 
-      const config = {
-        replace: {
-          main: "dist/index.cjs.js",
-          module: "dist/index.esm.js",
-          types: "dist/index.d.ts",
-          exports: {
-            ".": {
-              import: "./dist/index.esm.js",
-              require: "./dist/index.cjs.js",
-            },
-            "./package.json": "./package.json",
-          },
-        },
-      }
-
-      fs.writeFile(
-        `${pkg.dir}/clean-package.config.json`,
-        JSON.stringify(config, null, 2),
+      return fs.writeFile(
+        `${pkg.dir}/package.json`,
+        JSON.stringify(data, null, 2),
       )
-      fs.writeFile(`${pkg.dir}/package.json`, JSON.stringify(data, null, 2))
     }),
   )
 }
