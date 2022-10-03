d9eb7e43dcd7a53465fcf1a96f5905493278f2a7
anubra266 <anubra266@gmail.com>
2022-10-03 17:35:32 +0100

    fix(toast): expose options to toast render

--- packages/components/toast/stories/toast.stories.tsx
+++ packages/components/toast/stories/toast.stories.tsx
@@ -62,14 +62,11 @@ export function CustomRender() {
           toast({
             duration: null,
             position: "top-right",
-            render: ({ options }) => {
-              console.log("options :>> ", options)
-              return (
-                <chakra.div rounded="md" color="white" p={3} bg="blue.500">
-                  Hello World
-                </chakra.div>
-              )
-            },
+            render: () => (
+              <chakra.div rounded="md" color="white" p={3} bg="blue.500">
+                Hello World
+              </chakra.div>
+            ),
           })
         }
       >
