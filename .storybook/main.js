f33267102d26cd78153f94c9067eb9dd28288638
Segun Adebayo <joseshegs@gmail.com>
2022-10-17 12:27:10 +0400

    chore: remove storybook alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,12 @@
-const path = require("path")
 const fs = require("fs")
 
 // [Workaround] This logic means `"../packages/components/*/stories/*.stories.tsx"` but it's much faster.
 function getStories(pkg) {
   const scope = pkg ? [pkg] : fs.readdirSync("packages/components")
   return scope
-    .map((package) => `packages/components/${package}/stories`)
-    .filter((storyDir) => fs.existsSync(storyDir))
-    .map((storyDir) => `../${storyDir}/*.stories.tsx`)
+    .map((pkg) => `packages/components/${pkg}/stories`)
+    .filter((dir) => fs.existsSync(dir))
+    .map((dir) => `../${dir}/*.stories.tsx`)
 }
 
 module.exports = {
@@ -22,21 +21,6 @@ module.exports = {
     "@storybook/addon-storysource",
     "@chakra-ui/storybook-addon",
   ],
-  webpackFinal: async (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@chakra-ui/react": path.resolve(
-        __dirname,
-        "../packages/components/react/src",
-      ),
-      "@chakra-ui/theme": path.resolve(
-        __dirname,
-        "../packages/components/theme/src",
-      ),
-    }
-    config.resolve.extensions.push(".ts", ".tsx")
-    return config
-  },
   typescript: {
     reactDocgen: false,
   },
