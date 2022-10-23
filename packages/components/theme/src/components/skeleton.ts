6cf4be80847d231d42e0be5f1d22563620f3535d
anubra266 <anubra266@gmail.com>
2022-10-23 23:28:53 +0100

    chore: refactor theme - Skeleton

--- packages/components/theme/src/components/skeleton.ts
+++ packages/components/theme/src/components/skeleton.ts
@@ -3,31 +3,34 @@ import {
   defineStyle,
   defineStyleConfig,
 } from "@chakra-ui/styled-system"
-import { getColor, mode } from "@chakra-ui/theme-tools"
+import { getColor } from "@chakra-ui/theme-tools"
 
 const $startColor = cssVar("skeleton-start-color")
 const $endColor = cssVar("skeleton-end-color")
+const $bg = cssVar("skeleton-background")
+const $bc = cssVar("skeleton-border-color")
 
 const baseStyle = defineStyle((props) => {
-  const defaultStartColor = mode("gray.100", "gray.800")(props)
-  const defaultEndColor = mode("gray.400", "gray.600")(props)
-
-  const {
-    startColor = defaultStartColor,
-    endColor = defaultEndColor,
-    theme,
-  } = props
+  const { startColor, endColor, theme } = props
 
   const start = getColor(theme, startColor)
   const end = getColor(theme, endColor)
 
   return {
-    [$startColor.variable]: start,
-    [$endColor.variable]: end,
+    [$startColor.variable]: start || "colors.gray.100",
+    [$endColor.variable]: end || "colors.gray.400",
+    [$bc.variable]: "colors.gray.100",
+    [$bg.variable]: "colors.gray.400",
+    _dark: {
+      [$startColor.variable]: start || "colors.gray.800",
+      [$endColor.variable]: end || "colors.gray.600",
+      [$bc.variable]: "colors.gray.800",
+      [$bg.variable]: "colors.gray.600",
+    },
+    background: end || $bg.reference,
+    borderColor: start || $bg.reference,
     opacity: 0.7,
     borderRadius: "2px",
-    borderColor: start,
-    background: end,
   }
 })
 
