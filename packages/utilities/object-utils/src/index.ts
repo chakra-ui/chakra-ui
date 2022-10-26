3a3436244efae0736abe216e283da119ae6ae02c
Reetu Mönkkönen <TrashF@taistelumarsu.org>
2022-10-26 10:36:38 +0300

    Omit keys by default if keys are not set
    This fixes a case where split is called without keys, resulting in "keys is undefined" error message.

--- packages/utilities/object-utils/src/index.ts
+++ packages/utilities/object-utils/src/index.ts
@@ -38,7 +38,7 @@ export function split<T extends Record<string, any>, K extends keyof T>(
   const omitted: Record<string, any> = {}
 
   for (const [key, value] of Object.entries(object)) {
-    if (keys.includes(key as T[K])) picked[key] = value
+    if (keys && keys.includes(key as T[K])) picked[key] = value
     else omitted[key] = value
   }
 
