3ae5df659a2c6061dbfb57bfce92b5bf3e0b5cd7
Alex Anthony <alex.anthony28991@gmail.com>
2022-11-01 18:07:18 +0000

    feat(progress): allow overriding role for Progress (#6915)

--- packages/components/progress/src/progress.tsx
+++ packages/components/progress/src/progress.tsx
@@ -39,8 +39,14 @@ export interface ProgressFilledTrackProps
  */
 const ProgressFilledTrack = forwardRef<ProgressFilledTrackProps, "div">(
   (props, ref) => {
-    const { min, max, value, isIndeterminate, ...rest } = props
-    const progress = getProgressProps({ value, min, max, isIndeterminate })
+    const { min, max, value, isIndeterminate, role, ...rest } = props
+    const progress = getProgressProps({
+      value,
+      min,
+      max,
+      isIndeterminate,
+      role,
+    })
 
     const styles = useProgressStyles()
     const trackStyles = {
@@ -122,6 +128,7 @@ export const Progress = forwardRef<ProgressProps, "div">((props, ref) => {
     "aria-label": ariaLabel,
     "aria-labelledby": ariaLabelledBy,
     title,
+    role,
     ...rest
   } = omitThemingProps(props)
 
@@ -177,6 +184,7 @@ export const Progress = forwardRef<ProgressProps, "div">((props, ref) => {
           css={css}
           borderRadius={borderRadius}
           title={title}
+          role={role}
         />
         {children}
       </ProgressStylesProvider>
