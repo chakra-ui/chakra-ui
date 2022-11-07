ae4baeeeedb22a3bf5dd49087f401ac1036b4bb6
Esther Agbaje <folasadeagbaje@gmail.com>
2022-11-07 18:17:39 +0400

    feat: updated thme using css variables

--- packages/components/theme/src/components/card.ts
+++ packages/components/theme/src/components/card.ts
@@ -1,12 +1,17 @@
 import { cardAnatomy as parts } from "@chakra-ui/anatomy"
-import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system"
+import { createMultiStyleConfigHelpers, cssVar } from "@chakra-ui/styled-system"
 
 const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(parts.keys)
 
+const $bg = cssVar("card-bg")
+
 const baseStyle = definePartsStyle({
   container: {
     borderRadius: "md",
+    [$bg.variable]: "chakra-body-bg",
+    backgroundColor: $bg.reference,
+    color: "chakra-body-text",
   },
   body: {
     paddingX: "6",
@@ -27,14 +32,22 @@ const variants = {
   elevated: definePartsStyle({
     container: {
       boxShadow: "base",
+      _dark: {
+        [$bg.variable]: "colors.gray.700",
+      },
     },
   }),
   outline: definePartsStyle({
     container: {
       borderWidth: "1px",
+      borderColor: "chakra-border-color",
     },
   }),
-  filled: definePartsStyle({}),
+  filled: definePartsStyle({
+    container: {
+      [$bg.variable]: "colors.chakra-subtle",
+    },
+  }),
 }
 
 export const cardTheme = defineMultiStyleConfig({
