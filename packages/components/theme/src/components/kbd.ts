bbc1faec83426c40cf79189556447897762ebde1
Abraham <anubra266@gmail.com>
2022-11-08 08:21:12 +0100

    chore: refactor theme - Select (#6892)
    Co-authored-by: Segun Adebayo <joseshegs@gmail.com>

--- packages/components/theme/src/components/kbd.ts
+++ packages/components/theme/src/components/kbd.ts
@@ -1,18 +1,25 @@
-import { defineStyle, defineStyleConfig } from "@chakra-ui/styled-system"
-import { mode } from "@chakra-ui/theme-tools"
+import {
+  cssVar,
+  defineStyle,
+  defineStyleConfig,
+} from "@chakra-ui/styled-system"
 
-const baseStyle = defineStyle((props) => {
-  return {
-    bg: mode("gray.100", "whiteAlpha")(props),
-    borderRadius: "md",
-    borderWidth: "1px",
-    borderBottomWidth: "3px",
-    fontSize: "0.8em",
-    fontWeight: "bold",
-    lineHeight: "normal",
-    px: "0.4em",
-    whiteSpace: "nowrap",
-  }
+const $bg = cssVar("kbd-bg")
+
+const baseStyle = defineStyle({
+  [$bg.variable]: "colors.gray.100",
+  _dark: {
+    [$bg.variable]: "colors.whiteAlpha.100",
+  },
+  bg: $bg.reference,
+  borderRadius: "md",
+  borderWidth: "1px",
+  borderBottomWidth: "3px",
+  fontSize: "0.8em",
+  fontWeight: "bold",
+  lineHeight: "normal",
+  px: "0.4em",
+  whiteSpace: "nowrap",
 })
 
 export const kbdTheme = defineStyleConfig({
