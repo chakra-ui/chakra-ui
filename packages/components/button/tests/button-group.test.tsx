5b419a256c5b9907fc470dfa7c1635e1266ffae3
Segun Adebayo <joseshegs@gmail.com>
2022-11-22 16:57:18 +0400

    fix: test

--- packages/components/button/tests/button-group.test.tsx
+++ packages/components/button/tests/button-group.test.tsx
@@ -72,7 +72,7 @@ test("Should flush outline button", () => {
 
 test("Should flush vertical button", () => {
   const { getByText } = render(
-    <ButtonGroup isAttached orientation="vertical">
+    <ButtonGroup isAttached orientation="vertical" variant="outline">
       <Button>Button 1</Button>
       <Button>Button 2</Button>
       <Button>Button 3</Button>
@@ -80,8 +80,8 @@ test("Should flush vertical button", () => {
     </ButtonGroup>,
   )
   expect(getByText(/Button 1/i)).toHaveStyle({
-    borderBottomLeftRadius: "0",
-    borderBottomRightRadius: "0",
+    borderBottomLeftRadius: "0px",
+    borderBottomRightRadius: "0px",
   })
   expect(getByText(/Button 2/i)).toHaveStyle({
     borderRadius: "0px",
@@ -90,8 +90,8 @@ test("Should flush vertical button", () => {
     borderRadius: "0px",
   })
   expect(getByText(/Button 4/i)).toHaveStyle({
-    borderTopLeftRadius: "0",
-    borderTopRightRadius: "0",
+    borderTopLeftRadius: "0px",
+    borderTopRightRadius: "0px",
   })
 })
 
