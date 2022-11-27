310742c8fe18f78cf70ce23af975ceb3bdd9b1a2
manato kuroda <a7108100@gmail.com>
2022-11-27 15:55:51 +0900

    fix(badge): ellipsis show up when using noOfLines and maxW

--- packages/components/layout/src/badge.tsx
+++ packages/components/layout/src/badge.tsx
@@ -5,8 +5,10 @@ import {
   ThemingProps,
   useStyleConfig,
   HTMLChakraProps,
+  ChakraProps,
 } from "@chakra-ui/system"
 import { cx } from "@chakra-ui/shared-utils"
+import { useMemo } from "react"
 
 export interface BadgeProps
   extends HTMLChakraProps<"span">,
@@ -21,18 +23,34 @@ export interface BadgeProps
 export const Badge = forwardRef<BadgeProps, "span">(function Badge(props, ref) {
   const styles = useStyleConfig("Badge", props)
   const { className, ...rest } = omitThemingProps(props)
+  const badgeStyle = useMemo((): ChakraProps => {
+    if (props.noOfLines) {
+      return {
+        __css: {
+          verticalAlign: "middle",
+          ...styles,
+        },
+        sx: {
+          display: props.display || "-webkit-inline-box",
+        },
+      }
+    }
+    return {
+      __css: {
+        display: "inline-block",
+        whiteSpace: "nowrap",
+        verticalAlign: "middle",
+        ...styles,
+      },
+    }
+  }, [props.display, props.noOfLines, styles])
 
   return (
     <chakra.span
       ref={ref}
       className={cx("chakra-badge", props.className)}
       {...rest}
-      __css={{
-        display: "inline-block",
-        whiteSpace: "nowrap",
-        verticalAlign: "middle",
-        ...styles,
-      }}
+      {...badgeStyle}
     />
   )
 })
