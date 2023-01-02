8ec15493bc4218d8873ade65c87fdb268a5499c2
Segun Adebayo <joseshegs@gmail.com>
2023-01-02 16:31:33 +0400

    chore: cleanup workflow scripts

---
+++ scripts/slack.ts
@@ -0,0 +1,38 @@
+import { config } from "dotenv"
+import { readFile } from "fs/promises"
+
+config()
+
+async function getLatestVersion() {
+  let [content] = JSON.parse(await readFile(".changelog/manifest.json", "utf8"))
+  return content.version
+}
+
+export async function main() {
+  const url = process.env.SLACK_WEBHOOK_URL
+
+  if (!url) {
+    throw new Error("Missing Slack Webhook URL")
+  }
+
+  const version = await getLatestVersion()
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      project: "chakra-ui",
+      version,
+    }),
+  })
+
+  if (response.ok) {
+    console.log("✅ Successfully sent message to Slack")
+  } else {
+    console.error("❌ Failed to send message to Slack")
+  }
+}
+
+main()
