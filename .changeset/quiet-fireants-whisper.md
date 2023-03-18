46bd146415ba8232ac1106e1714608704ca73712
Segun Adebayo <joseshegs@gmail.com>
2023-03-18 17:51:33 +0000

    feat: add next-js image integration

---
+++ .changeset/quiet-fireants-whisper.md
@@ -0,0 +1,6 @@
+---
+"@chakra-ui/next-js": minor
+---
+
+Add `Image` component that supercharges the Next.js optimized image with Chakra
+style props.
---
+++ packages/integrations/next-js/src/image.tsx
@@ -0,0 +1,9 @@
+import { chakra, ChakraComponent, HTMLChakraProps } from "@chakra-ui/react"
+import NextImage, { ImageProps as NextImageProps } from "next/image"
+
+export type ImageProps = NextImageProps &
+  Omit<HTMLChakraProps<"img">, keyof NextImageProps>
+
+export const Image: ChakraComponent<"img", NextImageProps> = chakra(NextImage, {
+  shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
+})
