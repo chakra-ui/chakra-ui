be28f5d0ba3a14d9a6b6bd9e059b922d25cdb260
Segun Adebayo <joseshegs@gmail.com>
2023-03-23 10:24:41 +0000

    fix(next-js): add theming props to link

--- packages/integrations/next-js/src/link.tsx
+++ packages/integrations/next-js/src/link.tsx
@@ -1,6 +1,15 @@
-import { forwardRef, chakra, HTMLChakraProps } from "@chakra-ui/react"
+import {
+  forwardRef,
+  chakra,
+  HTMLChakraProps,
+  ThemingProps,
+  useStyleConfig,
+  omitThemingProps,
+} from "@chakra-ui/react"
 import NextLink, { LinkProps as NextLinkProps } from "next/link"
 import type { FC, RefAttributes } from "react"
+
+const cx = (...classNames: any[]) => classNames.filter(Boolean).join(" ")
 
 type Pretty<T> = { [K in keyof T]: T[K] } & {}
 type Merge<P, T> = Pretty<Omit<P, keyof T> & T>
@@ -9,15 +18,24 @@ type LegacyProps = "as" | "legacyBehavior" | "passHref"
 type LinkComponent = FC<RefAttributes<HTMLAnchorElement> & LinkProps>
 
 export type LinkProps = Merge<
-  HTMLChakraProps<"a">,
+  HTMLChakraProps<"a"> & ThemingProps<"Link">,
   Omit<NextLinkProps, LegacyProps>
 >
 
 export const Link: LinkComponent = forwardRef<LinkProps, typeof NextLink>(
   function Link(props, ref) {
-    const { href, children, ...rest } = props
+    const styles = useStyleConfig("Link", props)
+    const { className, href, children, ...rest } = omitThemingProps(props)
+
     return (
-      <chakra.a ref={ref} href={href as any} {...rest} as={NextLink}>
+      <chakra.a
+        ref={ref}
+        href={href as any}
+        {...rest}
+        className={cx("chakra-link", className)}
+        __css={styles}
+        as={NextLink}
+      >
         {children}
       </chakra.a>
     )
