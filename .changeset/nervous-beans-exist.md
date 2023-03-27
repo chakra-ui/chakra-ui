b45f7d0b326a12e70010e8fb446e4a186f7175af
K Kudose <2448738+kkudose@users.noreply.github.com>
2023-03-26 22:52:55 -0500

    fix(toast): remove invalid role

---
+++ .changeset/nervous-beans-exist.md
@@ -0,0 +1,5 @@
+---
+"@chakra-ui/toast": patch
+---
+
+Remove invalid role in toast
--- packages/components/toast/src/toast.provider.tsx
+++ packages/components/toast/src/toast.provider.tsx
@@ -123,7 +123,6 @@ export const ToastProvider = (props: ToastProviderProps) => {
 
     return (
       <ul
-        role="region"
         aria-live="polite"
         key={position}
         id={`chakra-toast-manager-${position}`}
