19752320fceefca517e5866499c0e35e806ba28f
Paul Christophe <paul.christophe6@gmail.com>
2023-05-04 18:51:46 -0400

    Use array indices over array at method (#7624)

--- packages/components/avatar/src/avatar-name.tsx
+++ packages/components/avatar/src/avatar-name.tsx
@@ -4,8 +4,8 @@ import { AvatarOptions } from "./avatar-types"
 
 export function initials(name: string) {
   const names = name.split(" ")
-  const firstName = names.at(0) ?? ""
-  const lastName = names.length > 1 ? names.at(-1) : ""
+  const firstName = names[0] ?? ""
+  const lastName = names.length > 1 ? names[names.length - 1] : ""
   return firstName && lastName
     ? `${firstName.charAt(0)}${lastName.charAt(0)}`
     : firstName.charAt(0)
