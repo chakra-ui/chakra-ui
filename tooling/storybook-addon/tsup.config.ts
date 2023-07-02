efe3f4ad54c9cc99a810784a522eb4696f41e9ce
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-07-02 02:33:04 -0400

    refactor(storybook-addon): improve tsup config and force dts generation

--- tooling/storybook-addon/tsup.config.ts
+++ tooling/storybook-addon/tsup.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig({
-  entry: ["src"],
+export default defineConfig((options) => ({
+  entry: ["src/index.ts", "src/preview.ts", "src/manager.ts"],
   clean: true,
   external: ["@chakra-ui/storybook-addon"],
   format: ["esm", "cjs"],
-})
+  dts: {
+    resolve: true,
+  },
+  treeshake: "recommended",
+  sourcemap: true,
+  splitting: false,
+  minify: !options.watch,
+  platform: "browser",
+}))
