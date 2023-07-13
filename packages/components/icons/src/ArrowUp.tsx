eb13655285fc6dc8772b1518bbc4a1a11a285323
Segun Adebayo <joseshegs@gmail.com>
2023-07-13 23:11:51 +0100

    chore: remove icons and rearrange

--- packages/components/icons/src/ArrowUp.tsx
+++
@@ -1,6 +0,0 @@
-import { createIcon } from "@chakra-ui/icon"
-
-export const ArrowUpIcon = createIcon({
-  d: "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z",
-  displayName: "ArrowUpIcon",
-})
