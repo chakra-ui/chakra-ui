3003d3c3e97b9798b54192d83d9d1f5d01fb7e2f
Segun Adebayo <joseshegs@gmail.com>
2023-07-13 23:04:14 +0100

    refactor: move all components to single package

---
+++ packages/components/alert/alert.test.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { render, testA11y, screen } from "@chakra-ui/test-utils"
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from "."
+
+test("passes a11y test", async () => {
+  await testA11y(
+    <Alert>
+      <AlertIcon />
+      <AlertTitle>Alert title</AlertTitle>
+      <AlertDescription>Alert description</AlertDescription>
+    </Alert>,
+  )
+})
+
+test("should have role='alert'", () => {
+  render(
+    <Alert>
+      <AlertIcon />
+      <AlertTitle>Alert title</AlertTitle>
+      <AlertDescription>Alert description</AlertDescription>
+    </Alert>,
+  )
+
+  expect(screen.getByRole("alert")).toBeInTheDocument()
+})
