4c83a79bf998a3be809d7db1505509feccfb16fe
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-07-14 15:53:55 -0400

    refactor(storybook-addon): remove root `manager` and `preview` files

--- tooling/storybook-addon/tsup.config.ts
+++ tooling/storybook-addon/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts", "src/preview.ts", "src/manager.ts"],
+  entry: ["src"],
   clean: true,
   external: ["@chakra-ui/storybook-addon"],
   format: ["esm", "cjs"],
