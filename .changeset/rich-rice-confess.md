e16ee11eb1d9849d91604251fe83c96b60166913
Segun Adebayo <joseshegs@gmail.com>
2023-07-18 13:29:42 +0100

    fix: next image prop filter

---
+++ .changeset/rich-rice-confess.md
@@ -0,0 +1,5 @@
+---
+"@chakra-ui/next-js": patch
+---
+
+Fix issue where DOM image props are not forwarded to Image component
--- packages/integrations/next-js/src/image.tsx
+++ packages/integrations/next-js/src/image.tsx
@@ -4,21 +4,31 @@ import NextImage, { ImageProps as NextImageProps } from "next/image"
 export type ImageProps = NextImageProps &
   Omit<HTMLChakraProps<"img">, keyof NextImageProps>
 
+const imageProps: (keyof NextImageProps)[] = [
+  "src",
+  "alt",
+  "sizes",
+  "width",
+  "height",
+  "fill",
+  "loader",
+  "quality",
+  "priority",
+  "loading",
+  "placeholder",
+  "blurDataURL",
+  "unoptimized",
+  "onLoadingComplete",
+  "alt",
+  "crossOrigin",
+  "decoding",
+  "loading",
+  "referrerPolicy",
+  "sizes",
+  "src",
+  "useMap",
+]
+
 export const Image: ChakraComponent<"img", NextImageProps> = chakra(NextImage, {
-  shouldForwardProp: (prop) =>
-    [
-      "src",
-      "alt",
-      "width",
-      "height",
-      "fill",
-      "loader",
-      "quality",
-      "priority",
-      "loading",
-      "placeholder",
-      "blurDataURL",
-      "unoptimized",
-      "onLoadingComplete",
-    ].includes(prop),
+  shouldForwardProp: (prop) => (imageProps as string[]).includes(prop),
 })
