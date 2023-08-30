7d348da212c7126af9787bfcddf51e537f2c5d28
Segun Adebayo <joseshegs@gmail.com>
2023-08-30 10:06:40 +0100

    fix: system package client directive setup

--- packages/core/system/tsup.config.ts
+++ packages/core/system/tsup.config.ts
@@ -1,4 +1,14 @@
+import fs from "node:fs"
+import path from "node:path"
 import { defineConfig } from "tsup"
+
+function removeDirective(fileName: string) {
+  const file = path.resolve(__dirname, "dist", fileName)
+  return fs.promises.writeFile(
+    file,
+    fs.readFileSync(file, "utf-8").replace("'use client'\n", ""),
+  )
+}
 
 export default defineConfig({
   format: ["cjs", "esm"],
@@ -6,5 +16,11 @@ export default defineConfig({
   sourcemap: true,
   banner: {
     js: "'use client'",
+  },
+  async onSuccess() {
+    await Promise.all([
+      removeDirective("index.mjs"),
+      removeDirective("index.js"),
+    ])
   },
 })
