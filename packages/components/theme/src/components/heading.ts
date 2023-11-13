f3593bc1c6ddab7f79e540a2fa9daa93f52386f0
Suraj Pheudin <pheudinsuraj@gmail.com>
2023-11-13 11:07:13 +0545

    fix(Heading): using object syntax for fontsize not working in extendTheme (#7807)

--- packages/components/theme/src/components/heading.ts
+++ packages/components/theme/src/components/heading.ts
@@ -7,23 +7,32 @@ const baseStyle = defineStyle({
 
 const sizes = {
   "4xl": defineStyle({
-    fontSize: ["6xl", null, "7xl"],
+    fontSize: {
+      base: "6xl",
+      md: "7xl",
+    },
     lineHeight: 1,
   }),
   "3xl": defineStyle({
-    fontSize: ["5xl", null, "6xl"],
+    fontSize: {
+      base: "5xl",
+      md: "6xl",
+    },
     lineHeight: 1,
   }),
   "2xl": defineStyle({
-    fontSize: ["4xl", null, "5xl"],
+    fontsize: {
+      base: "4xl",
+      md: "5xl",
+    },
     lineHeight: [1.2, null, 1],
   }),
   xl: defineStyle({
-    fontSize: ["3xl", null, "4xl"],
+    fontSize: { base: "3xl", md: "6xl" },
     lineHeight: [1.33, null, 1.2],
   }),
   lg: defineStyle({
-    fontSize: ["2xl", null, "3xl"],
+    fontSize: { base: "2xl", md: "3xl" },
     lineHeight: [1.33, null, 1.2],
   }),
   md: defineStyle({
