17d0646711267b2691a2d359064c35a597ca355b
Ben Wiley <therealbenwiley@gmail.com>
2023-11-23 11:31:43 -0500

    fix: useCallbackRef now actually works

--- packages/hooks/use-callback-ref/src/index.ts
+++ packages/hooks/use-callback-ref/src/index.ts
@@ -1,15 +1,11 @@
-import { useCallback, useEffect, useRef } from "react"
+import { useCallback, useRef } from "react"
 
 export function useCallbackRef<T extends (...args: any[]) => any>(
   callback: T | undefined,
   deps: React.DependencyList = [],
 ) {
   const callbackRef = useRef(callback)
+  callbackRef.current = callback
 
-  useEffect(() => {
-    callbackRef.current = callback
-  })
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useCallback(((...args) => callbackRef.current?.(...args)) as T, deps)
+  return useCallback(((...args) => callbackRef.current?.(...args)) as T, [])
 }
