c6d4870e45e009373765970768dafa7a0a32d4af
Segun Adebayo <joseshegs@gmail.com>
2024-02-28 14:11:09 +0000

    fix: dont generate reset always

--- packages/components/src/styled-system-new/preflight.ts
+++ packages/components/src/styled-system-new/preflight.ts
@@ -4,6 +4,8 @@ import { PreflightConfig } from "./types"
 
 export function createPreflight(options: PreflightConfig) {
   const { preflight } = options
+  if (!preflight) return {}
+
   const { scope = "", level = "parent" } = isObject(preflight) ? preflight : {}
 
   let selector = ""
