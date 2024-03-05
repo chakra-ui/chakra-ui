350370c11b973114e38cb9d52affb4df23569657
Segun Adebayo <joseshegs@gmail.com>
2024-03-05 19:48:16 +0000

    chore: phew

--- packages/storybook-addon/src/preview.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectAnnotations, Renderer } from "@storybook/types"
-import { Provider } from "./utils/provider"
-
-const preview: ProjectAnnotations<Renderer> = {
-  decorators: [Provider],
-}
-
-export default preview
