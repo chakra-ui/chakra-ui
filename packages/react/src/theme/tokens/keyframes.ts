059eed9ff00eeb9b0f5bb1bbdf2f0c98f6d416d8
Segun Adebayo <joseshegs@gmail.com>
2024-03-07 14:58:20 +0000

    feat: add generic enter and exit keyframes

--- packages/react/src/theme/tokens/keyframes.ts
+++ packages/react/src/theme/tokens/keyframes.ts
@@ -60,4 +60,18 @@ export const keyframes = defineKeyframes({
     from: { translate: "0 100%" },
     to: { translate: "0 0" },
   },
+  enter: {
+    from: {
+      opacity: "var(--enter-opacity, 1)",
+      transform:
+        "translate3d(var(--enter-translate-x, 0), var(--enter-translate-y, 0), 0) scale3d(var(--enter-scale, 1), var(--enter-scale, 1), var(--enter-scale, 1)) rotate(var(--enter-rotate, 0))",
+    },
+  },
+  exit: {
+    to: {
+      opacity: "var(--exit-opacity, 1)",
+      transform:
+        "translate3d(var(--exit-translate-x, 0), var(--exit-translate-y, 0), 0) scale3d(var(--exit-scale, 1), var(--exit-scale, 1), var(--exit-scale, 1)) rotate(var(--exit-rotate, 0))",
+    },
+  },
 })
