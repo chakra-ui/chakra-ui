76a9e3bc056a8b98f3dec545c8de6069d3410278
Segun Adebayo <joseshegs@gmail.com>
2024-03-28 15:33:48 +0000

    chore(examples): prepare vite example

--- examples/vite-ts/src/main.tsx
+++ examples/vite-ts/src/main.tsx
@@ -1,12 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <App />
+      </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
