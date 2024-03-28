52d499ec0c05ead216dd72639bd4fd758353948d
Segun Adebayo <joseshegs@gmail.com>
2024-03-28 16:38:18 +0000

    chore(examples): prepare next 14 pages

--- examples/next-pages/pages/_app.tsx
+++ examples/next-pages/pages/_app.tsx
@@ -1,12 +1,13 @@
-export interface AppRenderProps {
-  pageProps: object
-  err?: Error
-  Component: NextComponentType<NextPageContext, AppRenderProps, object>
-  router: NextRouter
-}
-import type { NextComponentType, NextPageContext } from "next"
-import type { NextRouter } from "next/router"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
+import { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }: AppRenderProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ChakraProvider>
+  )
 }
--- examples/next-pages/pages/index.tsx
+++ examples/next-pages/pages/index.tsx
@@ -1,49 +1,67 @@
-import { Box, Code, Grid, Link, Text, VStack } from "@chakra-ui/react"
-import { Suspense } from "react"
-import { Chakra } from "../Chakra"
-import { Layout } from "../components/Layout"
-import { Logo } from "../components/Logo"
-import { NextChakraLink } from "../components/NextChakraLink"
+import {
+  Box,
+  Button,
+  Checkbox,
+  ClientOnly,
+  HStack,
+  Heading,
+  Progress,
+  RadioGroup,
+  Skeleton,
+  VStack,
+} from "@chakra-ui/react"
+import Image from "next/image"
+import { ColorModeToggle } from "../components/color-mode-toggle"
 
-interface IndexProps {
-  cookies?: string
+export default function Page() {
+  return (
+    <Box textAlign="center" fontSize="xl" pt="30vh">
+      <VStack gap="8">
+        <Image
+          alt="chakra logo"
+          src="/static/logo.svg"
+          width="80"
+          height="80"
+        />
+        <Heading size="2xl" letterSpacing="tight">
+          Welcome to Chakra UI v3 + Next.js (App)
+        </Heading>
+
+        <HStack gap="10">
+          <Checkbox.Root defaultChecked>
+            <Checkbox.Control />
+            <Checkbox.Label>Checkbox</Checkbox.Label>
+          </Checkbox.Root>
+
+          <RadioGroup.Root display="inline-flex" defaultValue="1">
+            <RadioGroup.Item value="1" mr="2">
+              <RadioGroup.ItemControl />
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+            <RadioGroup.Item value="2">
+              <RadioGroup.ItemControl />
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </RadioGroup.Root>
+        </HStack>
+
+        <Progress.Root width="300px" value={65} hasStripe>
+          <Progress.Track>
+            <Progress.FilledTrack />
+          </Progress.Track>
+        </Progress.Root>
+
+        <HStack>
+          <Button>Let's go!</Button>
+          <Button variant="outline">bun install @chakra-ui/react</Button>
+        </HStack>
+      </VStack>
+
+      <Box pos="absolute" top="4" right="4">
+        <ClientOnly fallback={<Skeleton w="10" h="10" rounded="md" />}>
+          <ColorModeToggle />
+        </ClientOnly>
+      </Box>
+    </Box>
+  )
 }
-
-const IndexPage = ({ cookies }: IndexProps) => (
-  <Chakra cookies={cookies}>
-    <Layout title="Next.js + TypeScript example">
-      <Suspense fallback="Loading...">
-        <Box textAlign="center" fontSize="xl">
-          <Grid minH="100vh" p={3}>
-            <VStack gap={8}>
-              <Logo h="40vmin" pointerEvents="none" />
-              <Text>
-                Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
-                reload.
-                <br />
-                <br />
-                <NextChakraLink href="/properties" color="teal.500">
-                  View the properties
-                </NextChakraLink>{" "}
-                to see the Nextjs <Code fontSize="xl">&lt;Link&gt;</Code> in
-                action
-              </Text>
-              <Link
-                color="teal.500"
-                fontSize="2xl"
-                href="https://chakra-ui.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn more about Chakra
-              </Link>
-            </VStack>
-          </Grid>
-        </Box>
-      </Suspense>
-    </Layout>
-  </Chakra>
-)
-
-export default IndexPage
-export { getServerSideProps } from "../Chakra"
