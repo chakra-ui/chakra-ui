f479e6e4e065af98cfddbc827c900380ccb4232e
Segun Adebayo <joseshegs@gmail.com>
2024-05-08 18:11:24 +0100

    fix: examples and website

--- examples/vite-ts/vite.config.ts
+++ examples/vite-ts/vite.config.ts
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    conditions: ["source"],
+    alias: {
+      "@chakra-ui/utils": resolve("..", "..", "packages/utils/src"),
+      "@chakra-ui/react": resolve("..", "..", "packages/react/src"),
+      "@chakra-ui/hooks": resolve("..", "..", "packages/hooks/src"),
+    },
   },
 })
