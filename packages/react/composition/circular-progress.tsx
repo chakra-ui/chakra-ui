5a2875776a235dbdb62dc48c3aea4f1e725ae707
Segun Adebayo <joseshegs@gmail.com>
2024-06-20 02:48:05 +0100

    feat(compositions): add circular progress

---
+++ packages/react/composition/circular-progress.tsx
@@ -0,0 +1,27 @@
+import { forwardRef } from "react"
+import { CircularProgress as Progress, type SystemStyleObject } from "../src"
+
+export interface CircularProgressProps extends Progress.RootProps {
+  showValue?: boolean
+  trackColor?: SystemStyleObject["stroke"]
+  capIsRound?: boolean
+}
+
+export const CircularProgress = forwardRef<
+  HTMLDivElement,
+  CircularProgressProps
+>(function CircularProgress(props, ref) {
+  const { value, showValue, trackColor, capIsRound, color, ...rest } = props
+  return (
+    <Progress.Root ref={ref} valuePlacement="center" {...rest}>
+      <Progress.ValueText />
+      <Progress.Circle>
+        <Progress.Track stroke={trackColor} />
+        <Progress.Range
+          stroke={color}
+          strokeLinecap={capIsRound ? "round" : undefined}
+        />
+      </Progress.Circle>
+    </Progress.Root>
+  )
+})
