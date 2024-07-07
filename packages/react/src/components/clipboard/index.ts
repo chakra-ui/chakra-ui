cc87eaea36ed2124004f8931b79a4769c34d0e11
Segun Adebayo <joseshegs@gmail.com>
2024-07-07 18:26:26 +0100

    feat: add clipboard component

---
+++ packages/react/src/components/clipboard/index.ts
@@ -0,0 +1,24 @@
+export {
+  ClipboardControl,
+  ClipboardIndicator,
+  ClipboardRoot,
+  ClipboardTrigger,
+  ClipboardInput,
+  useClipboardStyles,
+} from "./clipboard"
+
+export type {
+  ClipboardControlProps,
+  ClipboardIndicatorProps,
+  ClipboardRootProps,
+  ClipboardTriggerProps,
+  ClipboardInputProps,
+} from "./clipboard"
+
+export {
+  useClipboard,
+  useClipboardContext,
+  ClipboardContext,
+} from "@ark-ui/react/clipboard"
+
+export * as Clipboard from "./namespace"
---
+++ packages/react/src/components/clipboard/namespace.ts
@@ -0,0 +1,17 @@
+export {
+  ClipboardControl as Control,
+  ClipboardIndicator as Indicator,
+  ClipboardRoot as Root,
+  ClipboardTrigger as Trigger,
+  ClipboardInput as Input,
+} from "./clipboard"
+
+export type {
+  ClipboardControlProps as ControlProps,
+  ClipboardIndicatorProps as IndicatorProps,
+  ClipboardRootProps as RootProps,
+  ClipboardTriggerProps as TriggerProps,
+  ClipboardInputProps as InputProps,
+} from "./clipboard"
+
+export { ClipboardContext as Context } from "@ark-ui/react/clipboard"
