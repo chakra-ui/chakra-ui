d7e5ee946169d46ed27e04f111c36a906827dc3e
Segun Adebayo <joseshegs@gmail.com>
2024-07-09 21:46:19 +0100

    refactor: accordion example

--- apps/compositions/src/examples/accordion-controlled.tsx
+++ apps/compositions/src/examples/accordion-controlled.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import {
   AccordionItem,
   AccordionItemContent,
   AccordionItemTrigger,
   AccordionRoot,
 } from "compositions/ui/accordion"
+import { useState } from "react"
 
 const items = [
   { value: "a", title: "First Item", text: "Some value 1..." },
@@ -11,9 +14,10 @@ const items = [
   { value: "c", title: "Third Item", text: "Some value 3..." },
 ]
 
-export const AccordionBasic = () => {
+export const AccordionControlled = () => {
+  const [value, setValue] = useState(["b"])
   return (
-    <AccordionRoot collapsible defaultValue={["b"]}>
+    <AccordionRoot value={value} onValueChange={(e) => setValue(e.value)}>
       {items.map((item, index) => (
         <AccordionItem key={index} value={item.value}>
           <AccordionItemTrigger>{item.title}</AccordionItemTrigger>
