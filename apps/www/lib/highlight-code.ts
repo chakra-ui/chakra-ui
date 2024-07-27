bf483eacb7acae5876ca7f9e1ce41500685b6334
Segun Adebayo <joseshegs@gmail.com>
2024-07-27 20:18:56 +0100

    docs: use consistent theme

--- apps/www/lib/highlight-code.ts
+++ apps/www/lib/highlight-code.ts
@@ -5,7 +5,7 @@ export const highlightCode = (code: string) => {
     lang: "tsx",
     themes: {
       light: "github-light",
-      dark: "vesper",
+      dark: "dark-plus",
     },
     defaultColor: "light",
   })
