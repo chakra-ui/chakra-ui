7d082e8c5704b807b82462453a7af009281dee80
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2024-07-27 10:58:36 -0400

    float examples

---
+++ apps/compositions/src/examples/float-with-offset.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Box, Circle, Float, Stack } from "@chakra-ui/react"
+
+const placements = [
+  "bottom-end",
+  "bottom-start",
+  "top-end",
+  "top-start",
+  "bottom-center",
+  "top-center",
+  "middle-center",
+  "middle-end",
+  "middle-start",
+] as const
+
+export const FloatWithOffset = () => (
+  <Stack gap="10">
+    {placements.map((placement) => (
+      <Stack key={placement} gap="3">
+        <p>{placement}</p>
+        <Box position="relative" width="80px" height="80px" bg="gray.500">
+          <Float placement={placement} offset="4">
+            <Circle size="5" bg="red.500">
+              3
+            </Circle>
+          </Float>
+        </Box>
+      </Stack>
+    ))}
+  </Stack>
+)
