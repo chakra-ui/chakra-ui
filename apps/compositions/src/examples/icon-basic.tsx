7aefdd5c50b021e685459e2e04fdd47fc2a7872c
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2024-07-27 11:40:14 -0400

    add icon examples

---
+++ apps/compositions/src/examples/icon-basic.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { Icon } from "@chakra-ui/react"
+import { HiHeart } from "react-icons/hi"
+
+export const IconBasic = () => (
+  <Icon asChild fontSize="100px" color="pink.700">
+    <HiHeart />
+  </Icon>
+)
