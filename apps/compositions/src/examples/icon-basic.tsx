f8c9b82f95fbdaedd298eb484b869c138e6b7622
Segun Adebayo <joseshegs@gmail.com>
2024-07-30 20:46:16 +0100

    docs: icon

--- apps/compositions/src/examples/icon-basic.tsx
+++ apps/compositions/src/examples/icon-basic.tsx
@@ -1,10 +1,8 @@
-"use client"
-
 import { Icon } from "@chakra-ui/react"
 import { HiHeart } from "react-icons/hi"
 
 export const IconBasic = () => (
-  <Icon asChild fontSize="100px" color="pink.700">
+  <Icon asChild fontSize="2xl" color="pink.700">
     <HiHeart />
   </Icon>
 )
--- apps/compositions/src/examples/icon-with-react-icon.tsx
+++ apps/compositions/src/examples/icon-with-react-icon.tsx
@@ -1,8 +1,8 @@
-"use client"
-
 import { Icon } from "@chakra-ui/react"
 import { Md3dRotation } from "react-icons/md"
 
 export const IconWithReactIcon = () => (
-  <Icon as={Md3dRotation} boxSize="40px" color="tomato" />
+  <Icon asChild boxSize="40px" color="tomato">
+    <Md3dRotation />
+  </Icon>
 )
