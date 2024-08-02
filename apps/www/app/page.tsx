be6be8369c850ceca68a2287cd8cfae5bc09e0dd
anubra266 <anubra266@gmail.com>
2024-08-01 23:05:41 -0500

    chore: refactor

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,18 +1,8 @@
-import {
-  Box,
-  Button,
-  Circle,
-  Container,
-  Heading,
-  Icon,
-  Span,
-  Stack,
-  Text,
-} from "@chakra-ui/react"
-import { HiArrowRight } from "react-icons/hi"
+import { Box, Circle, Container, Icon, Stack } from "@chakra-ui/react"
 import { Accessibility } from "./page/accessibility"
 import { DesignSystem } from "./page/design-system"
 import { Header } from "./page/header"
+import { Hero } from "./page/hero"
 import { HeroShowCase } from "./page/hero-showcase"
 import { BlitzIcon } from "./page/icons"
 import { Partners } from "./page/partners"
@@ -40,58 +30,13 @@ export default function Page() {
 
       <Container pt="8">
         <Stack gap="8">
-          <Button
-            size="md"
-            colorPalette="teal"
-            variant="outline"
-            w="fit-content"
-            bg="#061416"
-            px="4"
-            py="2.5"
-            fontWeight="bold"
-            gap="2.5"
-          >
-            Celebrating the launch of v3
-            <HiArrowRight />
-          </Button>
-          <Stack gap="5" pr="4" maxW="3xl" px="1.5">
-            <Heading size="5xl" fontWeight="bold">
-              Chakra UI is a component system for building products{" "}
-              <Span
-                color="teal.500"
-                pos="relative"
-                px="2"
-                display="inline-block"
-                _before={{
-                  pos: "absolute",
-                  content: "''",
-                  w: "full",
-                  h: "full",
-                  bg: "teal.500/10",
-                  bottom: "-3px",
-                  left: "0",
-                  borderRight: "solid 1.5px",
-                  borderColor: "currentColor",
-                }}
-              >
-                with speed
-              </Span>
-            </Heading>
-            <Text fontSize="2xl" color="gray.400" fontWeight="medium">
-              Beautiful, accessible, React components for high-quality web apps
-              and design systems.
-            </Text>
-          </Stack>
-
+          <Hero />
           <HeroShowCase />
 
           <Stack gap="52">
             <Partners />
-
             <DesignSystem />
-
             <Accessibility />
-
             <Stats />
           </Stack>
         </Stack>
---
+++ apps/www/app/page/hero.tsx
@@ -0,0 +1,49 @@
+import { Button, Heading, Span, Stack, Text } from "@chakra-ui/react"
+import { HiArrowRight } from "react-icons/hi"
+
+export const Hero = () => (
+  <>
+    <Button
+      size="md"
+      colorPalette="teal"
+      variant="outline"
+      w="fit-content"
+      bg="#061416"
+      px="4"
+      py="2.5"
+      fontWeight="bold"
+      gap="2.5"
+    >
+      Celebrating the launch of v3
+      <HiArrowRight />
+    </Button>
+    <Stack gap="5" pr="4" maxW="3xl" px="1.5">
+      <Heading size="5xl" fontWeight="bold">
+        Chakra UI is a component system for building products{" "}
+        <Span
+          color="teal.500"
+          pos="relative"
+          px="2"
+          display="inline-block"
+          _before={{
+            pos: "absolute",
+            content: "''",
+            w: "full",
+            h: "full",
+            bg: "teal.500/10",
+            bottom: "-3px",
+            left: "0",
+            borderRight: "solid 1.5px",
+            borderColor: "currentColor",
+          }}
+        >
+          with speed
+        </Span>
+      </Heading>
+      <Text fontSize="2xl" color="gray.400" fontWeight="medium">
+        Beautiful, accessible, React components for high-quality web apps and
+        design systems.
+      </Text>
+    </Stack>
+  </>
+)
