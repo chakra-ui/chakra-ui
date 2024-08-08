4944de88a053b19eefaa3f1019e88cb129b3d20f
Esther Agbaje <53586167+estheragbaje@users.noreply.github.com>
2024-08-08 19:43:38 +0100

    docs: review typography components (#8723)
    * docs: updated typography components
    
    * docs: updates

--- apps/compositions/src/examples/mark-basic.tsx
+++ apps/compositions/src/examples/mark-basic.tsx
@@ -3,7 +3,11 @@ import { Mark, Text } from "@chakra-ui/react"
 export const MarkBasic = () => {
   return (
     <Text>
-      The <Mark>design system</Mark> is a collection of UI elements
+      The
+      <Mark variant="subtle" colorPalette="yellow">
+        design system
+      </Mark>
+      is a collection of UI elements
     </Text>
   )
 }
