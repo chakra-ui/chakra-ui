3f4f1e633d48daa56dba4f79d772b3ef99481a86
Segun Adebayo <joseshegs@gmail.com>
2024-08-15 17:43:23 +0100

    fix: cx

--- packages/utils/src/cx.ts
+++ packages/utils/src/cx.ts
@@ -1,1 +1,5 @@
-export const cx = (...classNames: any[]) => classNames.filter(Boolean).join(" ")
+export const cx = (...classNames: any[]) =>
+  classNames
+    .filter(Boolean)
+    .map((r) => r.trim())
+    .join(" ")
