3a7ca8ea8b31f6845b44a47572769a7315273fee
Segun Adebayo <joseshegs@gmail.com>
2024-08-16 10:32:09 +0100

    docs: add accordion disabled

--- packages/react/__stories__/accordion.stories.tsx
+++ packages/react/__stories__/accordion.stories.tsx
@@ -13,8 +13,9 @@ export default {
 } satisfies Meta
 
 export { AccordionBasic as Basic } from "compositions/examples/accordion-basic"
-export { AccordionVariantTable as Variants } from "compositions/examples/accordion-variant-table"
+export { AccordionControlled as Controlled } from "compositions/examples/accordion-controlled"
 export { AccordionSizeTable as Sizes } from "compositions/examples/accordion-size-table"
+export { AccordionVariantTable as Variants } from "compositions/examples/accordion-variant-table"
+export { AccordionWithDisabledItem as WithDisabledItem } from "compositions/examples/accordion-with-disabled-item"
 export { AccordionWithIcon as Icon } from "compositions/examples/accordion-with-icon"
 export { AccordionWithMultiple as WithMultiple } from "compositions/examples/accordion-with-multiple"
-export { AccordionControlled as Controlled } from "compositions/examples/accordion-controlled"
