436c79195f703183e46244373fd0784bfcdd0535
Segun Adebayo <joseshegs@gmail.com>
2024-08-16 10:40:06 +0100

    fix: pagination stories

--- packages/react/__stories__/pagination.stories.tsx
+++ packages/react/__stories__/pagination.stories.tsx
@@ -12,12 +12,12 @@ export default {
   ],
 } satisfies Meta
 
+export { PaginationAttached as Attached } from "compositions/examples/pagination-attached"
 export { PaginationBasic as Basic } from "compositions/examples/pagination-basic"
-export { PaginationControlled as Controlled } from "compositions/examples/pagination-controlled"
-export { PaginationWithSizes as Sizes } from "compositions/examples/pagination-with-sizes"
-export { PaginationWithSiblingCount as WithSiblingCount } from "compositions/examples/pagination-with-sibling-count"
-export { PaginationWithVariantMap as WithVariantMap } from "compositions/examples/pagination-with-variant-map"
 export { PaginationCompact as Compact } from "compositions/examples/pagination-compact"
+export { PaginationControlled as Controlled } from "compositions/examples/pagination-controlled"
 export { PaginationWithContent as WithContent } from "compositions/examples/pagination-with-content"
 export { PaginationWithCountText as WithCountText } from "compositions/examples/pagination-with-count-text"
-export { PaginationAttached as Attached } from "compositions/examples/pagination-attached"
+export { PaginationWithSiblingCount as WithSiblingCount } from "compositions/examples/pagination-with-sibling-count"
+export { PaginationWithSizes as Sizes } from "compositions/examples/pagination-with-sizes"
+export { PaginationWithVariants as WithVariants } from "compositions/examples/pagination-with-variants"
