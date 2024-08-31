90ea56c6d6a26159f39fbbf63b4fc20ad38a0118
Ivica Batinić <ivicabatinic123@gmail.com>
2024-08-31 19:03:34 +0200

    chore: add other pages to search

--- apps/www/lib/search.ts
+++ apps/www/lib/search.ts
@@ -1,5 +1,32 @@
 import { docs } from ".velite"
 
+const getStarted = docs
+  .filter((page) => page.category === "docs/get-started")
+  .map((page) => ({
+    label: page.title,
+    value: page.slug,
+    description: page.description,
+    category: "Get started",
+  }))
+
+const styling = docs
+  .filter((page) => page.category === "docs/styling")
+  .map((page) => ({
+    label: page.title,
+    value: page.slug,
+    description: page.description,
+    category: "Styling",
+  }))
+
+const theming = docs
+  .filter((page) => page.category === "docs/styling")
+  .map((page) => ({
+    label: page.title,
+    value: page.slug,
+    description: page.description,
+    category: "Styling",
+  }))
+
 const components = docs
   .filter((page) => page.category === "docs/components")
   .map((page) => ({
@@ -9,4 +36,4 @@ const components = docs
     category: "Component",
   }))
 
-export const data = { components }
+export const data = { getStarted, styling, theming, components }
