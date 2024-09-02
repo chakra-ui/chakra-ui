65ba3de80a1f47272df78cc8b132f823517a2ef9
Ivica Batinić <ivicabatinic123@gmail.com>
2024-09-02 09:13:50 +0200

    feat: init setup

--- apps/www/app/provider.tsx
+++ apps/www/app/provider.tsx
@@ -1,17 +1,9 @@
 "use client"
 
-import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import { Toaster } from "compositions/ui/toaster"
 import { ThemeProvider } from "next-themes"
-
-const system = createSystem(defaultConfig, {
-  globalCss: {
-    ":root": {
-      "--header-height": { base: "64px", md: "104px" },
-      "--content-height": "calc(100dvh - var(--header-height))",
-    },
-  },
-})
+import { system } from "./theme"
 
 export const Provider = (props: { children: React.ReactNode }) => {
   return (
