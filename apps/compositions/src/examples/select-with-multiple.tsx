62ff92d1d8415b69dec96952016096ec348c4115
Segun Adebayo <joseshegs@gmail.com>
2024-10-01 19:10:49 +0100

    chore: add select multiple example

---
+++ apps/compositions/src/examples/select-with-multiple.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { createListCollection } from "@chakra-ui/react"
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "compositions/ui/select"
+
+export const SelectWithMultiple = () => {
+  return (
+    <SelectRoot multiple collection={frameworks} size="sm" width="320px">
+      <SelectLabel>Select framework</SelectLabel>
+      <SelectTrigger>
+        <SelectValueText placeholder="Movie" />
+      </SelectTrigger>
+      <SelectContent>
+        {frameworks.items.map((movie) => (
+          <SelectItem item={movie} key={movie.value}>
+            {movie.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </SelectRoot>
+  )
+}
+
+const frameworks = createListCollection({
+  items: [
+    { label: "React.js", value: "react" },
+    { label: "Vue.js", value: "vue" },
+    { label: "Angular", value: "angular" },
+    { label: "Svelte", value: "svelte" },
+  ],
+})
--- packages/react/__stories__/select.stories.tsx
+++ packages/react/__stories__/select.stories.tsx
@@ -22,6 +22,7 @@ export { SelectWithDisabled as Disabled } from "compositions/examples/select-wit
 export { SelectWithDisabledOption as DisabledOption } from "compositions/examples/select-with-disabled-option"
 export { SelectWithHookForm as HookForm } from "compositions/examples/select-with-hook-form"
 export { SelectWithInvalid as Invalid } from "compositions/examples/select-with-invalid"
+export { SelectWithMultiple as Multiple } from "compositions/examples/select-with-multiple"
 export { SelectWithOptionGroup as OptionGroup } from "compositions/examples/select-with-option-group"
 export { SelectWithOverflow as Overflow } from "compositions/examples/select-with-overflow"
 export { SelectWithPositioning as Positioning } from "compositions/examples/select-with-positioning"
