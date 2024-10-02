670a2db1bfa09776fcea1ef54fc8e62c32a347c8
Segun Adebayo <joseshegs@gmail.com>
2024-10-02 19:17:35 +0100

    fix: extend theme exports

--- packages/components/src/extend-theme/index.ts
+++ packages/components/src/extend-theme/index.ts
@@ -1,1 +1,5 @@
 export * from "./extend-theme"
+export * from "./with-default-color-scheme"
+export * from "./with-default-props"
+export * from "./with-default-size"
+export * from "./with-default-variant"
