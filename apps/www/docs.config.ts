5c12231e81f14cb19eb761120325c26d72c29e5c
Segun Adebayo <joseshegs@gmail.com>
2024-10-03 20:26:46 +0100

    docs: change nav order

--- apps/www/docs.config.ts
+++ apps/www/docs.config.ts
@@ -30,112 +30,6 @@ const docsLinks: NavItem = {
     },
 
     {
-      title: "Styling",
-      url: "styling",
-      items: [
-        {
-          title: "Concepts",
-          items: [
-            { title: "Overview", url: "overview" },
-            { title: "Chakra Factory", url: "chakra-factory" },
-            { title: "Responsive Design", url: "responsive-design" },
-            { title: "CSS Variables", url: "css-variables" },
-            { title: "Dark Mode", url: "dark-mode" },
-            { title: "Color Opacity Modifier", url: "color-opacity-modifier" },
-            { title: "Conditional Styles", url: "conditional-styles" },
-            { title: "Virtual Color", url: "virtual-color" },
-            { title: "Cascade Layers", url: "cascade-layers" },
-          ],
-        },
-        {
-          title: "Compositions",
-          items: [
-            { title: "Text Styles", url: "text-styles" },
-            { title: "Layer Styles", url: "layer-styles" },
-            { title: "Animation Styles", url: "animation-styles" },
-            { title: "Focus Ring", url: "focus-ring" },
-          ],
-        },
-        {
-          title: "Style Props",
-          url: "style-props",
-          items: [
-            { title: "Background", url: "background" },
-            { title: "Border", url: "border" },
-            { title: "Display", url: "display" },
-            { title: "Effects", url: "effects" },
-            { title: "Filters", url: "filters" },
-            { title: "Flex and Grid", url: "flex-and-grid" },
-            { title: "Interactivity", url: "interactivity" },
-            { title: "Layout", url: "layout" },
-            { title: "List", url: "list" },
-            { title: "Sizing", url: "sizing" },
-            { title: "Spacing", url: "spacing" },
-            { title: "SVG", url: "svg" },
-            { title: "Tables", url: "tables" },
-            { title: "Transforms", url: "transforms" },
-            { title: "Transitions", url: "transitions" },
-            { title: "Typography", url: "typography" },
-          ],
-        },
-      ],
-    },
-
-    {
-      title: "Theming",
-      url: "theming",
-      items: [
-        {
-          title: "Concepts",
-          items: [
-            { title: "Overview", url: "overview" },
-            { title: "Tokens", url: "tokens" },
-            { title: "Semantic Tokens", url: "semantic-tokens" },
-            { title: "Recipes", url: "recipes" },
-            { title: "Slot Recipes", url: "slot-recipes" },
-          ],
-        },
-        {
-          title: "Design Tokens",
-          items: [
-            { title: "Animations", url: "animations" },
-            { title: "Aspect Ratios", url: "aspect-ratios" },
-            { title: "Breakpoints", url: "breakpoints" },
-            { title: "Colors", url: "colors" },
-            { title: "Radii", url: "radii" },
-            { title: "Shadows", url: "shadows" },
-            { title: "Sizes", url: "sizes" },
-            { title: "Spacing", url: "spacing" },
-            { title: "Typography", url: "typography" },
-            { title: "Z-Index", url: "z-index" },
-          ],
-        },
-        {
-          title: "Compositions",
-          items: [
-            { title: "Text Styles", url: "text-styles" },
-            { title: "Layer Styles", url: "layer-styles" },
-          ],
-        },
-        {
-          title: "Customization",
-          url: "customization",
-          items: [
-            { title: "Overview", url: "overview" },
-            { title: "Breakpoints", url: "breakpoints" },
-            { title: "Colors", url: "colors" },
-            { title: "Conditions", url: "conditions" },
-            { title: "CSS Variables", url: "css-variables" },
-            { title: "Global CSS", url: "global-css" },
-            { title: "Recipes", url: "recipes" },
-            { title: "Sizes", url: "sizes" },
-            { title: "Spacing", url: "spacing" },
-          ],
-        },
-      ],
-    },
-
-    {
       title: "Components",
       url: "components",
       items: [
@@ -252,6 +146,112 @@ const docsLinks: NavItem = {
             { title: "Portal", url: "portal" },
             { title: "Show", url: "show" },
             { title: "Visually Hidden", url: "visually-hidden" },
+          ],
+        },
+      ],
+    },
+
+    {
+      title: "Styling",
+      url: "styling",
+      items: [
+        {
+          title: "Concepts",
+          items: [
+            { title: "Overview", url: "overview" },
+            { title: "Chakra Factory", url: "chakra-factory" },
+            { title: "Responsive Design", url: "responsive-design" },
+            { title: "CSS Variables", url: "css-variables" },
+            { title: "Dark Mode", url: "dark-mode" },
+            { title: "Color Opacity Modifier", url: "color-opacity-modifier" },
+            { title: "Conditional Styles", url: "conditional-styles" },
+            { title: "Virtual Color", url: "virtual-color" },
+            { title: "Cascade Layers", url: "cascade-layers" },
+          ],
+        },
+        {
+          title: "Compositions",
+          items: [
+            { title: "Text Styles", url: "text-styles" },
+            { title: "Layer Styles", url: "layer-styles" },
+            { title: "Animation Styles", url: "animation-styles" },
+            { title: "Focus Ring", url: "focus-ring" },
+          ],
+        },
+        {
+          title: "Style Props",
+          url: "style-props",
+          items: [
+            { title: "Background", url: "background" },
+            { title: "Border", url: "border" },
+            { title: "Display", url: "display" },
+            { title: "Effects", url: "effects" },
+            { title: "Filters", url: "filters" },
+            { title: "Flex and Grid", url: "flex-and-grid" },
+            { title: "Interactivity", url: "interactivity" },
+            { title: "Layout", url: "layout" },
+            { title: "List", url: "list" },
+            { title: "Sizing", url: "sizing" },
+            { title: "Spacing", url: "spacing" },
+            { title: "SVG", url: "svg" },
+            { title: "Tables", url: "tables" },
+            { title: "Transforms", url: "transforms" },
+            { title: "Transitions", url: "transitions" },
+            { title: "Typography", url: "typography" },
+          ],
+        },
+      ],
+    },
+
+    {
+      title: "Theming",
+      url: "theming",
+      items: [
+        {
+          title: "Concepts",
+          items: [
+            { title: "Overview", url: "overview" },
+            { title: "Tokens", url: "tokens" },
+            { title: "Semantic Tokens", url: "semantic-tokens" },
+            { title: "Recipes", url: "recipes" },
+            { title: "Slot Recipes", url: "slot-recipes" },
+          ],
+        },
+        {
+          title: "Design Tokens",
+          items: [
+            { title: "Animations", url: "animations" },
+            { title: "Aspect Ratios", url: "aspect-ratios" },
+            { title: "Breakpoints", url: "breakpoints" },
+            { title: "Colors", url: "colors" },
+            { title: "Radii", url: "radii" },
+            { title: "Shadows", url: "shadows" },
+            { title: "Sizes", url: "sizes" },
+            { title: "Spacing", url: "spacing" },
+            { title: "Typography", url: "typography" },
+            { title: "Z-Index", url: "z-index" },
+          ],
+        },
+        {
+          title: "Compositions",
+          items: [
+            { title: "Text Styles", url: "text-styles" },
+            { title: "Layer Styles", url: "layer-styles" },
+          ],
+        },
+        {
+          title: "Customization",
+          url: "customization",
+          items: [
+            { title: "Overview", url: "overview" },
+            { title: "Breakpoints", url: "breakpoints" },
+            { title: "Colors", url: "colors" },
+            { title: "Conditions", url: "conditions" },
+            { title: "CSS Variables", url: "css-variables" },
+            { title: "Global CSS", url: "global-css" },
+            { title: "Recipes", url: "recipes" },
+            { title: "Sizes", url: "sizes" },
+            { title: "Spacing", url: "spacing" },
           ],
         },
       ],
