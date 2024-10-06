13354bfba8637cfd75f6ce9f1aab086580f7e3ae
Segun Adebayo <joseshegs@gmail.com>
2024-10-06 17:27:01 +0100

    chore: add close button stories

---
+++ packages/react/__stories__/close-button.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta } from "@storybook/react"
+import { Box } from "../src"
+
+export default {
+  title: "Components / Close Button",
+  decorators: [
+    (Story) => (
+      <Box p="10">
+        <Story />
+      </Box>
+    ),
+  ],
+} satisfies Meta
+
+export { CloseButtonBasic as Basic } from "compositions/examples/close-button-basic"
+export { CloseButtonWithSizes as Sizes } from "compositions/examples/close-button-with-sizes"
+export { CloseButtonWithVariants as Variants } from "compositions/examples/close-button-with-variants"
