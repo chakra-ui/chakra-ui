9cf6dfe9ca170196df2f0c537db2777842153c79
Segun Adebayo <joseshegs@gmail.com>
2024-10-07 18:14:33 +0100

    docs: add floating label example

---
+++ apps/compositions/src/examples/input-with-floating-label.tsx
@@ -0,0 +1,34 @@
+import { Box, Field, Input, defineStyle } from "@chakra-ui/react"
+
+export const InputWithFloatingLabel = () => {
+  return (
+    <Field.Root>
+      <Box pos="relative" w="full">
+        <Input className="peer" placeholder="" />
+        <Field.Label css={floatingStyles}>Email</Field.Label>
+      </Box>
+      <Field.HelperText>We will never share your email.</Field.HelperText>
+    </Field.Root>
+  )
+}
+
+const floatingStyles = defineStyle({
+  pos: "absolute",
+  bg: "bg",
+  px: "0.5",
+  top: "-3",
+  insetStart: "2",
+  fontWeight: "normal",
+  pointerEvents: "none",
+  transition: "position",
+  _peerPlaceholderShown: {
+    color: "fg.subtle",
+    top: "2.5",
+    insetStart: "3",
+  },
+  _peerFocusVisible: {
+    color: "fg",
+    top: "-3",
+    insetStart: "2",
+  },
+})
