6c544ed304f429c746520f8c5928318d9e98026c
Segun Adebayo <joseshegs@gmail.com>
2024-10-21 21:16:19 +0100

    perf: improve memo

--- packages/react/src/utils/memo.ts
+++ packages/react/src/utils/memo.ts
@@ -1,13 +1,9 @@
 export const memo = <T extends (...args: any[]) => any>(fn: T): T => {
-  const cache = new Map()
-  const get = (...args: any[]) => {
+  const cache: Record<string, any> = Object.create(null)
+  function get(...args: any[]) {
     const key = JSON.stringify(args)
-    if (cache.has(key)) {
-      return cache.get(key)
-    }
-    const result = fn(...args)
-    cache.set(key, result)
-    return result
+    if (cache[key] === undefined) cache[key] = fn(...args)
+    return cache[key]
   }
   return get as T
 }
