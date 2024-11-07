fac8d19ef7e2d3d12262997ccbc698f6441b70e8
Segun Adebayo <joseshegs@gmail.com>
2024-11-07 13:05:44 +0000

    refactor: client-only and show

--- .changeset/many-moose-fix.md
+++ .changeset/many-moose-fix.md
@@ -3,4 +3,8 @@
 ---
 
 - **Checkbox**: Fix issue where checkmark doesn't show on Safari
+
 - **Alert**: Fix issue where indicator icon is not visible on Safari
+
+- **ClientOnly**: Fix issue where returned the incorrect type leading to TS
+  error like `ClientOnly cannot be used as a JSX component.`
--- apps/compositions/src/examples/show-basic.tsx
+++ apps/compositions/src/examples/show-basic.tsx
@@ -7,12 +7,12 @@ export const ShowBasic = () => {
   const [count, setCount] = useState(0)
   return (
     <Stack align="flex-start">
-      <Show when={count > 3}>
-        <div>My Content</div>
-      </Show>
       <Button variant="outline" onClick={() => setCount(count + 1)}>
         Value: {count}
       </Button>
+      <Show when={count > 3}>
+        <div>My Content</div>
+      </Show>
     </Stack>
   )
 }
