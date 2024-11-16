eaea1d24999b50840150c5a7572980c65e18c0c1
Segun Adebayo <joseshegs@gmail.com>
2024-11-16 13:45:50 +0000

    fix: storybook

--- packages/react/src/theme/recipes/color-picker.ts
+++ packages/react/src/theme/recipes/color-picker.ts
@@ -1,4 +1,4 @@
-import { colorPickerAnatomy } from "@ark-ui/anatomy"
+import { colorPickerAnatomy } from "@ark-ui/react/color-picker"
 import { defineSlotRecipe } from "../../styled-system"
 
 export const colorPickerSlotRecipe = defineSlotRecipe({
