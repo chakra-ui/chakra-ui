c6a2287d7c57b75fa64a1510d1917f6b0151e9e1
Nicholas Barnett <n-barnett@hotmail.com>
2024-11-21 16:28:40 -0600

    fix: edited the story to confirm changes work

--- apps/compositions/src/examples/stack-with-separator.tsx
+++ apps/compositions/src/examples/stack-with-separator.tsx
@@ -1,9 +1,9 @@
-import { Stack, StackSeparator } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
 import { DecorativeBox } from "compositions/lib/decorative-box"
 
 export const StackWithSeparator = () => {
   return (
-    <Stack separator={<StackSeparator />}>
+    <Stack separator={<Box>∙</Box>}>
       <DecorativeBox h="20" />
       <DecorativeBox h="20" />
       <DecorativeBox h="20" />
