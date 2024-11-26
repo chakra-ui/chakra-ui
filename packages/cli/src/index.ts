6d4e7b6c794171392c57f394523e1298dfdc454e
TinsFox <fox@tinsfox.com>
2024-11-27 07:10:37 +0800

    fix(cli): output the real version (#9254)
    * feat(CLI): output the real CLI version
    
    * docs: changelog
    
    ---------
    
    Co-authored-by: Segun Adebayo <joseshegs@gmail.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 import * as p from "@clack/prompts"
 import { Command } from "commander"
+import { resolve } from "path/posix"
 import { EjectCommand } from "./commands/eject.js"
 import { SnippetCommand } from "./commands/snippet.js"
 import { TypegenCommand } from "./commands/typegen.js"
@@ -8,11 +9,11 @@ process.setMaxListeners(Infinity)
 
 export async function run() {
   p.intro("Chakra CLI ⚡️")
-
+  const packageJson = await import(resolve("package.json"))
   const program = new Command()
     .name("chakra-ui")
     .description("The official CLI for Chakra UI projects")
-    .version("3.0.0")
+    .version(packageJson.version)
 
   program
     .addCommand(TypegenCommand)
