4d8e6904c73948e3fa2d6e5db1ad75831fecd804
Segun Adebayo <joseshegs@gmail.com>
2024-11-27 23:56:10 +0000

    refactor: cli version eval

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,15 +1,17 @@
 import * as p from "@clack/prompts"
 import { Command } from "commander"
-import { resolve } from "path/posix"
+import { createRequire } from "node:module"
 import { EjectCommand } from "./commands/eject.js"
 import { SnippetCommand } from "./commands/snippet.js"
 import { TypegenCommand } from "./commands/typegen.js"
 
 process.setMaxListeners(Infinity)
 
+const req = createRequire(import.meta.url)
+const packageJson = req("@chakra-ui/cli/package.json")
+
 export async function run() {
   p.intro("Chakra CLI ⚡️")
-  const packageJson = await import(resolve("package.json"))
   const program = new Command()
     .name("chakra-ui")
     .description("The official CLI for Chakra UI projects")
