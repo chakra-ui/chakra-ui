537ace6b9dfb2df16e688a6d3c408aebc7a50fdc
Segun Adebayo <joseshegs@gmail.com>
2024-12-04 20:07:07 +0000

    docs: add popover example

---
+++ apps/compositions/src/examples/popover-with-custom-bg.tsx
@@ -0,0 +1,33 @@
+import { Input, Text } from "@chakra-ui/react"
+import { Button } from "compositions/ui/button"
+import {
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverRoot,
+  PopoverTitle,
+  PopoverTrigger,
+} from "compositions/ui/popover"
+
+export const PopoverWithCustomBg = () => {
+  return (
+    <PopoverRoot>
+      <PopoverTrigger asChild>
+        <Button size="sm" variant="outline">
+          Click me
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent css={{ "--popover-bg": "lightblue" }}>
+        <PopoverArrow />
+        <PopoverBody>
+          <PopoverTitle fontWeight="medium">Naruto Form</PopoverTitle>
+          <Text my="4">
+            Naruto is a Japanese manga series written and illustrated by Masashi
+            Kishimoto.
+          </Text>
+          <Input bg="bg" placeholder="Your fav. character" size="sm" />
+        </PopoverBody>
+      </PopoverContent>
+    </PopoverRoot>
+  )
+}
