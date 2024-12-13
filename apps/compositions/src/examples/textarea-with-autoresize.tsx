f3fb2bdfc17c8d321ee9263ed6b96344cc031378
Segun Adebayo <joseshegs@gmail.com>
2024-12-13 13:39:17 +0000

    docs: update textarea autoresize

--- apps/compositions/src/examples/textarea-with-autoresize.tsx
+++ apps/compositions/src/examples/textarea-with-autoresize.tsx
@@ -1,21 +1,5 @@
-"use client"
-
-import { chakra, useRecipe } from "@chakra-ui/react"
-import AutoResize from "react-textarea-autosize"
-
-const StyledAutoResize = chakra(AutoResize)
+import { Textarea } from "@chakra-ui/react"
 
 export const TextareaWithAutoresize = () => {
-  const recipe = useRecipe({ key: "textarea" })
-  const styles = recipe({ size: "sm" })
-  return (
-    <StyledAutoResize
-      placeholder="This textarea will autoresize as you type"
-      minH="initial"
-      resize="none"
-      overflow="hidden"
-      lineHeight="inherit"
-      css={styles}
-    />
-  )
+  return <Textarea autoresize />
 }
