4a7f3701afe33080b1b1e90cae244d08139f118e
Lucas J. Shuman <lucasjshuman@gmail.com>
2024-12-17 10:51:42 -0800

    Docs - Update input variant name in example (#9340)
    Update input-with-variants.tsx
    
    Update input variant name in example

--- apps/compositions/src/examples/input-with-variants.tsx
+++ apps/compositions/src/examples/input-with-variants.tsx
@@ -3,7 +3,7 @@ import { Input, Stack } from "@chakra-ui/react"
 export const InputWithVariants = () => {
   return (
     <Stack gap="4">
-      <Input placeholder="Filled" variant="subtle" />
+      <Input placeholder="Subtle" variant="subtle" />
       <Input placeholder="Outline" variant="outline" />
       <Input placeholder="Flushed" variant="flushed" />
     </Stack>
