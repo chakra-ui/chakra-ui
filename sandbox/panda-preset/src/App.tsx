0d8ddff323d0af810d3c5ee4e5020b302835c8c9
Segun Adebayo <joseshegs@gmail.com>
2025-01-04 21:00:46 +0000

    chore: test slot recipes

--- sandbox/panda-preset/src/App.tsx
+++ sandbox/panda-preset/src/App.tsx
@@ -1,6 +1,28 @@
 import { css } from "../styled-system/css"
 import { center, hstack, vstack } from "../styled-system/patterns"
-import { badge, button, heading } from "../styled-system/recipes"
+import { avatar, badge, button, card, heading } from "../styled-system/recipes"
+
+function MyAvatar(props: { src: string }) {
+  const classes = avatar({ size: "2xl" })
+  return (
+    <div className={classes.root}>
+      <img
+        src={props.src}
+        alt="Random placeholder image"
+        className={classes.image}
+      />
+    </div>
+  )
+}
+
+function MyCard(props: { children: React.ReactNode }) {
+  const classes = card({ size: "lg", variant: "outline" })
+  return (
+    <div className={classes.root}>
+      <div className={classes.body}>{props.children}</div>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -8,6 +30,7 @@ function App() {
       className={center({ padding: "4", colorPalette: "pink", minH: "dvh" })}
     >
       <div className={vstack({ gap: "4" })}>
+        <MyAvatar src="https://i.pravatar.cc/300?u=8" />
         <h1 className={heading({ size: "3xl" })}>Panda 🐼 + Chakra UI ⚡️</h1>
         <div className={css({ textStyle: "sm", color: "fg.muted" })}>
           This is a panda preset for Chakra UI{" "}
@@ -17,6 +40,8 @@ function App() {
           <button className={button({ variant: "solid" })}>Click me</button>
           <button className={button({ variant: "subtle" })}>Click me</button>
         </div>
+
+        <MyCard>This is a card</MyCard>
       </div>
     </div>
   )
