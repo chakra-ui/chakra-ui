6836416b09a1205b0f20ec0992e841ec31b5d1a4
Segun Adebayo <joseshegs@gmail.com>
2025-01-16 12:44:23 +0000

    docs: add context examples

---
+++ apps/compositions/src/examples/dialog-with-context.tsx
@@ -0,0 +1,38 @@
+import { DialogContext } from "@chakra-ui/react"
+import { Button } from "compositions/ui/button"
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogRoot,
+  DialogTrigger,
+} from "compositions/ui/dialog"
+
+export const DialogWithContext = () => {
+  return (
+    <DialogRoot>
+      <DialogTrigger asChild>
+        <Button variant="outline" size="sm">
+          Open Dialog
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogContext>
+          {(store) => (
+            <>
+              <DialogBody>
+                <p>Dialog is open: {store.open ? "true" : "false"}</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <button onClick={() => store.setOpen(false)}>Close</button>
+              </DialogBody>
+            </>
+          )}
+        </DialogContext>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
+  )
+}
---
+++ apps/compositions/src/examples/drawer-with-context.tsx
@@ -0,0 +1,38 @@
+import { DrawerContext } from "@chakra-ui/react"
+import { Button } from "compositions/ui/button"
+import {
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerRoot,
+  DrawerTrigger,
+} from "compositions/ui/drawer"
+
+export const DrawerWithContext = () => {
+  return (
+    <DrawerRoot>
+      <DrawerBackdrop />
+      <DrawerTrigger asChild>
+        <Button variant="outline" size="sm">
+          Open Drawer
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerContext>
+          {(store) => (
+            <DrawerBody>
+              <p>Drawer is open: {store.open ? "true" : "false"}</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button onClick={() => store.setOpen(false)}>Close</button>
+            </DrawerBody>
+          )}
+        </DrawerContext>
+        <DrawerCloseTrigger />
+      </DrawerContent>
+    </DrawerRoot>
+  )
+}
