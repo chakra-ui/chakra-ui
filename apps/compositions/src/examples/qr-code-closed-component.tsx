fbc0fc46346d3704d14ef89bdabaf7306c2d6ed9
Esther Agbaje <folasadeagbaje@gmail.com>
2025-01-28 14:30:20 +0000

    docs: updates to qr code

--- apps/compositions/src/examples/qr-code-closed-component.tsx
+++ apps/compositions/src/examples/qr-code-closed-component.tsx
@@ -7,18 +7,17 @@ export interface QrCodeProps
   overlay?: React.ReactNode
 }
 
-export const QrCodeClosedComponent = React.forwardRef<
-  HTMLDivElement,
-  QrCodeProps
->(function QrCode(props, ref) {
-  const { children, fill, overlay, ...rest } = props
-  return (
-    <ChakraQrCode.Root ref={ref} {...rest}>
-      <ChakraQrCode.Frame style={{ fill }}>
-        <ChakraQrCode.Pattern />
-      </ChakraQrCode.Frame>
-      {overlay}
-      {children && <ChakraQrCode.Overlay>{children}</ChakraQrCode.Overlay>}
-    </ChakraQrCode.Root>
-  )
-})
+export const QrCode = React.forwardRef<HTMLDivElement, QrCodeProps>(
+  function QrCode(props, ref) {
+    const { children, fill, overlay, ...rest } = props
+    return (
+      <ChakraQrCode.Root ref={ref} {...rest}>
+        <ChakraQrCode.Frame style={{ fill }}>
+          <ChakraQrCode.Pattern />
+        </ChakraQrCode.Frame>
+        {overlay}
+        {children && <ChakraQrCode.Overlay>{children}</ChakraQrCode.Overlay>}
+      </ChakraQrCode.Root>
+    )
+  },
+)
--- apps/compositions/src/examples/qr-code-with-fill.tsx
+++ apps/compositions/src/examples/qr-code-with-fill.tsx
@@ -6,7 +6,7 @@ export const QrCodeWithFill = () => {
       <For each={["#5417D7", "#FF0000"]}>
         {(fill) => (
           <QrCode.Root key={fill} value="https://www.google.com">
-            <QrCode.Frame fill={fill}>
+            <QrCode.Frame style={{ fill }}>
               <QrCode.Pattern />
             </QrCode.Frame>
           </QrCode.Root>
