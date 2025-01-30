613eee2de869c465c68e3fc25cd94fb0a2eaf3b7
Segun Adebayo <joseshegs@gmail.com>
2025-01-30 11:50:40 +0000

    refactor: progress closed component

--- apps/compositions/src/examples/progress-closed-component.tsx
+++ apps/compositions/src/examples/progress-closed-component.tsx
@@ -2,33 +2,32 @@ import { Progress as ChakraProgress } from "@chakra-ui/react"
 import { InfoTip } from "compositions/ui/toggle-tip"
 import * as React from "react"
 
-export const ProgressBar = React.forwardRef<
-  HTMLDivElement,
-  ChakraProgress.TrackProps
->(function ProgressBar(props, ref) {
-  return (
-    <ChakraProgress.Track {...props} ref={ref}>
-      <ChakraProgress.Range />
-    </ChakraProgress.Track>
-  )
-})
-
-export interface ProgressLabelProps extends ChakraProgress.LabelProps {
+interface ProgressProps extends ChakraProgress.RootProps {
+  showValueText?: boolean
+  valueText?: React.ReactNode
+  label?: React.ReactNode
   info?: React.ReactNode
 }
 
-export const ProgressLabel = React.forwardRef<
-  HTMLDivElement,
-  ProgressLabelProps
->(function ProgressLabel(props, ref) {
-  const { children, info, ...rest } = props
-  return (
-    <ChakraProgress.Label {...rest} ref={ref}>
-      {children}
-      {info && <InfoTip>{info}</InfoTip>}
-    </ChakraProgress.Label>
-  )
-})
-
-export const ProgressRoot = ChakraProgress.Root
-export const ProgressValueText = ChakraProgress.ValueText
+export const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(
+  function Progress(props, ref) {
+    const { showValueText, valueText, label, info, ...rest } = props
+    return (
+      <ChakraProgress.Root {...rest} ref={ref}>
+        {label && (
+          <ChakraProgress.Label>
+            {label} {info && <InfoTip>{info}</InfoTip>}
+          </ChakraProgress.Label>
+        )}
+        <ChakraProgress.Track>
+          <ChakraProgress.Range />
+        </ChakraProgress.Track>
+        {showValueText && (
+          <ChakraProgress.ValueText ref={ref}>
+            {valueText}
+          </ChakraProgress.ValueText>
+        )}
+      </ChakraProgress.Root>
+    )
+  },
+)
