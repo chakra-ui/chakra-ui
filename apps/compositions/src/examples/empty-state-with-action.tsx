f63af8aeefa10ccf754c6ac6cbcce1ecb81a2d95
Esther Agbaje <folasadeagbaje@gmail.com>
2025-01-31 14:55:31 +0000

    docs: remove snippets for empty state

--- apps/compositions/src/examples/empty-state-with-action.tsx
+++ apps/compositions/src/examples/empty-state-with-action.tsx
@@ -1,18 +1,25 @@
-import { Button, Group } from "@chakra-ui/react"
-import { EmptyState } from "compositions/ui/empty-state"
+import { Button, EmptyState, Group, VStack } from "@chakra-ui/react"
 import { HiColorSwatch } from "react-icons/hi"
 
 export const EmptyStateWithAction = () => {
   return (
-    <EmptyState
-      icon={<HiColorSwatch />}
-      title="Start adding tokens"
-      description="Add a new design token to get started"
-    >
-      <Group>
-        <Button>Create token</Button>
-        <Button variant="outline">Import</Button>
-      </Group>
-    </EmptyState>
+    <EmptyState.Root>
+      <EmptyState.Content>
+        <EmptyState.Indicator>
+          <HiColorSwatch />
+        </EmptyState.Indicator>
+        <VStack textAlign="center">
+          <EmptyState.Title>Start adding tokens</EmptyState.Title>
+          <EmptyState.Description>
+            Add a new design token to get started
+          </EmptyState.Description>
+
+          <Group>
+            <Button>Create token</Button>
+            <Button variant="outline">Import</Button>
+          </Group>
+        </VStack>
+      </EmptyState.Content>
+    </EmptyState.Root>
   )
 }
