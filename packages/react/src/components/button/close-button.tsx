614539f6283d3e4f7bafee6e17f54cf315be21f6
Segun Adebayo <joseshegs@gmail.com>
2025-02-01 20:17:01 +0000

    fix: close button

--- packages/react/src/components/button/close-button.tsx
+++ packages/react/src/components/button/close-button.tsx
@@ -1,9 +1,8 @@
-import type { ButtonProps } from "@chakra-ui/react"
-import { IconButton } from "@chakra-ui/react"
 import * as React from "react"
 import { CloseIcon } from "../icons"
+import { IconButton, type IconButtonProps } from "./icon-button"
 
-export interface CloseButtonProps extends ButtonProps {}
+export interface CloseButtonProps extends IconButtonProps {}
 
 export const CloseButton = React.forwardRef<
   HTMLButtonElement,
