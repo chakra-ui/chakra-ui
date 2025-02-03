a4d2ceb80509cf077f42c103412954ded4e2b9f2
Esther Agbaje <folasadeagbaje@gmail.com>
2025-02-03 18:14:15 +0000

    docs: remove snippets from tag

--- apps/compositions/src/examples/tag-with-close.tsx
+++ apps/compositions/src/examples/tag-with-close.tsx
@@ -1,14 +1,24 @@
-import { HStack } from "@chakra-ui/react"
-import { Tag } from "compositions/ui/tag"
+import { HStack, Tag } from "@chakra-ui/react"
 import { LuActivity } from "react-icons/lu"
 
 export const TagWithClose = () => {
   return (
     <HStack>
-      <Tag startElement={<LuActivity />} closable>
-        Tag 1
-      </Tag>
-      <Tag closable>Tag 2</Tag>
+      <Tag.Root>
+        <Tag.StartElement>
+          <LuActivity />
+        </Tag.StartElement>
+        <Tag.Label>Tag 1</Tag.Label>
+        <Tag.EndElement>
+          <Tag.CloseTrigger />
+        </Tag.EndElement>
+      </Tag.Root>
+      <Tag.Root>
+        <Tag.Label>Tag 2</Tag.Label>
+        <Tag.EndElement>
+          <Tag.CloseTrigger />
+        </Tag.EndElement>
+      </Tag.Root>
     </HStack>
   )
 }
--- apps/compositions/src/examples/tag-with-icon.tsx
+++ apps/compositions/src/examples/tag-with-icon.tsx
@@ -1,12 +1,27 @@
-import { HStack } from "@chakra-ui/react"
-import { Tag } from "compositions/ui/tag"
+import { HStack, Tag } from "@chakra-ui/react"
 import { LuCircleUser, LuFileBadge } from "react-icons/lu"
 
 export const TagWithIcon = () => {
   return (
     <HStack>
-      <Tag startElement={<LuCircleUser />}>Tag 2</Tag>
-      <Tag startElement={<LuFileBadge />}>Top Rated</Tag>
+      <Tag.Root>
+        <Tag.StartElement>
+          <LuCircleUser />
+        </Tag.StartElement>
+        <Tag.Label>Tag 1</Tag.Label>
+      </Tag.Root>
+      <Tag.Root>
+        <Tag.StartElement>
+          <LuFileBadge />
+        </Tag.StartElement>
+        <Tag.Label>Top Rated</Tag.Label>
+      </Tag.Root>
+      <Tag.Root>
+        <Tag.Label>Tag 2</Tag.Label>
+        <Tag.EndElement>
+          <LuCircleUser />
+        </Tag.EndElement>
+      </Tag.Root>
     </HStack>
   )
 }
