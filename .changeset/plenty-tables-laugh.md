e4d1e54e494ed802161f79be3c808d84971bbdec
Segun Adebayo <joseshegs@gmail.com>
2025-02-19 16:22:36 +0000

    fix: checkbox html-for

--- .changeset/plenty-tables-laugh.md
+++ .changeset/plenty-tables-laugh.md
@@ -5,4 +5,6 @@
 - Add support for React 19 `Ref` and `RefObject` typings
 - **Descendant**: Export `useDescendant` hooks
 - **NumberInput**: Allow `role` to be customizable
-- **Checkbox**: Fix `aria-checked` value when `isIndeterminate` is `true`
+- **Checkbox**
+  - Fix `aria-checked` value when `isIndeterminate` is `true`
+  - Fix removed `for` attribute on checkbox input
--- packages/components/src/checkbox/use-checkbox.ts
+++ packages/components/src/checkbox/use-checkbox.ts
@@ -247,6 +247,7 @@ export function useCheckbox(props: UseCheckboxProps = {}) {
   const getRootProps: PropGetter = useCallback(
     (props = {}, forwardedRef = null) => ({
       ...htmlProps,
+      htmlFor: rootIsLabelElement ? id : undefined,
       ...props,
       ref: mergeRefs(forwardedRef, (node: HTMLElement) => {
         if (!node) return
@@ -274,7 +275,7 @@ export function useCheckbox(props: UseCheckboxProps = {}) {
       "data-checked": dataAttr(isChecked),
       "data-invalid": dataAttr(isInvalid),
     }),
-    [htmlProps, isDisabled, isChecked, isInvalid, rootIsLabelElement],
+    [htmlProps, isDisabled, isChecked, isInvalid, rootIsLabelElement, id],
   )
 
   const getInputProps: PropGetter = useCallback(
@@ -305,7 +306,7 @@ export function useCheckbox(props: UseCheckboxProps = {}) {
         "aria-invalid": ariaInvalid ? Boolean(ariaInvalid) : isInvalid,
         "aria-describedby": ariaDescribedBy,
         "aria-disabled": isDisabled,
-        "aria-checked": isIndeterminate ? 'mixed' : isChecked,
+        "aria-checked": isIndeterminate ? "mixed" : isChecked,
         style: visuallyHiddenStyle,
       }
     },
@@ -313,6 +314,7 @@ export function useCheckbox(props: UseCheckboxProps = {}) {
       name,
       value,
       id,
+      tabIndex,
       handleChange,
       onBlurProp,
       onFocusProp,
@@ -328,7 +330,7 @@ export function useCheckbox(props: UseCheckboxProps = {}) {
       isInvalid,
       ariaDescribedBy,
       isDisabled,
-      tabIndex,
+      isIndeterminate,
     ],
   )
 
