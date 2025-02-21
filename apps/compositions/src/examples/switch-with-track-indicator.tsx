cdab70bd7f045b5cdb0f81f960dd86bafb861cb6
Esther Agbaje <folasadeagbaje@gmail.com>
2025-02-21 17:02:37 +0000

    docs: update track indicator

--- apps/compositions/src/examples/switch-with-track-indicator.tsx
+++ apps/compositions/src/examples/switch-with-track-indicator.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Icon, Switch } from "@chakra-ui/react"
 import { FaMoon, FaSun } from "react-icons/fa"
 
@@ -7,16 +9,8 @@ export const SwitchWithTrackIndicator = () => {
       <Switch.HiddenInput />
       <Switch.Control>
         <Switch.Thumb />
-        <Switch.Indicator
-          fallback={
-            <Icon color="gray.400">
-              <FaMoon />
-            </Icon>
-          }
-        >
-          <Icon color="yellow.400">
-            <FaSun />
-          </Icon>
+        <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+          <Icon as={FaSun} color="yellow.400" />
         </Switch.Indicator>
       </Switch.Control>
       <Switch.Label>Switch me</Switch.Label>
