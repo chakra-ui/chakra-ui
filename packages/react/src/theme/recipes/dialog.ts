5f6f9b7a104dbb8e21cbf4bf0a98191a6550532c
Mohamed Sayed <mohamedsayed008@gmail.com>
2025-04-07 18:06:23 -0400

    chore: space

--- packages/react/src/theme/recipes/dialog.ts
+++ packages/react/src/theme/recipes/dialog.ts
@@ -12,7 +12,7 @@ export const dialogSlotRecipe = defineSlotRecipe({
       top: 0,
       w: "100vw",
       h: "100dvh",
-      zIndex: "calc(var(--z-index) - 2) ",
+      zIndex: "calc(var(--z-index) - 2)",
       _open: {
         animationName: "fade-in",
         animationDuration: "slow",
