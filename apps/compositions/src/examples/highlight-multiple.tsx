0fe7341b603ed786062757a065dfcf765e5313ff
Luke Okomilo <git@email.okomilo.com>
2025-05-13 12:49:13 +0100

    Fix capitalization error in highlight multiple docs
    `Accentuate` in the array passed to query was capitalized, however `accentuate` in the content was lowercase.
    
    This caused the accentuate in the content to not be highlighted.

--- apps/compositions/src/examples/highlight-multiple.tsx
+++ apps/compositions/src/examples/highlight-multiple.tsx
@@ -4,7 +4,7 @@ export const HighlightMultiple = () => {
   return (
     <Heading lineHeight="tall">
       <Highlight
-        query={["spotlight", "emphasize", "Accentuate"]}
+        query={["spotlight", "emphasize", "accentuate"]}
         styles={{ px: "0.5", bg: "teal.muted" }}
       >
         With the Highlight component, you can spotlight, emphasize and
