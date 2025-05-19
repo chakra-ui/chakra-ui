0a0bc64dfda8c08f6f2dd3156575d114838fe585
James Fox <james@gamma.app>
2025-05-18 18:47:26 -0700

    fix(styleconfig): add tests

--- packages/components/src/system/style-config.test.tsx
+++ packages/components/src/system/style-config.test.tsx
@@ -1,123 +1,206 @@
 import { render } from "@chakra-ui/test-utils"
 import { ThemeProvider, useStyleConfig } from "."
 import { createTheme } from "./theme"
+import { Icon } from "../icon"
+import { FaReact } from "react-icons/fa"
+import type * as StyledSystem from "@chakra-ui/styled-system"
 
-test("should resolve styles in theme", async () => {
-  const Component = () => {
-    const styles = useStyleConfig("Button", { size: "sm" })
-    return <>{JSON.stringify(styles, null, 2)}</>
-  }
+const mockResolveStyleConfig = vi.fn().mockReturnValue(() => ({}))
+let mockResolveStyleConfigEnabled = false
 
-  const { asFragment } = render(
-    <ThemeProvider
-      theme={createTheme({
-        components: {
-          Button: {
-            baseStyle: {
-              px: 4,
-              py: 8,
-            },
-            sizes: {
-              sm: {
-                fontSize: 4,
-              },
-              md: {
-                fontSize: 8,
-              },
-            },
-          },
-        },
-      })}
-    >
-      <Component />
-    </ThemeProvider>,
-    {
-      withChakraProvider: false,
+vi.mock("@chakra-ui/styled-system", async (importOriginal) => {
+  const actual = await importOriginal<typeof StyledSystem>()
+  return {
+    ...actual,
+    resolveStyleConfig: (config: any) => {
+      if (mockResolveStyleConfigEnabled) {
+        return (props: any) => mockResolveStyleConfig(props)
+      }
+      return actual.resolveStyleConfig(config)
     },
-  )
-
-  expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
-      {
-      "px": 4,
-      "py": 8,
-      "fontSize": 4
-    }
-    </DocumentFragment>
-  `)
+  }
 })
 
-test("should resolve multipart styles in theme", async () => {
-  const Component = () => {
-    const props = {
-      size: "sm",
-      variant: "outline",
-    }
-    const styles = useStyleConfig("Tabs", props)
-    return <>{JSON.stringify(styles, null, 2)}</>
-  }
+describe("useStyleConfig", () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    mockResolveStyleConfigEnabled = false
+  })
 
-  const { asFragment } = render(
-    <ThemeProvider
-      theme={createTheme({
-        components: {
-          Tabs: {
-            parts: ["tablist", "tabpanel", "tab"],
-            defaultProps: { variant: "solid" },
-            baseStyle: {
-              tab: {
+  test("should resolve styles in theme", async () => {
+    const Component = () => {
+      const styles = useStyleConfig("Button", { size: "sm" })
+      return <>{JSON.stringify(styles, null, 2)}</>
+    }
+
+    const { asFragment } = render(
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            Button: {
+              baseStyle: {
                 px: 4,
-                py: 3,
-                color: "red.500",
+                py: 8,
               },
-            },
-            sizes: {
-              sm: {
-                tab: {
-                  fontSize: 5,
+              sizes: {
+                sm: {
+                  fontSize: 4,
                 },
-                tablist: {
-                  p: 4,
-                },
-              },
-            },
-            variants: {
-              outline: {
-                tablist: {
-                  border: "1px solid",
-                },
-              },
-              solid: {
-                tab: {
-                  bg: "red.300",
+                md: {
+                  fontSize: 8,
                 },
               },
             },
           },
-        },
-      })}
-    >
-      <Component />
-    </ThemeProvider>,
-    {
-      withChakraProvider: false,
-    },
-  )
-
-  expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
+        })}
+      >
+        <Component />
+      </ThemeProvider>,
       {
-      "tab": {
-        "px": 4,
-        "py": 3,
-        "color": "red.500",
-        "fontSize": 5
+        withChakraProvider: false,
       },
-      "tablist": {
-        "p": 4,
-        "border": "1px solid"
+    )
+
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        {
+        "px": 4,
+        "py": 8,
+        "fontSize": 4
       }
+      </DocumentFragment>
+    `)
+  })
+
+  test("should omit react element props", async () => {
+    mockResolveStyleConfigEnabled = true
+    const Component = () => {
+      const props = {
+        size: "sm",
+        children: <div>Hello</div>,
+        leftIcon: <Icon as={FaReact} />,
+      }
+      const styles = useStyleConfig("Button", props)
+      return <>{JSON.stringify(styles, null, 2)}</>
     }
-    </DocumentFragment>
-  `)
+
+    render(
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            Button: {
+              baseStyle: {
+                px: 4,
+                py: 8,
+              },
+              sizes: {
+                sm: {
+                  fontSize: 4,
+                },
+                md: {
+                  fontSize: 8,
+                },
+              },
+            },
+          },
+        })}
+      >
+        <Component />
+      </ThemeProvider>,
+      {
+        withChakraProvider: false,
+      },
+    )
+
+    expect(mockResolveStyleConfig).toHaveBeenCalledWith(
+      expect.objectContaining({
+        size: "sm",
+      }),
+    )
+    expect(mockResolveStyleConfig).not.toHaveBeenCalledWith(
+      expect.objectContaining({
+        children: expect.anything(),
+      }),
+    )
+    expect(mockResolveStyleConfig).not.toHaveBeenCalledWith(
+      expect.objectContaining({
+        leftIcon: expect.anything(),
+      }),
+    )
+  })
+
+  test("should resolve multipart styles in theme", async () => {
+    const Component = () => {
+      const props = {
+        size: "sm",
+        variant: "outline",
+      }
+      const styles = useStyleConfig("Tabs", props)
+      return <>{JSON.stringify(styles, null, 2)}</>
+    }
+
+    const { asFragment } = render(
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            Tabs: {
+              parts: ["tablist", "tabpanel", "tab"],
+              defaultProps: { variant: "solid" },
+              baseStyle: {
+                tab: {
+                  px: 4,
+                  py: 3,
+                  color: "red.500",
+                },
+              },
+              sizes: {
+                sm: {
+                  tab: {
+                    fontSize: 5,
+                  },
+                  tablist: {
+                    p: 4,
+                  },
+                },
+              },
+              variants: {
+                outline: {
+                  tablist: {
+                    border: "1px solid",
+                  },
+                },
+                solid: {
+                  tab: {
+                    bg: "red.300",
+                  },
+                },
+              },
+            },
+          },
+        })}
+      >
+        <Component />
+      </ThemeProvider>,
+      {
+        withChakraProvider: false,
+      },
+    )
+
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        {
+        "tab": {
+          "px": 4,
+          "py": 3,
+          "color": "red.500",
+          "fontSize": 5
+        },
+        "tablist": {
+          "p": 4,
+          "border": "1px solid"
+        }
+      }
+      </DocumentFragment>
+    `)
+  })
 })
