14003a9f73d4954c19ec4071da7180fab50a6b45
Segun Adebayo <joseshegs@gmail.com>
2025-07-01 15:35:19 +0100

    fix: lint and docs

--- apps/compositions/eslint.config.mjs
+++ apps/compositions/eslint.config.mjs
@@ -59,6 +59,7 @@ export default defineConfig([
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "react/prop-types": "off",
     },
   },
 ])
