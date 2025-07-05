ee2b9f02dcc1b911f4b70a0d3ee23381cdad09aa
Segun Adebayo <joseshegs@gmail.com>
2025-07-05 18:56:51 +0100

    chore: bump ark-ui

--- packages/react/src/components/client-only/client-only.tsx
+++ packages/react/src/components/client-only/client-only.tsx
@@ -4,7 +4,16 @@ import { useEffect, useState } from "react"
 import { Show } from "../show"
 
 export interface ClientOnlyProps {
-  children: React.ReactNode
+  /**
+   * The content to render on the client side.
+   *
+   * **Note:** Use the function pattern when accessing browser-only APIs.
+   */
+  children: React.ReactNode | (() => React.ReactNode)
+  /**
+   * The fallback content to render while the component is mounting on the client
+   * side.
+   */
   fallback?: React.ReactNode | undefined
 }
 
