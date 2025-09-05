ba379c7868fa3a10081f765b3fe8c538c3da99ff
Pranav Agone <pranavagone080304@gmail.com>
2025-09-05 09:53:38 +0530

    fix: Resolve CLS in color mode button (#10292)

--- apps/compositions/src/ui/color-mode.tsx
+++ apps/compositions/src/ui/color-mode.tsx
@@ -54,7 +54,7 @@ export const ColorModeButton = React.forwardRef<
 >(function ColorModeButton(props, ref) {
   const { toggleColorMode } = useColorMode()
   return (
-    <ClientOnly fallback={<Skeleton boxSize="8" />}>
+    <ClientOnly fallback={<Skeleton boxSize="9" />}>
       <IconButton
         onClick={toggleColorMode}
         variant="ghost"
