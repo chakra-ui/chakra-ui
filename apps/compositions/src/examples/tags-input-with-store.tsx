799f9653cd7d4ee94a1443a491ce0ce287f0d78e
Segun Adebayo <joseshegs@gmail.com>
2025-10-15 22:57:56 +0100

    chore: update example

--- apps/compositions/src/examples/tags-input-with-store.tsx
+++ apps/compositions/src/examples/tags-input-with-store.tsx
@@ -1,34 +1,32 @@
 "use client"
 
-import { TagsInput } from "@chakra-ui/react"
+import { Button, Stack, TagsInput } from "@chakra-ui/react"
 import { useTagsInput } from "@chakra-ui/react"
 
 export const TagsInputWithStore = () => {
-  const tagsStore = useTagsInput()
+  const tags = useTagsInput()
 
   return (
-    <div>
-      <TagsInput.RootProvider value={tagsStore}>
+    <Stack align="flex-start" gap="4">
+      <TagsInput.RootProvider value={tags}>
+        <TagsInput.Label>Tags: {JSON.stringify(tags.value)}</TagsInput.Label>
         <TagsInput.Control>
-          <TagsInput.Context>
-            {({ value }) =>
-              value.map((tag, index) => (
-                <TagsInput.Item key={index} index={index} value={tag}>
-                  <TagsInput.ItemPreview>
-                    <TagsInput.ItemText>{tag}</TagsInput.ItemText>
-                    <TagsInput.ItemDeleteTrigger />
-                  </TagsInput.ItemPreview>
-                  <TagsInput.ItemInput />
-                </TagsInput.Item>
-              ))
-            }
-          </TagsInput.Context>
-
+          {tags.value.map((tag, index) => (
+            <TagsInput.Item key={index} index={index} value={tag}>
+              <TagsInput.ItemPreview>
+                <TagsInput.ItemText>{tag}</TagsInput.ItemText>
+                <TagsInput.ItemDeleteTrigger />
+              </TagsInput.ItemPreview>
+              <TagsInput.ItemInput />
+            </TagsInput.Item>
+          ))}
           <TagsInput.Input placeholder="Add tag..." />
         </TagsInput.Control>
       </TagsInput.RootProvider>
 
-      <button onClick={() => tagsStore.setValue([])}>Clear All</button>
-    </div>
+      <Button variant="outline" size="sm" onClick={() => tags.clearValue()}>
+        Clear All
+      </Button>
+    </Stack>
   )
 }
