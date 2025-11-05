7b160256915f67cfc00f9ebd5cd30f542b201756
Segun Adebayo <joseshegs@gmail.com>
2025-11-05 17:47:27 +0000

    docs: carousel

--- apps/www/components/component-explorer.tsx
+++ apps/www/components/component-explorer.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import { ComponentExplorerWrapper } from "./component-snapshot"
 import { ExamplePreview } from "./example"
 
