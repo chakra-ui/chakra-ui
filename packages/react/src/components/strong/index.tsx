89fc9e5bcd2cdf1cde45b07bf6b2b853affc074b
justice chimobi <chimobi.justice@gmail.com>
2025-11-30 09:23:01 +0100

    fix(strong): correct props type and font weight for Strong component

--- packages/react/src/components/strong/index.tsx
+++ packages/react/src/components/strong/index.tsx
@@ -2,10 +2,10 @@
 
 import { type HTMLChakraProps, chakra } from "../../styled-system"
 
-export interface StrongProps extends HTMLChakraProps<"em"> {}
+export interface StrongProps extends HTMLChakraProps<"strong"> {}
 
 export const Strong = chakra("strong", {
-  base: { fontWeight: "semibold" },
+  base: { fontWeight: "bold" },
 })
 
 Strong.displayName = "Strong"
