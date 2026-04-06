646f35fd3f8b42f7e47633e62030f5a316f1a309
Segun Adebayo <joseshegs@gmail.com>
2026-04-06 10:24:16 +0200

    fix: panda preset script

--- sandbox/panda-preset/eslint.config.js
+++ sandbox/panda-preset/eslint.config.js
@@ -1,17 +1,25 @@
 import js from "@eslint/js"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
+import { defineConfig } from "eslint/config"
 import globals from "globals"
+import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+const tsconfigRootDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig(
+  { ignores: ["dist", "styled-system"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
