5b4a686d24141d8f7a298a36a29d20ebaf3c3477
Reza Abdi <52544177+Rezaabdi78@users.noreply.github.com>
2026-06-30 23:11:43 +0330

    Update vite.config.ts

--- sandbox/tanstack-router/vite.config.ts
+++ sandbox/tanstack-router/vite.config.ts
@@ -1,11 +1,11 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@chakra-ui/react": resolve("..", "..", "packages/react/src"),
