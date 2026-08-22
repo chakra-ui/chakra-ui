6bf27a7156b00bb036819a6606ff885dfbe7bc33
Segun Adebayo <joseshegs@gmail.com>
2026-08-22 14:11:56 +0200

    docs: simplify custom spinner indicator example

--- apps/compositions/src/examples/spinner-with-custom-indicator.tsx
+++ apps/compositions/src/examples/spinner-with-custom-indicator.tsx
@@ -1,28 +1,8 @@
-import { For, HStack, Spinner, Stack } from "@chakra-ui/react"
-import { ImSpinner6, ImSpinner9 } from "react-icons/im"
-import { PiSpinnerGapLight } from "react-icons/pi"
+import { Spinner } from "@chakra-ui/react"
+import { LuLoader } from "react-icons/lu"
 
 export const SpinnerWithCustomIndicator = () => (
-  <Stack>
-    <For each={[ImSpinner6, PiSpinnerGapLight, ImSpinner9]}>
-      {(Icon, i) => (
-        <HStack key={i}>
-          <For each={["xs", "sm", "md", "lg", "xl"]}>
-            {(size, j) => (
-              <Spinner
-                asChild
-                key={`${i}-${j}`}
-                borderWidth="0"
-                color="cyan.800"
-                size={size}
-                animationDuration="1s"
-              >
-                <Icon />
-              </Spinner>
-            )}
-          </For>
-        </HStack>
-      )}
-    </For>
-  </Stack>
+  <Spinner asChild borderWidth="0">
+    <LuLoader />
+  </Spinner>
 )
