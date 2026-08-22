f36ae5e2460e70cd174269ab8f12006d74a54c7d
Segun Adebayo <joseshegs@gmail.com>
2026-08-22 16:01:41 +0200

    revert: keep the default next-app template without an Emotion registry
    Streaming hydration belongs in the dedicated next-app-streaming sandbox.

--- sandbox/next-app/app/provider.tsx
+++ sandbox/next-app/app/provider.tsx
@@ -1,62 +1,14 @@
 "use client"
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import { CacheProvider } from "@emotion/react"
-import createCache from "@emotion/cache"
-import { useServerInsertedHTML } from "next/navigation"
 import { ThemeProvider } from "next-themes"
-import { useState } from "react"
-
-function EmotionRegistry({ children }: { children: React.ReactNode }) {
-  const [{ cache, flush }] = useState(() => {
-    const cache = createCache({ key: "css" })
-    cache.compat = true
-
-    const previousInsert = cache.insert
-    let inserted: string[] = []
-
-    cache.insert = (...args) => {
-      const serialized = args[1]
-      if (cache.inserted[serialized.name] === undefined) {
-        inserted.push(serialized.name)
-      }
-      return previousInsert(...args)
-    }
-
-    const flush = () => {
-      const previouslyInserted = inserted
-      inserted = []
-      return previouslyInserted
-    }
-
-    return { cache, flush }
-  })
-
-  useServerInsertedHTML(() => {
-    const names = flush()
-    if (names.length === 0) return null
-
-    const styles = names.map((name) => cache.inserted[name]).join("")
-
-    return (
-      <style
-        data-emotion={`${cache.key} ${names.join(" ")}`}
-        dangerouslySetInnerHTML={{ __html: styles }}
-      />
-    )
-  })
-
-  return <CacheProvider value={cache}>{children}</CacheProvider>
-}
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <EmotionRegistry>
-      <ChakraProvider value={defaultSystem}>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
-          {props.children}
-        </ThemeProvider>
-      </ChakraProvider>
-    </EmotionRegistry>
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {props.children}
+      </ThemeProvider>
+    </ChakraProvider>
   )
 }
