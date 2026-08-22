4cab8eba4b1bca54c5d974d5a82f0b3fb09e24b3
Segun Adebayo <joseshegs@gmail.com>
2026-08-22 14:52:26 +0200

    docs(date-picker): simplify Persian calendar example

--- apps/compositions/src/examples/date-picker-persian-calendar.tsx
+++ apps/compositions/src/examples/date-picker-persian-calendar.tsx
@@ -1,54 +1,53 @@
 "use client"
 
-import { Box, DatePicker, LocaleProvider, Portal } from "@chakra-ui/react"
-import { PersianCalendar, today } from "@internationalized/date"
+import { DatePicker, Portal } from "@chakra-ui/react"
+import { PersianCalendar } from "@internationalized/date"
 import { LuCalendar } from "react-icons/lu"
+
+const createCalendar = (identifier: string) => {
+  switch (identifier) {
+    case "persian":
+      return new PersianCalendar()
+    default:
+      throw new Error(`Unsupported calendar: ${identifier}`)
+  }
+}
 
 export const DatePickerPersianCalendar = () => {
   return (
-    <LocaleProvider locale="fa-IR">
-      <Box dir="rtl">
-        <DatePicker.Root
-          locale="fa-IR"
-          createCalendar={() => new PersianCalendar()}
-          defaultValue={[today("Asia/Tehran")]}
-          startOfWeek={6}
-          maxWidth="20rem"
-        >
-          <DatePicker.Label>تاریخ را انتخاب کنید</DatePicker.Label>
-
-          <DatePicker.Control>
-            <DatePicker.Input />
-
-            <DatePicker.IndicatorGroup>
-              <DatePicker.Trigger>
-                <LuCalendar />
-              </DatePicker.Trigger>
-            </DatePicker.IndicatorGroup>
-          </DatePicker.Control>
-
-          <Portal>
-            <DatePicker.Positioner>
-              <DatePicker.Content dir="rtl">
-                <DatePicker.View view="day">
-                  <DatePicker.Header dir={"ltr"} />
-                  <DatePicker.DayTable />
-                </DatePicker.View>
-
-                <DatePicker.View view="month">
-                  <DatePicker.Header dir={"ltr"} />
-                  <DatePicker.MonthTable />
-                </DatePicker.View>
-
-                <DatePicker.View view="year">
-                  <DatePicker.Header dir={"ltr"} />
-                  <DatePicker.YearTable />
-                </DatePicker.View>
-              </DatePicker.Content>
-            </DatePicker.Positioner>
-          </Portal>
-        </DatePicker.Root>
-      </Box>
-    </LocaleProvider>
+    <DatePicker.Root
+      locale="fa-IR"
+      createCalendar={createCalendar}
+      startOfWeek={6}
+      maxWidth="20rem"
+    >
+      <DatePicker.Label>تاریخ را انتخاب کنید</DatePicker.Label>
+      <DatePicker.Control>
+        <DatePicker.Input />
+        <DatePicker.IndicatorGroup>
+          <DatePicker.Trigger>
+            <LuCalendar />
+          </DatePicker.Trigger>
+        </DatePicker.IndicatorGroup>
+      </DatePicker.Control>
+      <Portal>
+        <DatePicker.Positioner>
+          <DatePicker.Content>
+            <DatePicker.View view="day">
+              <DatePicker.Header />
+              <DatePicker.DayTable />
+            </DatePicker.View>
+            <DatePicker.View view="month">
+              <DatePicker.Header />
+              <DatePicker.MonthTable />
+            </DatePicker.View>
+            <DatePicker.View view="year">
+              <DatePicker.Header />
+              <DatePicker.YearTable />
+            </DatePicker.View>
+          </DatePicker.Content>
+        </DatePicker.Positioner>
+      </Portal>
+    </DatePicker.Root>
   )
 }
