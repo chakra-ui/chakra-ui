55aee43ad25aab61b898f51e0b72f6b0a90df1be
Lope <adebesintolulope80@gmail.com>
2026-09-21 09:15:55 +0100

    docs(checkbox): fix controlled example with RootProvider (#11012)

--- apps/compositions/src/examples/checkbox-with-store.tsx
+++ apps/compositions/src/examples/checkbox-with-store.tsx
@@ -6,11 +6,9 @@ export const CheckboxWithStore = () => {
   const checkbox = useCheckbox()
   return (
     <Checkbox.RootProvider value={checkbox}>
-      <Checkbox.Root>
-        <Checkbox.HiddenInput />
-        <Checkbox.Control />
-        <Checkbox.Label>Accept terms and conditions</Checkbox.Label>
-      </Checkbox.Root>
+      <Checkbox.HiddenInput />
+      <Checkbox.Control />
+      <Checkbox.Label>Accept terms and conditions</Checkbox.Label>
     </Checkbox.RootProvider>
   )
 }
